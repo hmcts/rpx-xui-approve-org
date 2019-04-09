4a7858a809c65ee3f407247b2f2c1f99c66e5be8
Toireasa Moley <toireasa.moley@hmcts.net>
2019-04-09 14:19:15 +0100

    Further refactoring

--- src/org-manager/org-manager.routing.ts
+++ src/org-manager/org-manager.routing.ts
@@ -3,7 +3,6 @@ import {RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {AuthGuard} from '../auth/guards/auth.guard';
 import {OrganisationOverviewComponent} from './containers/overview/org-overview.component';
-import {Organisation} from '../organisation/organisation.model';
 
 export const ROUTES: Routes = [
   {
