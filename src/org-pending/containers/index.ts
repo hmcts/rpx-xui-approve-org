01ab6f0b7a3358305cf7112decf238254db83b73
Toireasa Moley <toireasa.moley@hmcts.net>
2019-04-17 12:20:03 +0100

    Org pending overview container added

---
+++ src/org-pending/containers/index.ts
@@ -0,0 +1,7 @@
+import { OverviewComponent } from './overview/org-pending-overview.component';
+
+export const containers: any[] = [
+  OverviewComponent,
+];
+
+export * from './overview/org-pending-overview.component';
