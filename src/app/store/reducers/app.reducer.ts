af80379d1aa08a0e73d6f1cc6aab71a28a2d931a
Toireasa Moley <toireasa.moley@hmcts.net>
2019-04-18 15:16:20 +0100

    Removing reference to pui

--- src/app/store/reducers/app.reducer.ts
+++ src/app/store/reducers/app.reducer.ts
@@ -2,18 +2,7 @@ import * as fromAction from '../actions';
 
 /* function that returns page title base on page url indexOf */
 export function setPageTitle(url): string {
-  if (url.indexOf('invite-user') !== -1) {
-    return 'Invite Users - Professional User Interface ';
-  } else if (url.indexOf('organisation') !== -1) {
-    return 'Organisation - Professional User Interface';
-  } else if (url.indexOf('profile') !== -1) {
-    return 'Profile - Professional User Interface';
-  } else if (url.indexOf('users') !== -1) {
-    return 'Users - Professional User Interface';
-  } else if (url.indexOf('login') !== -1) {
-    return 'Login - Professional User Interface';
-  }
-  return 'Professional User Interface';
+  return 'Organisation User Interface';
 }
 
 export interface AppState {
