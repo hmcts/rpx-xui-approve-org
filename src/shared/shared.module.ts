1e66723668dbee9efb4bd31db066f86b7c74fdcc
Toireasa Moley <toireasa.moley@hmcts.net>
2019-04-23 11:42:58 +0100

    Removing modules in shared not used

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core'
 import {GovUiModule} from '../../projects/gov-ui/src/lib/gov-ui.module';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {HeadersService} from './headers.service';
 import {ReactiveFormsModule} from '@angular/forms';
 import {RemoveHostDirective} from '../app/directives/remove-host.directive';
 
@@ -15,7 +14,6 @@ import {RemoveHostDirective} from '../app/directives/remove-host.directive';
     GovUiModule,
   ],
   providers: [
-    HeadersService
   ]
 })
 export class SharedModule {
