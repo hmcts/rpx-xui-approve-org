48b90641189f8f975f991d14e9bdeb611de697da
Toireasa Moley <toireasa.moley@hmcts.net>
2019-04-25 14:15:09 +0100

    Moving actions into action file

--- src/org-pending/store/actions/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './pending-organisation.actions';
--- src/org-pending/store/effects/index.ts
+++
@@ -1,7 +0,0 @@
-import { PendingOrganisationEffects } from './pending-organisation.effects';
-
-export const effects: any[] = [
-  PendingOrganisationEffects,
-];
-
-export * from './pending-organisation.effects';
