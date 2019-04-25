c1756b1bb0547c20a68540d84700cce56eed71e8
Toireasa Moley <toireasa.moley@hmcts.net>
2019-04-25 12:57:31 +0100

    Pending activation fields

--- src/org-pending/mock/pending-organisation.mock.ts
+++ src/org-pending/mock/pending-organisation.mock.ts
@@ -3,8 +3,20 @@ import { PendingOrganisation } from '../models/pending-organisation';
 export const PendingOrganisationsMock: PendingOrganisation[] = [
   {
     name: 'Miamora Limited',
+    organisationId: 'Miamora Limited',
+    address: '72 Guild Street, London, SE23 6FH',
+    pbaNumber: '123456',
+    admin: 'Mia Mamatora',
+    status: 'PENDING',
+    view: 'View'
   },
   {
     name: 'Turpin Limited',
+    organisationId: 'Turpin Limited',
+    address: '72 Guild Street, London, SE23 6FH',
+    pbaNumber: '123456',
+    admin: 'Mia Mamatora',
+    status: 'PENDING',
+    view: 'View'
   }
 ]
--- src/org-pending/models/pending-organisation.ts
+++ src/org-pending/models/pending-organisation.ts
@@ -1,5 +1,11 @@
 export interface  PendingOrganisation {
   name:string;
+  organisationId: string;
+  address: string;
+  pbaNumber: string;
+  admin: string;
+  status: string;
+  view: string;
 }
 export interface PendingOrganisationSummary extends PendingOrganisation {
   routerLink: string;
