d346af750d284a5cc1dbac05afae834d729e4b3a
Toireasa Moley <toireasa.moley@hmcts.net>
2019-04-29 10:59:39 +0100

    Updating the state

---
+++ src/org-pending/state/org-pending.effects.ts
@@ -0,0 +1,25 @@
+ import { Injectable } from '@angular/core'
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { PendingOrganisationService } from 'src/org-pending/services/pending-organisation.service';
+import * as pendingOrgActions from './actions/pendingOrg.actions'
+import { mergeMap, map } from 'rxjs/operators';
+import { PendingOrganisation } from "../models/pending-organisation";
+ 
+@Injectable()
+ export class PendingOrgEffects {
+     constructor(private actions$: Actions,
+        private pendingOrgService: PendingOrganisationService){}
+
+        @Effect()
+        loadPendingOrgs$ = this.actions$.pipe(
+            ofType(pendingOrgActions.PendingOrgActionTypes.Load),
+            mergeMap((action:pendingOrgActions.Load) => this.pendingOrgService.fetchPendingOrganisations().pipe
+            (
+                map((pendingOrganisations: PendingOrganisation[]) => new pendingOrgActions.LoadSuccess(pendingOrganisations)))
+            ))
+            }
+
+
+
+
+ 
