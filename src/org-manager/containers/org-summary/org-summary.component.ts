2fd9f4828e4c0283b11437c1d7e949bb38e03840
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-03 10:59:22 +0100

    Product example working

--- src/org-manager/containers/org-summary/org-summary.component.ts
+++ src/org-manager/containers/org-summary/org-summary.component.ts
@@ -4,6 +4,8 @@ import {select, Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {map} from 'rxjs/internal/operators';
+import { IProduct } from '../../models/product'
+import { ProductService } from '../../services/product.service'
 
 @Component({
   selector: 'app-org-summary',
@@ -13,9 +15,13 @@ import {map} from 'rxjs/internal/operators';
 export class OrgSummaryComponent implements OnInit, OnDestroy {
   orgSummary$: Observable<any>;
   loading$: Observable<boolean>;
+  products: IProduct[] = [];
+  errorMessage = '';
+  
   constructor(
     private activeRoute: ActivatedRoute,
-    private store: Store<fromfeatureStore.OrganisationState>) { }
+    private store: Store<fromfeatureStore.OrganisationState>,
+    private productService: ProductService) { }
 
   ngOnInit() {
     console.log('the activated route path')
@@ -35,6 +41,13 @@ export class OrgSummaryComponent implements OnInit, OnDestroy {
 
     this.orgSummary$ = this.store.pipe(select(fromfeatureStore.getSingleAccounOverview));
     this.loading$ = this.store.pipe(select(fromfeatureStore.orgSummaryLoading));
+
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products;
+      },
+      error => this.errorMessage = <any>error
+    );
   }
   ngOnDestroy() {
     this.store.dispatch(new fromfeatureStore.ResetSingleOrg({}));
---
+++ src/org-manager/services/product.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+import { IProduct } from '../../org-manager/models/product'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private productUrl = 'http://localhost:3000/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  getProduct(id: number): Observable<IProduct | undefined> {
+    console.log('in individual')
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find(p => p.productId === id))
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
