fe598ca046208b523ff98747247147058a898cc7
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-03 11:28:54 +0100

    Single json return

--- src/org-manager/models/product.ts
+++ src/org-manager/models/product.ts
@@ -7,4 +7,13 @@ export interface IProduct {
     description: string;
     starRating: number;
     imageUrl: string;
+    name: string;
+    address: string;
+    status: string | 'Active';
+    effective_date: string;
+    dx_exchange: string;
+    pbaNumber: string;
+    dxNumber: string;
+    dxExchange: string;
+    admin: string;
   }
