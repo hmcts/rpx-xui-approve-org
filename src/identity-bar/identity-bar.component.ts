98ee55c3965c97971b4ee62f22f6c2ac969242b7
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-07 15:42:53 +0100

    Adding identity bar component

--- src/identity-bar/identity-bar.component.ts
+++ src/identity-bar/identity-bar.component.ts
@@ -1,11 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+
 @Component({
   selector: 'identity-bar-component',
   templateUrl: './identity-bar.component.html',
   styleUrls: ['./identity-bar.component.scss']
 })
 
-export class IdentityBarComponent implements OnInit{
+export class IdentityBarComponent implements OnInit {
+
+  @Input() data: any;
   
   constructor() {}
 
