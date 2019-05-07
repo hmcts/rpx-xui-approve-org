30279b13f881ac02b4bd9544e44d8b32d2118cd9
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-07 16:42:51 +0100

    Removing unused files

--- projects/admin-panel-ui/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of admin-panel-ui
- */
-
-export * from './lib/admin-panel-ui.service';
-export * from './lib/admin-panel-ui.component';
-export * from './lib/admin-panel-ui.module';
