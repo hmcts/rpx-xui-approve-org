6e6f0357676128212aa358a699d9fdf552761d65
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-08 12:52:45 +0100

    Moving components into the org manager feature module

--- src/org-manager/org-manager.module.ts
+++ src/org-manager/org-manager.module.ts
@@ -17,8 +17,8 @@ import { reducers, effects } from './store';
 import { OrgOverviewComponent } from './containers/org-overview/org-overview.component';
 import { OrgSummaryComponent } from './containers/org-summary/org-summary.component';
 import { OrgPendingModule } from 'src/org-pending/org-pending.module';
-import { NotificationBannerComponent } from 'src/notification-banner/notification-banner.component';
-import { IdentityBarComponent } from 'src/identity-bar/identity-bar.component'
+import { NotificationBannerComponent } from 'src/org-manager/components/notification-banner/notification-banner.component';
+import { IdentityBarComponent } from 'src/org-manager/components/identity-bar/identity-bar.component'
 
 export const COMPONENTS = [ OrgOverviewComponent, OrgSummaryComponent, NotificationBannerComponent, IdentityBarComponent];
 
