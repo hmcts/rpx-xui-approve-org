c7136c8d9c9c190d43f5d0c62c2dcc6219d36ec0
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-09 10:55:48 +0100

    Removing unused files

--- api/local.ts
+++ api/local.ts
@@ -6,11 +6,9 @@ import * as session from 'express-session'
 import * as globalTunnel from 'global-tunnel-ng'
 import * as log4js from 'log4js'
 import * as sessionFileStore from 'session-file-store'
-import * as auth from './auth'
 import { appInsights } from './lib/appInsights'
 import config from './lib/config'
 import { errorStack } from './lib/errorStack'
-import routes from './routes'
 
 const FileStore = sessionFileStore(session)
 
@@ -47,11 +45,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.get('/oauth2/callback', auth.oauth)
-
-app.use(auth.attach)
-
-app.use('/api', routes)
 
 const port = process.env.PORT || 3000
 app.listen(port)
