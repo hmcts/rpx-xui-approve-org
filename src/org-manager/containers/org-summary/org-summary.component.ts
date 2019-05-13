2ff1e879460d35137269563379f3e60f9e0ceef6
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-13 16:58:53 +0100

    Small fixes

--- src/org-manager/containers/org-summary/org-summary.component.ts
+++ src/org-manager/containers/org-summary/org-summary.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import * as fromfeatureStore from '../../store';
 import {select, Store} from '@ngrx/store';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -13,18 +13,24 @@ export class OrgSummaryComponent implements OnInit, OnDestroy {
   loading$: Observable<boolean>;
   orgSummary = {};
   errorMessage = '';
+  $routeSubscription: Subscription;
+  pageId: string;
   constructor(
     private activeRoute: ActivatedRoute,
     private store: Store<fromfeatureStore.OrganisationState>) { }
 
   ngOnInit() {
 
-    this.activeRoute.url.subscribe(url => {
-      this.store.dispatch(new fromfeatureStore.LoadSingleOrg({ id: url[0].path
-      }));
-    });
     this.orgSummary$ = this.store.pipe(select(fromfeatureStore.getSingleOrgOverview));
     this.loading$ = this.store.pipe(select(fromfeatureStore.orgSummaryLoading));
+
+    this.$routeSubscription = this.store.pipe(select(fromfeatureStore.getCurrentPage)).subscribe((routeParams) => {
+      if (routeParams.id && routeParams.id !== this.pageId) { // TODO see why double call.
+        this.pageId = routeParams.id;
+        this.store.dispatch(new fromfeatureStore.LoadSingleOrg({ id: this.pageId
+        }));
+      }
+    });
 
   }
   ngOnDestroy() {
