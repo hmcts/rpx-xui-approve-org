7dc62d2b284305de0eab8f7d1da49e94b58f917e
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-13 16:58:53 +0100

    Small fixes

--- src/org-manager/store/selectors/organisation.selectors.ts
+++ src/org-manager/store/selectors/organisation.selectors.ts
@@ -2,6 +2,7 @@ import {createFeatureSelector, createSelector} from '@ngrx/store';
 
 import * as fromFeature from '../reducers';
 import * as fromOrganisation from '../reducers/organisation.reducer';
+import * as fromRoot from '../../../app/store';
 
 
 
@@ -10,3 +11,8 @@ export const getOrganisationsState = createSelector( selectFeatureOrg, (state: a
 export const organisations = createSelector( getOrganisationsState, fromOrganisation.getOrganisations);
 export const organisationsLoading = createSelector( getOrganisationsState, fromOrganisation.getOrganisationsLoading);
 export const organisationsLoaded = createSelector( getOrganisationsState, fromOrganisation.getOrganisationsLoaded);
+
+export const getCurrentPage = createSelector(
+    fromRoot.getRouterState,
+    (router) => router.state.params
+  );
