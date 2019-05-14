cb0f961892f5b9e956ac52fff29e616fe4dbb75b
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-14 17:06:26 +0100

    Changing store

--- src/org-pending/store/effects/org-pending.effects.ts
+++ src/org-pending/store/effects/org-pending.effects.ts
@@ -2,19 +2,25 @@
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { PendingOrganisationService } from 'src/org-pending/services/pending-organisation.service';
 import * as pendingOrgActions from '../actions/org-pending.actions';
-import { mergeMap, map } from 'rxjs/operators';
+import { mergeMap, map, switchMap, catchError } from 'rxjs/operators';
 import { PendingOrganisation } from '../../models/pending-organisation';
+import { of } from 'rxjs';
 @Injectable()
  export class PendingOrgEffects {
-     constructor(private actions$: Actions,
+     constructor(
+        private actions$: Actions,
         private pendingOrgService: PendingOrganisationService) {}
 
         @Effect()
         loadPendingOrgs$ = this.actions$.pipe(
             ofType(pendingOrgActions.PendingOrgActionTypes.Load),
-            mergeMap((action: pendingOrgActions.Load) => this.pendingOrgService.fetchPendingOrganisations().pipe
-            (
-                map((pendingOrganisations: PendingOrganisation[]) => new pendingOrgActions.LoadSuccess(pendingOrganisations)))
-            ));
+            switchMap(() => {
+                return this.pendingOrgService.fetchPendingOrganisations().pipe(
+                  map(pendingOrganisations => new pendingOrgActions.LoadSuccess(pendingOrganisations)),
+                  catchError(error => of(new pendingOrgActions.LoadFail(error)))
+                );
+              })
+            );
             }
 
+  
