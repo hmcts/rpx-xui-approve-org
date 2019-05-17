a5a5b1b0dce04d314af00c2f04862838a3d45a08
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-17 13:41:33 +0100

    Removing unused code

--- projects/gov-ui/src/lib/components/govuk-table/govuk-table.component.ts
+++ projects/gov-ui/src/lib/components/govuk-table/govuk-table.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
-import { MOCKDATAMAPPINGS } from '../govuk-table/mock-data-mappings';
 import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 
 @Component({
@@ -17,13 +16,7 @@ export class GovukTableComponent implements OnInit{
 
     @Input() rows;
 
-    @Input() columnConfig: GovukTableColumnConfig[] = [
-        { header: MOCKDATAMAPPINGS[0].header, key: MOCKDATAMAPPINGS[0].key, type: MOCKDATAMAPPINGS[0].type },
-        { header: MOCKDATAMAPPINGS[1].header, key: MOCKDATAMAPPINGS[1].key },
-        { header: MOCKDATAMAPPINGS[2].header, key: MOCKDATAMAPPINGS[2].key, class: MOCKDATAMAPPINGS[2].class },
-        { header: MOCKDATAMAPPINGS[3].header, key: MOCKDATAMAPPINGS[3].key, type: MOCKDATAMAPPINGS[3].type,
-         multiColumnMapping: MOCKDATAMAPPINGS[3].multiColumnMapping }
-    ];
+    @Input() columnConfig: GovukTableColumnConfig[];
 
     constructor(private fb: FormBuilder) { }
     ngOnInit(): void {
