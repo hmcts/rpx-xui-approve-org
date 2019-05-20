293b734117fda57f4c0b19bca3ff6809d0e27909
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-20 12:19:24 +0100

    Updating actions

--- src/org-pending/store/selectors/org-pending.selectors.ts
+++ src/org-pending/store/selectors/org-pending.selectors.ts
@@ -3,16 +3,6 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 const getPendingOrgsFeatureState = createFeatureSelector<fromOrganisation.PendingOrganisationState>('org-pending');
 
-export const getShowPendingOrgCode = createSelector(
-    getPendingOrgsFeatureState,
-    state => state.showPendingOrg
-);
-
-export const getCurrentOrgs = createSelector(
-    getPendingOrgsFeatureState,
-    state => state.currentOrg
-);
-
 export const getPendingOrgs = createSelector(
     getPendingOrgsFeatureState,
     state => state.pendingOrganisations
