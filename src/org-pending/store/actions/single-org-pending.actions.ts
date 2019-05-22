2006029187f87d353a0ca36ba76f18367fd25953
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-22 15:32:13 +0100

    Updating actions and effects

--- src/org-pending/store/actions/single-org-pending.actions.ts
+++ src/org-pending/store/actions/single-org-pending.actions.ts
@@ -2,14 +2,14 @@ import {Action} from '@ngrx/store';
 import {SingleOrgSummary} from '../../../org-manager/models/single-org-summary';
 
 export enum SinglePendingOrgActionTypes {
-   LOAD = '[Single Pending Organisation] Load Single Pending Organisation',
+   LOAD_SINGLE_PENDING_ORGANISATIONS = '[Single Pending Organisation] Load Single Pending Organisation',
    LOAD_SUCCESS = '[Single Pending Organisation] Load Single Pending Organisations Success',
    LOAD_FAIL = '[Single Pending Organisation] Load Single Pending Organisation Fail',
    RESET_SINGLE_ORG = '[Single Pending Organisation] Reset Single Pending Organisation'
 }
 
 export class LoadSingleOrg {
-  readonly type = SinglePendingOrgActionTypes.LOAD;
+  readonly type = SinglePendingOrgActionTypes.LOAD_SINGLE_PENDING_ORGANISATIONS;
   constructor(public payload: any) {}
 }
 
