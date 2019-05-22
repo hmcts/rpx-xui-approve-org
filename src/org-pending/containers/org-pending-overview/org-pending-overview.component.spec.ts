5667f27179444544afbae8dee6928c8d40f9fa0b
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-22 17:10:33 +0100

    Removing tests

--- src/org-pending/containers/org-pending-overview/org-pending-overview.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import {OrgOverviewComponent} from './org-pending-overview.component';
-
-describe('Org Overview Component', () => {
-    it('should have a component', () => {
-        const component = new OrgOverviewComponent();
-        expect(component).toBeTruthy();
-});
-});
-
