3d6a028997d85597b1fc8256f50cdb43b7e46040
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-22 16:45:37 +0100

    Removing identity bar

--- src/org-pending/components/identity-bar/identity-bar.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IdentityBarComponent } from './identity-bar.component';
-
-describe('Identity Bar Component', () => {
-    it('should have a component', () => {
-        const component = new IdentityBarComponent();
-        expect(component).toBeTruthy();
-});
-});
