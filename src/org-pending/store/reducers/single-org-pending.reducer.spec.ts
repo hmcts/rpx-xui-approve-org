b0116155e96a6881ddce80f8b3f4c706302fbf62
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-23 11:28:30 +0100

    Updating reducer tests

--- src/org-pending/store/reducers/single-org-pending.reducer.spec.ts
+++ src/org-pending/store/reducers/single-org-pending.reducer.spec.ts
@@ -1,7 +1,7 @@
 import { initialState, reducer } from './single-org-pending.reducer';
 import { LoadSingleOrgSuccess } from '../actions';
 import { SingleOrgSummary } from 'src/org-manager/models/single-org-summary';
-import * as fromSingleOrg from './single-org-pending.reducer';
+import * as fromSingleOrgPending from './single-org-pending.reducer';
 
   describe('SingleOrgPendingReducer', () => {
     describe('undefined action', () => {
@@ -12,3 +12,28 @@ import * as fromSingleOrg from './single-org-pending.reducer';
       });
     });
 });
+
+describe('LOAD_SINGLE_PENDING_SUCCESS action', () => {
+    it('should update the state.data', () => {
+      const payload: SingleOrgSummary = {
+        'pbaNumber': 'SU2DSCSA',
+         'status': 'ACTIVE',
+         'effective_date': '22/10/2022',
+         'dx_exchange': '7654321',
+         'name': 'Speake Limited',
+         'address': '72 Guild Street, London, SE23 6FH',
+         'dxNumber': '12345567',
+         'dxExchange': '7654321',
+         'admin': 'Matt Speake',
+       };
+      const action = new LoadSingleOrgSuccess(payload);
+      const state = reducer(initialState, action);
+      expect(state.data).toEqual(payload);
+    });
+  });
+
+  describe('getSingleOrganisation export', () => {
+    it('should return state.organisation', () => {
+      expect(fromSingleOrgPending.getSingleOrgOverview(initialState)).toEqual({});
+    });
+  });
