1ac0d3337a8eb8ed2b9192ca8e665e639a5e2a2d
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-23 12:11:07 +0100

    Code refactor tests

--- src/org-pending/store/actions/single-org-pending.actions.spec.ts
+++ src/org-pending/store/actions/single-org-pending.actions.spec.ts
@@ -1,8 +1,8 @@
 import * as fromSingleOrg from './single-org-pending.actions';
 import { SingleOrgSummary } from 'src/org-manager/models/single-org-summary';
 
-describe('Single organisation actions', () => {
-  describe('LoadSingleOrganisation actions GROUP', () => {
+describe('Single pending organisation actions', () => {
+  describe('LoadSinglePendingOrganisation actions GROUP', () => {
     // Init state
     describe('LoadSingleOrg', () => {
       it('should create an action', () => {
@@ -17,38 +17,37 @@ describe('Single organisation actions', () => {
   });
 });
 
-    // Success
-    describe('LoadSingleOrganisationSuccess', () => {
-      it('should create an action', () => {
-        const payload: SingleOrgSummary = {
-          'pbaNumber': 'SU2DSCSA',
-           'status': 'ACTIVE',
-           'effective_date': '22/10/2022',
-           'dx_exchange': '7654321',
-           'name': 'Speake Limited',
-           'address': '72 Guild Street, London, SE23 6FH',
-           'dxNumber': '12345567',
-           'dxExchange': '7654321',
-           'admin': 'Matt Speake',
-         };
-        const action = new fromSingleOrg.LoadSingleOrgSuccess(payload);
-        expect({ ...action }).toEqual({
-          type: fromSingleOrg.SinglePendingOrgActionTypes.LOAD_SINGLE_PENDING_ORGANISATIONS_SUCCESS,
-          payload
-        });
-      });
+// Success
+describe('LoadSinglePendingOrganisationSuccess', () => {
+  it('should create an action', () => {
+    const payload: SingleOrgSummary = {
+      'pbaNumber': 'SU2DSCSA',
+      'status': 'ACTIVE',
+      'effective_date': '22/10/2022',
+      'dx_exchange': '7654321',
+      'name': 'Speake Limited',
+      'address': '72 Guild Street, London, SE23 6FH',
+      'dxNumber': '12345567',
+      'dxExchange': '7654321',
+      'admin': 'Matt Speake',
+    };
+    const action = new fromSingleOrg.LoadSingleOrgSuccess(payload);
+    expect({ ...action }).toEqual({
+      type: fromSingleOrg.SinglePendingOrgActionTypes.LOAD_SINGLE_PENDING_ORGANISATIONS_SUCCESS,
+      payload
+    });
+  });
 
-              // Fail
-              describe('LoadSingleOrganisationFail', () => {
-                it('should create an action', () => {
-                  // Action is not been used. Should be passing error handler or error friendly string.
-                  const action = new fromSingleOrg.LoadSingleOrgFail('Something');
-                  const payload = 'Something';
-                  expect({ ...action }).toEqual({
-                    type: fromSingleOrg.SinglePendingOrgActionTypes.LOAD_SINGLE_PENDING_ORGANISATIONS_FAIL,
-                    payload
-                  });
-                });
-         
-          });
-          });
+  // Fail
+  describe('LoadSinglePendingOrganisationFail', () => {
+    it('should create an action', () => {
+      // Action is not been used. Should be passing error handler or error friendly string.
+      const action = new fromSingleOrg.LoadSingleOrgFail('Error');
+      const payload = 'Error';
+      expect({ ...action }).toEqual({
+        type: fromSingleOrg.SinglePendingOrgActionTypes.LOAD_SINGLE_PENDING_ORGANISATIONS_FAIL,
+        payload
+      });
+    });
+  });
+});
