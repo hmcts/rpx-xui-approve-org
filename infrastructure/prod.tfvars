e7984e5cc79fc43e5964112b969956f5519b9df0
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-29 11:03:56 +0100

    Adding infra files

---
+++ infrastructure/prod.tfvars
@@ -0,0 +1,1 @@
+additional_host_name="xui-manage-org.judiciary.uk"
---
+++ infrastructure/variables.tf
@@ -0,0 +1,53 @@
+variable "product" {
+  type = "string"
+}
+
+variable "shared_product_name" {
+    default = "rpx"
+}
+
+variable "component" {
+  type = "string"
+}
+
+variable "team_name" {
+  default = "xui-org-manag"
+}
+
+variable "app_language" {
+    default = "node"
+}
+
+variable "location" {
+  type = "string"
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+}
+
+variable "subscription" {
+  type = "string"
+}
+
+variable "ilbIp"{}
+
+variable "tenant_id" {}
+
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "common_tags" {
+  type = "map"
+}
+
+variable "capacity" {
+  default = "1"
+}
+
+variable "additional_host_name" {
+    default = "null"
+}
