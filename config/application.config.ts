d0b8e4b006924ec56dc475df15e6406c3273cc76
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-30 13:18:38 +0100

    add config files

---
+++ config/application.config.ts
@@ -0,0 +1,10 @@
+export const application = {
+    cookies: {
+        token: '__auth__',
+        userId: '__userid__'
+    },
+    microservice: 'jui_webapp',
+    idam_client: 'juiwebapp',
+    oauth_callback_url: 'oauth2/callback',
+    protocol: 'https'
+};
