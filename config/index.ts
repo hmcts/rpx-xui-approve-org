6eab462d6f81c258c34ff1ed313adcec8a1a8285
Toireasa Moley <toireasa.moley@hmcts.net>
2019-06-04 19:29:28 +0100

    Code refactor

--- config/index.ts
+++ config/index.ts
@@ -2,29 +2,19 @@ import { application } from "./application.config";
 
 import * as local from "./environments/local.config";
 import * as docker from "./environments/docker.config";
-import * as spreview from "./environments/spreview.config";
-import * as saat from "./environments/saat.config";
-import * as sprod from "./environments/sprod.config";
 import * as preview from "./environments/preview.config";
-import * as demo from "./environments/demo.config";
 import * as aat from "./environments/aat.config";
 import * as prod from "./environments/prod.config";
-import * as mock from "./environments/mock.config";
 import * as process from "process";
 
 const configs = {
     local,
     docker,
-    spreview,
-    saat,
-    sprod,
     preview,
-    demo,
     aat,
     prod,
-    mock,
-    microservice: "jui_webapp",
-    idam_client: "juiwebapp",
+    microservice: "xui_approve_org",
+    idam_client: "xui_approve_org",
     oauth_callback_url: "oauth2/callback",
     protocol: "https"
 };
