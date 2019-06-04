ee53a4df7e031be68773452cbb14c70c2b7eb853
Toireasa Moley <toireasa.moley@hmcts.net>
2019-06-04 19:29:28 +0100

    Code refactor

--- config/environments/docker.config.ts
+++ config/environments/docker.config.ts
@@ -1,15 +1,8 @@
 export default {
     services: {
-        ccd_data_api: 'http://localhost:4452',
-        ccd_def_api: 'http://localhost:4452',
         idam_web: 'https://localhost:3501',
         idam_api: 'http://localhost:4501',
-        s2s: 'http://localhost:4502',
-        draft_store_api: 'http://localhost:8080',
-        dm_store_api: 'http://localhost:4603',
-        em_anno_api: 'http://localhost:3621',
-        em_npa_api: 'http://localhost:3622',
-        coh_cor_api: 'http://localhost:4701'
+        s2s: 'http://localhost:4502'
     },
     useProxy: false,
     protocol: 'http',
--- config/environments/prod.config.ts
+++ config/environments/prod.config.ts
@@ -1,22 +1,11 @@
 export default {
     services: {
-        ccd_data_api:
-            'https://ccd-data-store-api-prod.service.core-compute-prod.internal',
-        ccd_def_api:
-            'https://ccd-definition-store-api-prod.service.core-compute-prod.internal',
         idam_web:
             'https://idam-web-public-idam-prod.service.core-compute-prod.internal',
         idam_api:
           'https://idam-api-idam-prod.service.core-compute-prod.internal',
         s2s:
-            'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal',
-        draft_store_api:
-            'https://draft-store-service-prod.service.core-compute-prod.internal',
-        dm_store_api:
-            'https://dm-store-prod.service.core-compute-prod.internal',
-        em_anno_api: 'https://em-anno-prod.service.core-compute-prod.internal',
-        em_npa_api: 'https://em-npa-prod.service.core-compute-prod.internal',
-        coh_cor_api: 'https://coh-cor-prod.service.core-compute-prod.internal'
+            'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal'
     },
     useProxy: false,
     secureCookie: true,
