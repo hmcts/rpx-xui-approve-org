e17039a582f077fc515bec6ec22f8dde4b25dc52
Toireasa Moley <toireasa.moley@hmcts.net>
2019-06-06 11:43:22 +0100

    updating client key

--- config/application.config.ts
+++ config/application.config.ts
@@ -3,8 +3,8 @@ export const application = {
         token: '__auth__',
         userId: '__userid__'
     },
-    microservice: 'xui_approve_org',
-    idam_client: 'xui_approve_org',
+    microservice: 'xuiaowebapp',
+    idam_client: 'xuiaowebapp',
     oauth_callback_url: 'oauth2/callback',
     protocol: 'https'
 };
