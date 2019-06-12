0a2c280e80681c11f53124a3be404b0dc0baba9a
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-12 16:31:23 +0100

    EUI-198 initial work

--- src/org-pending/containers/index.ts
+++ src/org-pending/containers/index.ts
@@ -1,11 +1,13 @@
 import { OverviewComponent } from './overview/pending-overview.component';
 import { OrgPendingOverviewComponent } from './org-pending-overview/org-pending-overview.component';
 import { OrgPendingSummaryComponent } from './org-pending-summary/org-pending-summary.component';
+import { OrgPendingApproveComponent } from './org-pending-approve/org-pending-approve.component';
 
 export const containers: any[] = [
   OverviewComponent,
   OrgPendingSummaryComponent,
-  OrgPendingOverviewComponent
+  OrgPendingOverviewComponent,
+  OrgPendingApproveComponent
 ];
 
 export * from './overview/pending-overview.component';
