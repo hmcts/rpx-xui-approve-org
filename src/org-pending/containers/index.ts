2931a9574c74c3a4fc1018f3c6b1420d6aff125f
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-13 11:48:43 +0100

    Adding Org approval success

--- src/org-pending/containers/index.ts
+++ src/org-pending/containers/index.ts
@@ -2,12 +2,14 @@ import { OverviewComponent } from './overview/pending-overview.component';
 import { OrgPendingOverviewComponent } from './org-pending-overview/org-pending-overview.component';
 import { OrgPendingSummaryComponent } from './org-pending-summary/org-pending-summary.component';
 import { OrgPendingApproveComponent } from './org-pending-approve/org-pending-approve.component';
+import { OrgApprovalSuccessComponent } from './org-success/org-approval-success.component';
 
 export const containers: any[] = [
   OverviewComponent,
   OrgPendingSummaryComponent,
   OrgPendingOverviewComponent,
-  OrgPendingApproveComponent
+  OrgPendingApproveComponent,
+  OrgApprovalSuccessComponent
 ];
 
 export * from './overview/pending-overview.component';
