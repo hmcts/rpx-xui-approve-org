a65775a61224120ade1ab6fd0a5569ce5569478e
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-13 10:31:56 +0100

    EUI-198 success route

--- src/org-pending/org-pending.ts
+++ src/org-pending/org-pending.ts
@@ -24,6 +24,9 @@ export const ROUTES: Routes = [
       {
         path: 'approve',
         component: OrgPendingApproveComponent
+      },
+      {
+        path: 'approve-success'
       }
     ]
   },
