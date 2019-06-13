7258a28bc352be9194d714dcecf87713749b1871
toireasam <toireasa.moley@hmcts.net>
2019-06-13 12:40:56 +0100

    EUI-188 banner notification (#29)
    * First attempt at banner notification
    
    * Notification banner updated
    
    * Refactoring banner code
    
    * Working banner
    
    * Updating pending org
    
    * Call to http
    
    * Updating service
    
    * Updating service
    
    * Working banner
    
    * Updating service
    
    * Code refactor
    
    * Code refactor
    
    * Selector test
    
    * Removing unused actions
    
    * Updating actiosn
    
    * cleaning up the code;
    
    * cleaning up
    
    * fixing tests

--- src/org-manager/containers/overview/org-overview.component.ts
+++ src/org-manager/containers/overview/org-overview.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import * as fromOrganisationStore from '../../../org-manager/store';
+import * as fromOrganisationPendingStore from '../../../org-pending/store';
 import { GovukTableColumnConfig } from 'projects/gov-ui/src/lib/components/govuk-table/govuk-table.component';
 import {Observable} from 'rxjs';
 import { OrganisationSummary } from '../../models/organisation';
+
 @Component({
   selector: 'app-prd-org-overview-component',
   templateUrl: './org-overview.component.html',
@@ -11,16 +13,17 @@ import { OrganisationSummary } from '../../models/organisation';
 
 export class OverviewComponent implements OnInit {
   columnConfig: GovukTableColumnConfig[];
-  tableRows: {}[];
   orgs$: Observable<Array<OrganisationSummary>>;
   loading$: Observable<boolean>;
+  pendingOrgsCount$: Observable<number>;
 
-  constructor(private store: Store<fromOrganisationStore.OrganisationState>) {}
+  constructor(private store: Store<fromOrganisationStore.OrganisationState>,private pendingStore: Store<fromOrganisationPendingStore.PendingOrganisationState>) {}
 
   ngOnInit(): void {
     this.store.dispatch(new fromOrganisationStore.LoadOrganisation());
     this.orgs$ = this.store.pipe(select(fromOrganisationStore.organisations));
     this.loading$ = this.store.pipe(select(fromOrganisationStore.organisationsLoading));
+    // TODO tidy up (maybe add to the store or config file)
     this.columnConfig = [
       { header: 'Reference', key: 'organisationId', type: 'multi-column', multiColumnMapping: 'id',
       class: 'govuk-caption-m govuk-!-font-size-16'},
@@ -30,5 +33,9 @@ export class OverviewComponent implements OnInit {
       { header: 'Status', key: 'status', type: 'styled', class: 'hmcts-badge hmcts-badge--green'},
       { header: null, key: 'view', type: 'link' }
     ];
+
+    this.pendingStore.dispatch(new fromOrganisationPendingStore.LoadPendingOrganisations());
+    this.pendingOrgsCount$ = this.pendingStore.pipe(select(fromOrganisationPendingStore.pendingOrganisationsCount));
+
   }
 }
