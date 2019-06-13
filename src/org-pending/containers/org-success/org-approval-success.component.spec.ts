18fdffea01efe47df09532f54f55f453f0cb5521
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-13 13:14:07 +0100

    Adding Org approval success test

---
+++ src/org-pending/containers/org-success/org-approval-success.component.spec.ts
@@ -0,0 +1,8 @@
+import { OrgApprovalSuccessComponent } from './org-approval-success.component';
+
+describe('Org Approval Success Component', () => {
+    it('should have a component', () => {
+        const component = new OrgApprovalSuccessComponent();
+        expect(component).toBeTruthy();
+});
+});
