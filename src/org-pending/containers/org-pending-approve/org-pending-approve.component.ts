c7363ed86cc19549ebde3cc229d500cad758d98c
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-13 11:08:16 +0100

    EUI-198 template on approve

--- src/org-pending/containers/org-pending-approve/org-pending-approve.component.ts
+++ src/org-pending/containers/org-pending-approve/org-pending-approve.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as fromOrganisationPendingStore from '../../../org-pending/store';
 import * as fromRoot from '../../../app/store';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { PendingOrganisation } from 'src/org-pending/models/pending-organisation';
 
 @Component({
     selector: 'app-org-pending-approve',
     templateUrl: './org-pending-approve.component.html'
 })
-export class OrgPendingApproveComponent implements OnInit {
+export class OrgPendingApproveComponent implements OnInit, OnDestroy {
 
     reviewedOrgs$: any;
-    reviewedOrganisations: Observable<PendingOrganisation[]>;
+    reviewedOrganisations: PendingOrganisation[];
+    reviewedOrganisationsSubscription: Subscription;
 
     constructor(private store: Store<fromOrganisationPendingStore.PendingOrganisationState>) { }
 
     ngOnInit() {
         this.reviewedOrgs$ = this.store.pipe(select(fromOrganisationPendingStore.pendingOrganisations));
-        this.reviewedOrgs$.subscribe(response => {
-            console.log(response);
-            this.reviewedOrganisations = response.reviewedOrganisations;
+        this.reviewedOrganisationsSubscription = this.reviewedOrgs$.subscribe(response => {
+            console.log(response.reviewedOrganisations);
+            if (response.reviewedOrganisations) {
+                this.reviewedOrganisations = response.reviewedOrganisations;
+            } else {
+                this.onGoBack();
+            }
         });
     }
 
 
     onGoBack() {
         this.store.dispatch(new fromRoot.Back());
+    }
+
+    ngOnDestroy() {
+        this.reviewedOrganisationsSubscription.unsubscribe();
     }
 }
