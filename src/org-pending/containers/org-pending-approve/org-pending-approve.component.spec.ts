738177127d339e15aa09a9550d810fe80fbcde45
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-14 22:06:51 +0100

    EUI-198 finished tests for component

--- src/org-pending/containers/org-pending-approve/org-pending-approve.component.spec.ts
+++ src/org-pending/containers/org-pending-approve/org-pending-approve.component.spec.ts
@@ -3,6 +3,7 @@ import { OrgPendingApproveComponent } from './org-pending-approve.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import * as fromOrganisationPendingStore from '../../../org-pending/store';
 import * as fromRoot from '../../../app/store/reducers';
+import * as fromRootActions from '../../../app/store/actions';
 import { StoreModule, Store, combineReducers } from '@ngrx/store';
 import { of } from 'rxjs';
 
@@ -18,10 +19,12 @@ const reviewedOrganisationsDummy = [{
     email: 'dummy 1'
 }];
 
-fdescribe('OrgPendingApproveComponent', () => {
+describe('OrgPendingApproveComponent', () => {
     let component: OrgPendingApproveComponent;
     let fixture: ComponentFixture<OrgPendingApproveComponent>;
     let store: Store<fromOrganisationPendingStore.PendingOrganisationState>;
+    let storePipeMock: any;
+    let storeDispatchMock: any;
 
     beforeEach((() => {
         TestBed.configureTestingModule({
@@ -38,15 +41,33 @@ fdescribe('OrgPendingApproveComponent', () => {
         }).compileComponents();
         store = TestBed.get(Store);
 
-        spyOn(store, 'pipe').and.returnValue(of({reviewedOrganisations: reviewedOrganisationsDummy}));
+        storePipeMock = spyOn(store, 'pipe');
+        storeDispatchMock = spyOn(store, 'dispatch');
 
         fixture = TestBed.createComponent(OrgPendingApproveComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     }));
 
     it('should return reviewed organisations', () => {
+        storePipeMock.and.returnValue(of({reviewedOrganisations: reviewedOrganisationsDummy}));
+        fixture.detectChanges();
         expect(component.reviewedOrganisations).toEqual(reviewedOrganisationsDummy);
+    });
+
+    it('should dispatch a router "back" action when there are no reviewed organisations', () => {
+        storePipeMock.and.returnValue(of({reviewedOrganisations: null}));
+        fixture.detectChanges();
+        expect(storeDispatchMock).toHaveBeenCalledWith(new fromRootActions.Back());
+    });
+
+    it('should dispatch a pending organisation "approve" action when reviewed organisations are approved', () => {
+        storePipeMock.and.returnValue(of({reviewedOrganisations: reviewedOrganisationsDummy}));
+        fixture.detectChanges();
+        component.onApproveOrganisations();
+        fixture.detectChanges();
+        expect(storeDispatchMock).toHaveBeenCalledWith(
+            new fromOrganisationPendingStore.ApprovePendingOrganisations(reviewedOrganisationsDummy)
+        );
     });
 
 });
