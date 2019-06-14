0ce712e8af57be2debb8c7184debf9b95c7157a7
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-14 23:01:58 +0100

    EUI-198 reducer tests

--- src/org-pending/store/reducers/org-pending.reducer.spec.ts
+++ src/org-pending/store/reducers/org-pending.reducer.spec.ts
@@ -1,7 +1,36 @@
 import { initialState, reducer } from './org-pending.reducer';
-import { LoadPendingOrganisationsSuccess } from '../actions';
+import { LoadPendingOrganisationsSuccess, AddReviewOrganisations, LoadPendingOrganisations } from '../actions';
 import { PendingOrganisation, PendingOrganisationSummary } from 'src/org-pending/models/pending-organisation';
 import * as fromPendingOrganisation from './org-pending.reducer';
+
+const PendingOrganisationsMock: PendingOrganisation[] = [
+  {
+    name: 'Glen Byrne',
+    organisationId: 'Byrne Limited',
+    address: '13 Berryfield drive, Finglas',
+    pbaNumber: '101010',
+    admin: 'Glen Byrne',
+    status: 'PENDING',
+    view: 'View',
+    id: '2424242',
+    email: 'glen@byrne.com'
+  }
+];
+
+const PendingOrganisationsMockSummary: PendingOrganisationSummary[] = [
+  {
+    name: 'Glen Byrne',
+    organisationId: 'Byrne Limited',
+    address: '13 Berryfield drive, Finglas',
+    pbaNumber: '101010',
+    admin: 'Glen Byrne',
+    status: 'PENDING',
+    view: 'View',
+    id: '2424242',
+    email: 'glen@byrne.com',
+    routerLink: '/pending-organisations/organisation/101010/'
+  }
+];
 
 describe('PendingOrganisationsReducer', () => {
   describe('undefined action', () => {
@@ -12,57 +41,57 @@ describe('PendingOrganisationsReducer', () => {
     });
   });
 
+  describe('LOAD_PENDING_ORGANISATION action', () => {
+    it('should return the initial state.pendingOrganisations', () => {
+
+      const action = new LoadPendingOrganisations();
+      const state = reducer(initialState, action);
+      expect(state.reviewedOrganisations).toEqual(initialState.pendingOrganisations);
+    });
+
+  });
+
   describe('LOAD_PENDING_ORGANISATION_SUCCESS action', () => {
     it('should update the state.pendingOrganisations', () => {
-      const PendingOrganisationsMock: PendingOrganisation[] =
-        [{
-          name: 'Glen Byrne',
-          organisationId: 'Byrne Limited',
-          address: '13 Berryfield drive, Finglas',
-          pbaNumber: '101010',
-          admin: 'Glen Byrne',
-          status: 'PENDING',
-          view: 'View',
-          id: '2424242',
-          email: 'glen@byrne.com'
-        }];
-
-      const PendingOrganisationsMockSummary: PendingOrganisationSummary[] = [
-        {
-          name: 'Glen Byrne',
-          organisationId: 'Byrne Limited',
-          address: '13 Berryfield drive, Finglas',
-          pbaNumber: '101010',
-          admin: 'Glen Byrne',
-          status: 'PENDING',
-          view: 'View',
-          id: '2424242',
-          email: 'glen@byrne.com',
-          routerLink: '/pending-organisations/organisation/101010/'
-        }
-      ];
 
       const action = new LoadPendingOrganisationsSuccess(PendingOrganisationsMock);
       const state = reducer(initialState, action);
       expect(state.pendingOrganisations).toEqual(PendingOrganisationsMockSummary);
     });
 
-    describe('getPendingOrganisations export', () => {
-      it('should return state.pendingOrganisations', () => {
-        expect(fromPendingOrganisation.getPendingOrganisations(initialState)).toEqual(null);
-        expect();
-      });
+  });
 
-      describe('getPendingOrganisationsLoading export', () => {
-        it('should return state.loading', () => {
-          expect(fromPendingOrganisation.getPendingOrganisationsLoading(initialState)).toEqual(false);
-        });
-      });
+  describe('ADD_REVIEW_ORGANISATIONS action', () => {
+    it('should update the state.reviewedOrganisations', () => {
 
-      describe('getPendingOrganisationsLoaded export', () => {
-        it('should return state.loaded', () => {
-          expect(fromPendingOrganisation.getPendingOrganisationsLoaded(initialState)).toEqual(false);
-        });
+      const action = new AddReviewOrganisations(PendingOrganisationsMock);
+      const state = reducer(initialState, action);
+      expect(state.reviewedOrganisations).toEqual(PendingOrganisationsMock);
+    });
+
+  });
+
+  describe('exports', () => {
+    it('should return state.pendingOrganisations', () => {
+      expect(fromPendingOrganisation.getPendingOrganisations(initialState)).toEqual(null);
+      expect();
+    });
+
+    describe('getPendingOrganisationsLoading export', () => {
+      it('should return state.loading', () => {
+        expect(fromPendingOrganisation.getPendingOrganisationsLoading(initialState)).toEqual(false);
+      });
+    });
+
+    describe('getPendingOrganisationsLoaded export', () => {
+      it('should return state.loaded', () => {
+        expect(fromPendingOrganisation.getPendingOrganisationsLoaded(initialState)).toEqual(false);
+      });
+    });
+
+    describe('getReviewedOrganisations export', () => {
+      it('should return state.loaded', () => {
+        expect(fromPendingOrganisation.getReviewedOrganisations(initialState)).toEqual(null);
       });
     });
   });
