f8262e43d60f10f6cb8894053a111d00b689b345
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-14 23:34:43 +0100

    EUI-198 effects tests

--- src/org-pending/store/effects/org-pending-effects.spec.ts
+++ src/org-pending/store/effects/org-pending-effects.spec.ts
@@ -5,16 +5,30 @@ import { of, throwError } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
 import * as fromPendingOrganisationEffects from './org-pending.effects';
 import { PendingOrgEffects } from './org-pending.effects';
-import { LoadPendingOrganisations } from '../actions/org-pending.actions';
+import { LoadPendingOrganisations, ApprovePendingOrganisations, ApprovePendingOrganisationsSuccess, ApprovePendingOrganisationsFail } from '../actions/org-pending.actions';
 import { LoadPendingOrganisationsSuccess, LoadPendingOrganisationsFail } from '../actions';
 import { PendingOrganisationService } from 'src/org-pending/services';
 import { PendingOrganisation } from '../../models/pending-organisation';
+import { Go } from 'src/app/store';
+
+const payload: PendingOrganisation[] = [{
+  name: 'Glen Byrne',
+  organisationId: 'Byrne Limited',
+  address: '13 Berryfield drive, Finglas',
+  pbaNumber: '101010',
+  admin: 'Glen Byrne',
+  status: 'PENDING',
+  view: 'View',
+  id: '2424242',
+  email: 'glen@byrne.com'
+}];
 
 describe('Pending Organisation Effects', () => {
   let actions$;
   let effects: PendingOrgEffects;
   const PendingOrganisationServiceMock = jasmine.createSpyObj('PendingOrganisationService', [
     'fetchPendingOrganisations',
+    'approvePendingOrganisations'
   ]);
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -33,19 +47,8 @@ describe('Pending Organisation Effects', () => {
 
   });
 
-  describe('loadPendingOrganisationss$', () => {
+  describe('loadPendingOrganisations$', () => {
     it('should return a collection from loadPendingOrgs$ - LoadPendingOrganisationsSuccess', () => {
-      const payload: PendingOrganisation[] = [{
-        name: 'Glen Byrne',
-        organisationId: 'Byrne Limited',
-        address: '13 Berryfield drive, Finglas',
-        pbaNumber: '101010',
-        admin: 'Glen Byrne',
-        status: 'PENDING',
-        view: 'View',
-        id: '2424242',
-        email: 'glen@byrne.com'
-      }];
 
       PendingOrganisationServiceMock.fetchPendingOrganisations.and.returnValue(of(payload));
       const action = new LoadPendingOrganisations();
@@ -64,6 +67,35 @@ describe('Pending Organisation Effects', () => {
       actions$ = hot('-a', { a: action });
       const expected = cold('-b', { b: completion });
       expect(effects.loadPendingOrgs$).toBeObservable(expected);
+    });
+  });
+
+  describe('approvPendingOrganisations$', () => {
+    it('should return a collection from approvePendingOrgs$ - ApprovePendingOrganisationsSuccess', () => {
+
+      PendingOrganisationServiceMock.approvePendingOrganisations.and.returnValue(of(true));
+      const action = new ApprovePendingOrganisations(payload);
+      const completion = new ApprovePendingOrganisationsSuccess(true);
+      actions$ = hot('-a', { a: action });
+      const expected = cold('-b', { b: completion });
+      expect(effects.approvePendingOrgs$).toBeObservable(expected);
+
+      const successAction = new ApprovePendingOrganisationsSuccess(true);
+      const successCompletion = new Go({ path: ['pending-organisations/approve-success'] });
+      actions$ = hot('-a', { a: successAction });
+      const successExpected = cold('--b', { b: successCompletion });
+      expect(effects.approvePendingOrgsSuccess$).toBeObservable(successExpected);
+    });
+  });
+
+  describe('approvPendingOrganisations$ error', () => {
+    it('should return ApprovePendingOrganisationsOrganisationsFail', () => {
+      PendingOrganisationServiceMock.approvePendingOrganisations.and.returnValue(throwError(new Error()));
+      const action = new ApprovePendingOrganisations(payload);
+      const completion = new ApprovePendingOrganisationsFail(new Error);
+      actions$ = hot('-a', { a: action });
+      const expected = cold('-b', { b: completion });
+      expect(effects.approvePendingOrgs$).toBeObservable(expected);
     });
   });
 });
