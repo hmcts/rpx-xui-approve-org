fc75454d349a0130184f7592a4cc54e7331ad2e8
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-17 11:07:40 +0100

    EUI-198 removed success component

--- src/org-pending/containers/index.ts
+++ src/org-pending/containers/index.ts
@@ -2,14 +2,12 @@ import { OverviewComponent } from './overview/pending-overview.component';
 import { OrgPendingOverviewComponent } from './org-pending-overview/org-pending-overview.component';
 import { OrgPendingSummaryComponent } from './org-pending-summary/org-pending-summary.component';
 import { OrgPendingApproveComponent } from './org-pending-approve/org-pending-approve.component';
-import { OrgApprovalSuccessComponent } from './org-success/org-approval-success.component';
 
 export const containers: any[] = [
   OverviewComponent,
   OrgPendingSummaryComponent,
   OrgPendingOverviewComponent,
-  OrgPendingApproveComponent,
-  OrgApprovalSuccessComponent
+  OrgPendingApproveComponent
 ];
 
 export * from './overview/pending-overview.component';
