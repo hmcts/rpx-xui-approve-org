63ce9b4a4e33d00f1dc57fcb916ffcfa4074a0c0
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2019-06-18 16:38:47 +0100

    move footer content into separate file, remove unused varibales in tests

--- src/app/components/hmcts-global-footer/hmcts-global-footer.component.spec.ts
+++ src/app/components/hmcts-global-footer/hmcts-global-footer.component.spec.ts
@@ -6,7 +6,7 @@ import {Helper, Navigation} from '../../containers/footer/footer.model';
 
 describe('HmctsGlobalFooterComponent', () => {
     @Component({
-        selector: `exui-app-host-dummy-component`,
+        selector: `app-host-dummy-component`,
         template: `<app-hmcts-global-footer
                     [reference]="iconFallbackText"
                     [title]="type"
@@ -18,10 +18,7 @@ describe('HmctsGlobalFooterComponent', () => {
         @ViewChild(HmctsGlobalFooterComponent)
         public hmctsGlobalFooterComponent: HmctsGlobalFooterComponent;
     }
-    let testHostComponent: TestDummyHostComponent;
-    let testHostFixture: ComponentFixture<TestDummyHostComponent>;
-    let el: DebugElement;
-    let de: any;
+
     let component: HmctsGlobalFooterComponent;
     let fixture: ComponentFixture<HmctsGlobalFooterComponent>;
 
--- src/app/containers/footer/footer.component.spec.ts
+++ src/app/containers/footer/footer.component.spec.ts
@@ -5,7 +5,7 @@ import {FooterComponent} from './footer.component';
 
 describe('FooterComponent', () => {
     @Component({
-        selector: `exui-app-host-dummy-component`,
+        selector: `app-host-dummy-component`,
         template: `<app-footer></app-footer>`
     })
     class TestDummyHostComponent {
@@ -15,8 +15,6 @@ describe('FooterComponent', () => {
 
     let testHostComponent: TestDummyHostComponent;
     let testHostFixture: ComponentFixture<TestDummyHostComponent>;
-    let el: DebugElement;
-    let de: any;
     let component: FooterComponent;
     let fixture: ComponentFixture<FooterComponent>;
     let element: DebugElement;
