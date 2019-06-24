0f8703d0890d9996f919a666ccb90fc07bc96966
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2019-06-24 14:49:48 +0100

    EUI-309_bugfix_EUI-198_test_automation (#45)

--- test/e2e/config/smoke.conf.js
+++ test/e2e/config/smoke.conf.js
@@ -6,17 +6,6 @@ chai.use(chaiAsPromised);
 
 const argv = minimist(process.argv.slice(2));
 
-//const specFilesFilter = ['../features/**/*.feature'];
-
-// module.exports = {
-//   chai: chai,
-//   chaiAsPromised: chaiAsPromised,
-//   minimist: minimist,
-//   argv: argv,
-//   specFilesFilter: specFilesFilter
-// }
-//
-
 const jenkinsConfig = [
 
   {
@@ -47,14 +36,6 @@ const config = {
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
   specs: ['../features/**/*.feature'],
-  // specs: [
-  //   '../features/**/caseFile.feature',
-  //   '../features/**/login.feature',
-  //   '../features/**/makeDecision.feature',
-  //   '../features/**/parties.feature',
-  //   '../features/**/questions.feature',
-  //   '../features/**/recentEvents.feature',
-  // ],
   baseUrl: process.env.TEST_URL || 'http://localhost:2500/',
   params: {
     serverUrls: process.env.TEST_URL || 'http://localhost:2500/',
@@ -101,7 +82,7 @@ const config = {
       options: {
         automaticallyGenerateReport: true,
         removeExistingJsonReportFile: true,
-        reportName: 'JUI Functional Tests',
+        reportName: 'Approve Organisation Functional Tests',
         // openReportInBrowser: true,
         jsonDir: 'reports/smoke_tests/functional',
         reportPath: 'reports/smoke_tests/functional'
