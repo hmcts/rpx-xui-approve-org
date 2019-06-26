87089832e2eab7344b1285721ce8bc0c94425182
Toireasa Moley <toireasa.moley@hmcts.net>
2019-06-26 16:02:23 +0100

    Updating modules

--- src/org-pending/org-pending.ts
+++ src/org-pending/org-pending.ts
@@ -1,9 +1,9 @@
 // routes
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
-import { OverviewComponent } from 'src/org-manager/containers/overview-pending/pending-overview.component';
+import { OverviewPendingComponent } from 'src/org-manager/containers/overview-pending/pending-overview.component';
 import { OrgPendingOverviewComponent } from 'src/org-manager/containers/org-pending-overview/org-pending-overview.component';
-import { OrgPendingSummaryComponent } from './containers/org-pending-summary/org-pending-summary.component';
+import { OrgPendingSummaryComponent } from 'src/org-manager/containers/org-pending-summary/org-pending-summary.component';
 import { OrgPendingApproveComponent } from 'src/org-manager/containers/org-pending-approve/org-pending-approve.component';
 import { OrgApprovalSuccessComponent } from 'src/org-manager/containers/org-success/org-approval-success.component';
 
@@ -16,7 +16,7 @@ export const ROUTES: Routes = [
     children: [
       {
         path: '',
-        component: OverviewComponent,
+        component: OverviewPendingComponent,
       },
       {
         path: 'organisation/:id',
