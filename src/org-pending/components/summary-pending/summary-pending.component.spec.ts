299ba078857c07d4991a5216f34ce3dab124e678
Toireasa Moley <toireasa.moley@hmcts.net>
2019-06-26 15:16:17 +0100

    Changing summary component

--- src/org-pending/components/summary-pending/summary-pending.component.spec.ts
+++ src/org-pending/components/summary-pending/summary-pending.component.spec.ts
@@ -1,8 +1,8 @@
-import { SummaryComponent } from './summary.component';
+import { SummaryPendingComponent } from './summary-pending.component';
 
 describe('Summary Component', () => {
     it('should have a component', () => {
-        const component = new SummaryComponent();
+        const component = new SummaryPendingComponent();
         expect(component).toBeTruthy();
 });
 });
