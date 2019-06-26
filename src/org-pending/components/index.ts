a4c4a3a73277baff566c5e1df03ac97681bf20f1
Toireasa Moley <toireasa.moley@hmcts.net>
2019-06-26 16:16:30 +0100

    Moving accross files

--- src/org-pending/components/index.ts
+++
@@ -1,7 +0,0 @@
-import { SummaryPendingComponent } from './summary-pending/summary-pending.component';
-
-export const components: any[] = [
-    SummaryPendingComponent
-  ];
-
-  export * from './summary-pending/summary-pending.component';
