0847c1eee6a1b012a186c2eac87262d7173432c7
Uday Denduluri <uday.denduluri@gmail.com>
2019-07-03 15:43:29 +0100

    Organisations

--- src/org-manager/models/organisation.ts
+++ src/org-manager/models/organisation.ts
@@ -1,15 +1,37 @@
-export interface  Organisation {
-  organisationId: string;
-  pbaNumber: string;
-  address: string;
-  admin: string;
-  status: string;
-  view: string;
-  id: string;
-  email: string;
+export interface OrganisationAddress {
+  addressLine1: string
+  townCity: string
+  county: string
+  dxAddress: [OrganisationDxAddress]
+  }
+
+export interface OrganisationDxAddress {
+      dxNumber: string,
+      dxExchange: string
+  }
+
+export interface OrganisationSuperUser {
+  userIdentifier: string
+  firstName: string,
+  lastName: string,
+  email: string
 }
+
+export interface  Organisation {
+  organisationIdentifier: string
+  contactInformation: [OrganisationAddress]
+  superUser: OrganisationSuperUser
+  status: string
+  name: string
+}
+
 export interface OrganisationSummary extends Organisation {
   routerLink: string;
 }
 
-
+export class organisationVM {
+  organisationId: string;
+  status: string;
+  admin: string;
+  address: string;
+}
--- src/org-manager/services/organisation.service.ts
+++ src/org-manager/services/organisation.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, of, throwError, noop, pipe } from 'rxjs';
 import { OrganisationsMock } from '../mock/organisation.mock';
-import { Organisation } from '../models/organisation';
+import { Organisation, organisationVM } from '../models/organisation';
 import {SingleOrgSummary} from '../models/single-org-summary';
 import {SingleOrgSummaryMock} from '../mock/single-org-summary.mock';
-import { tap, catchError } from 'rxjs/operators';
+import { tap, catchError, map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable()
 export class OrganisationService {
   private singleOrgUrl = environment.singleOrgUrl;
+  private orgActiveUrl = environment.orgActiveUrl;
   constructor(private http: HttpClient) {
   }
 
-  fetchOrganisations(): Observable<Array<Organisation>> {
+  fetchOrganisations(): Observable<Array<organisationVM>> {
     const obj: Organisation[] = OrganisationsMock;
-    return of(obj);
+
+    let organisationModel: organisationVM[] = []
+    obj.forEach((curr) => {
+      let organisation = new organisationVM()
+      organisation.organisationId = curr.organisationIdentifier
+      organisation.status = curr.status
+      organisation.admin = `${curr.superUser.firstName} ${curr.superUser.lastName}`
+      organisation.address = `${curr.contactInformation[0].addressLine1}, ${curr.contactInformation[0].county}, ${curr.contactInformation[0].townCity}`
+      organisationModel.push(organisation);
+    });
+
+    return of(organisationModel);
   }
 
   getSingleOrganisation(payload): Observable<SingleOrgSummary> {
