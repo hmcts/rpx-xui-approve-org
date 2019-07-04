8013d9bee2840cd6a412f75a892cd78ca7a3b438
Uday Denduluri <uday.denduluri@gmail.com>
2019-07-04 13:49:09 +0100

    Config changes for lDocker

--- src/org-manager/org-manager.routing.ts
+++ src/org-manager/org-manager.routing.ts
@@ -9,12 +9,13 @@ import { OrgPendingOverviewComponent } from 'src/org-manager/containers/org-pend
 import { OrgPendingSummaryComponent } from 'src/org-manager/containers/org-pending-summary/org-pending-summary.component';
 import { OrgPendingApproveComponent } from 'src/org-manager/containers/org-pending-approve/org-pending-approve.component';
 import { OrgApprovalSuccessComponent } from 'src/org-manager/containers/org-success/org-approval-success.component';
+import { AuthGuard } from 'src/services/auth/auth.guard';
 
 export const ROUTES: Routes = [
   {
     path: '',
     component: OverviewComponent,
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'organisations/organisation',
