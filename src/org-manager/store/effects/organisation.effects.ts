78f1e405745e8d1ef60fd01a1e13e8e270c95b7a
Uday Denduluri <uday.denduluri@gmail.com>
2019-07-04 16:36:29 +0100

    Linter errors

--- src/org-manager/store/effects/organisation.effects.ts
+++ src/org-manager/store/effects/organisation.effects.ts
@@ -21,28 +21,28 @@ export class OrganisationEffects {
     ofType(organisationActions.LOAD_ORGANISATIONS),
     switchMap(() => {
       return this.organisationService.fetchOrganisations().pipe(
-        
         map(organisationDetails => new organisationActions.LoadOrganisationSuccess(this.mapOrganisations(organisationDetails))),
         catchError(error => of(new organisationActions.LoadOrganisationFail(error)))
       );
-    })
+    });
   )
 
-  mapOrganisations(obj: Organisation[]) : OrganisationVM[] {
-    let organisationModel: OrganisationVM[] = []
+  mapOrganisations(obj: Organisation[]): OrganisationVM[] {
+    const organisationModel: OrganisationVM[] = [];
     obj.forEach((apiOrg) => {
-      const organisation = new OrganisationVM()
-      organisation.name = apiOrg.name
-      organisation.adminEmail = apiOrg.superUser.email
-      organisation.pbaNumber = apiOrg.paymentAccount
-      organisation.organisationId = apiOrg.organisationIdentifier
-      organisation.view = "View"
-      organisation.status = apiOrg.status
-      organisation.admin = `${apiOrg.superUser.firstName} ${apiOrg.superUser.lastName}`
-      organisation.dxNumber = apiOrg.contactInformation[0].dxAddress
-      organisation.address = `${apiOrg.contactInformation[0].addressLine1}, ${apiOrg.contactInformation[0].county}, ${apiOrg.contactInformation[0].townCity}`
-      organisationModel.push(organisation)
-    })
+      const organisation = new OrganisationVM();
+      organisation.name = apiOrg.name;
+      organisation.adminEmail = apiOrg.superUser.email;
+      organisation.pbaNumber = apiOrg.paymentAccount;
+      organisation.organisationId = apiOrg.organisationIdentifier;
+      organisation.view = 'View';
+      organisation.status = apiOrg.status;
+      organisation.admin = `${apiOrg.superUser.firstName} ${apiOrg.superUser.lastName}`;
+      organisation.dxNumber = apiOrg.contactInformation[0].dxAddress;
+      organisation.address = `${apiOrg.contactInformation[0].addressLine1}, ${apiOrg.contactInformation[0].county},
+      ${apiOrg.contactInformation[0].townCity}`;
+      organisationModel.push(organisation);
+    });
 
     return organisationModel;
   }
