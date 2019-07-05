295615e47c1bde8d562eda9bf2a8cd695ea88303
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-05 17:07:42 +0100

    EUI-47 unfinished

---
+++ src/app/store/effects/app.effects.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+
+import * as appActions from '../actions';
+import { map } from 'rxjs/operators';
+
+// import { CookieService } from 'ngx-cookie';
+
+@Injectable()
+export class AppEffects {
+    constructor(
+        private actions$: Actions,
+        // private cookieService: CookieService
+    ) { }
+
+    @Effect({ dispatch: false })
+    logout$ = this.actions$.pipe(
+        ofType(appActions.LOGOUT),
+        map(() => {
+            window.location.href = '/api/logout';
+            // TODO: shouldn't need to clear cookies here
+            // this.cookieService.removeAll();
+        })
+    );
+
+}
