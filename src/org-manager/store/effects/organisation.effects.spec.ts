880760e4cd102ee295521e7e95ea2405fd060c9c
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-08 13:46:51 +0100

    EUI-47 added mixing code in api and fixed legacy testing code

--- src/org-manager/store/effects/organisation.effects.spec.ts
+++ src/org-manager/store/effects/organisation.effects.spec.ts
@@ -8,6 +8,7 @@ import { OrganisationEffects } from './organisation.effects';
 import { LoadOrganisation, LoadOrganisationFail } from '../actions/organisation.actions';
 import { LoadOrganisationSuccess } from '../actions';
 import { OrganisationService } from 'src/org-manager/services';
+import { Organisation } from 'src/org-manager/models/organisation';
 
 describe('Organisation Effects', () => {
   let actions$;
@@ -19,17 +20,50 @@ describe('Organisation Effects', () => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
-          {
-            provide: OrganisationService,
-            useValue: OrganisationServiceMock,
-          },
-          fromOrganisationEffects.OrganisationEffects,
-          provideMockActions(() => actions$)
+        {
+          provide: OrganisationService,
+          useValue: OrganisationServiceMock,
+        },
+        fromOrganisationEffects.OrganisationEffects,
+        provideMockActions(() => actions$)
       ]
     });
 
     effects = TestBed.get(OrganisationEffects);
 
+  });
+  describe('loadOrganisationss$', () => {
+    it('should return a collection from loadOrganisations$ - LoadOrganisationsSuccess', () => {
+      const organisation: Organisation = {
+        organisationIdentifier: '',
+        contactInformation: [{
+          addressLine1: '',
+          townCity: 'string',
+          county: 'string',
+          dxAddress: [{
+            dxNumber: 'string',
+            dxExchange: 'string',
+          }],
+        }],
+        superUser: {
+          userIdentifier: '',
+          firstName: 'string',
+          lastName: 'string;',
+          email: 'string',
+        },
+        status: 'string;',
+        name: 'string;',
+        paymentAccount: [{}]
+      };
+      const payload = [organisation];
+      const result = effects.mapOrganisations([organisation]);
+      OrganisationServiceMock.fetchOrganisations.and.returnValue(of(payload));
+      const action = new LoadOrganisation();
+      const completion = new LoadOrganisationSuccess(result);
+      actions$ = hot('-a', { a: action });
+      const expected = cold('-b', { b: completion });
+      expect(effects.loadOrganisations$).toBeObservable(expected);
+    });
   });
 
   describe('loadOrganisations$ error', () => {
