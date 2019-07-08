f2b9f6428324a89523727776a7a1783bc4464670
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-08 16:43:51 +0100

    EUI-47 added

--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -4,10 +4,13 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import * as appActions from '../actions';
 import { map } from 'rxjs/operators';
 
+import { CookieService } from 'ngx-cookie';
+
 @Injectable()
 export class AppEffects {
     constructor(
-        private actions$: Actions
+        private actions$: Actions,
+        private cookieService: CookieService
     ) { }
 
     @Effect({ dispatch: false })
@@ -15,6 +18,8 @@ export class AppEffects {
         ofType(appActions.LOGOUT),
         map(() => {
             window.location.href = '/api/logout';
+            // TODO: shouldn't need to clear cookies here
+            this.cookieService.removeAll();
         })
     );
 
