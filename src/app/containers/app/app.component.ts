953e13ada0439bf626dead690cfe0603df199e2d
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-10 12:20:11 +0100

    EUI-47 trial

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -24,8 +24,7 @@ export class AppComponent implements OnInit {
     // this.identityBar$ = this.store.pipe(select(fromSingleFeeAccountStore.getSingleFeeAccountData));
     this.title$ = this.store.pipe(select(fromRoot.getAppPageTitle));
     this.store.pipe(select(fromRoot.getRouterState)).subscribe(rootState => {
-      if (rootState) {
-        console.log(JSON.stringify(rootState));
+      if (rootState.state) {
         this.store.dispatch(new fromRoot.SetPageTitle(rootState.state.url));
       }
     });
