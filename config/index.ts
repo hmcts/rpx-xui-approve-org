ecc118c6fe3b57a4413e0ad1baabf2ac8c556e84
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-10 16:49:30 +0100

    Revert "EUI-47 deleted config"
    This reverts commit 82852db1d1f4ce5840b6e680fed0d5b64389ac17.

---
+++ config/index.ts
@@ -0,0 +1,34 @@
+import { application } from './application.config';
+
+import * as local from './environments/local.config';
+import * as docker from './environments/docker.config';
+import * as preview from './environments/preview.config';
+import * as aat from './environments/aat.config';
+import * as prod from './environments/prod.config';
+import * as process from 'process';
+
+const configs = {
+    local,
+    docker,
+    preview,
+    aat,
+    prod,
+    microservice: 'xuiaowebapp',
+    idam_client: 'xuiaowebapp',
+    oauth_callback_url: 'oauth2/callback',
+    protocol: 'https'
+};
+
+export const configEnv = process ? process.env.PUI_ENV || 'local' : 'local';
+export const config = { ...application, ...configs[configEnv].default };
+
+if (process) {
+    console.log('PROCESSS')
+    config.appInsightsInstrumentationKey =
+        process.env.APPINSIGHTS_INSTRUMENTATIONKEY || 'AAAAAAAAAAAAAAAA';
+}
+
+if (configEnv === 'local') {
+    config.protocol = 'http';
+}
+
