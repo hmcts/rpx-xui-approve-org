3c6de72716dddbd4e47d5e45d6ee1aaed6eee63f
Toireasa Moley <toireasa.moley@hmcts.net>
2019-07-10 15:02:38 +0100

    Updating config

--- config/environments/ithc.config.ts
+++ config/environments/ithc.config.ts
@@ -1,11 +1,18 @@
 export default {
     services: {
-        idam_web: 'https://idam.preprod.ccidam.reform.hmcts.net',
-        idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
-        s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
+        ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal',
+        ccdDefApi: 'https://ccd-definition-store-api-aat.service.core-compute-aat.internal',
+        idamWeb: 'https://idam-web-public.aat.platform.hmcts.net',
+        idamApi: 'https://idam-api.aat.platform.hmcts.net',
+        s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
+        draftStoreApi: 'https://draft-store-service-demo.service.core-compute-demo.internal',
+        dmStoreApi: 'https://dm-store-demo.service.core-compute-demo.internal',
+        emAnnoApi: 'https://em-anno-demo.service.core-compute-demo.internal',
+        emNpaApi: 'https://em-npa-demo.service.core-compute-demo.internal',
+        rdProfessionalApi: 'https://rpa-rd-professional-aat.service.core-compute-aat.internal',
     },
     useProxy: false,
     secureCookie: false,
     sessionSecret: 'secretSauce',
-    logging: 'debug'
-};
+    logging: 'debug',
+}
