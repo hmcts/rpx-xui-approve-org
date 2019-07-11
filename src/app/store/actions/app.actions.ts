f129e20622b2275d2bae01fc1b36c97b14990709
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-11 12:01:27 +0100

    EUI-47 another attempt at build

--- src/app/store/actions/app.actions.ts
+++ src/app/store/actions/app.actions.ts
@@ -3,6 +3,8 @@ import { Action } from '@ngrx/store';
 export const SET_PAGE_TITLE = '[APP] Set Page Title';
 export const SET_PAGE_TITLE_ERRORS = '[APP] Set Page Title Errors';
 
+export const LOGOUT = '[App] Logout';
+
 export class SetPageTitle implements Action {
   readonly type = SET_PAGE_TITLE;
   constructor(public payload: string) {}
@@ -12,6 +14,11 @@ export class SetPageTitleErrors implements Action {
   readonly type = SET_PAGE_TITLE_ERRORS;
 }
 
+export class Logout implements Action {
+  readonly type = LOGOUT;
+}
+
 export type appActions =
   | SetPageTitle
-  | SetPageTitleErrors;
+  | SetPageTitleErrors
+  | Logout;
