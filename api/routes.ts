2f1e32d4177288a9f5fc9df4fd7b2b565cbdca37
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-11 15:49:29 +0100

    EUI-47 trying to get the PR build to pass

--- api/routes.ts
+++ api/routes.ts
@@ -1,8 +1,11 @@
 import * as express from 'express'
+import * as auth from './auth'
 import organisationRouter from './organisation'
 import stateRouter from './states'
 
 const router = express.Router({ mergeParams: true })
+
+router.use(auth.attach)
 
 router.use('/decisions', stateRouter)
 router.use('/organisations', organisationRouter)
