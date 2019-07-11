62055a4922cb895f409600c59f7a799ffcf006ad
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-11 12:01:27 +0100

    EUI-47 another attempt at build

--- api/server.ts
+++ api/server.ts
@@ -49,6 +49,8 @@ app.use(cookieParser())
 
 app.get('/oauth2/callback', auth.oauth)
 
+app.use(auth.attach)
+app.use('/api', routes)
 
 app.use('/*', (req, res) => {
     console.time(`GET: ${req.originalUrl}`)
@@ -62,9 +64,5 @@ app.use('/*', (req, res) => {
     })
     console.timeEnd(`GET: ${req.originalUrl}`)
 })
-
-
-app.use(auth.attach)
-app.use('/api', routes)
 
 app.listen(process.env.PORT || 3000)
