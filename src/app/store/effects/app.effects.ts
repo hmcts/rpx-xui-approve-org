79ecad59dda318b11c82a34331a656102738fa63
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-12 09:20:32 +0100

    EUI-47 added manual cookie removal

--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -3,17 +3,21 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 
 import * as appActions from '../actions';
 import { map } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie';
 
 @Injectable()
 export class AppEffects {
     constructor(
-        private actions$: Actions
+        private actions$: Actions,
+        private cookieService: CookieService,
     ) { }
 
     @Effect({ dispatch: false })
     logout$ = this.actions$.pipe(
         ofType(appActions.LOGOUT),
         map(() => {
+            // TODO: shouldn't need to clear cookies here
+            this.cookieService.removeAll();
             window.location.href = '/api/logout';
         })
     );
