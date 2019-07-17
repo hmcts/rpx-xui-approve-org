abbb6e88a7a0acda8f71f39277b406dc7f885e1d
Uday Denduluri <uday.denduluri@gmail.com>
2019-07-17 12:06:07 +0100

    Appinsights changes

---
+++ api/monitoring-tools/index.ts
@@ -0,0 +1,25 @@
+import * as express from 'express'
+import { config } from '../lib/config'
+import * as log4jui from '../lib/log4jui'
+
+const logger = log4jui.getLogger('service-token')
+
+async function handleInstrumentationKeyRoute(req, res) {
+    try {
+        console.log('config.appInsightsInstrumentationKey is ' + config.appInsightsInstrumentationKey)
+        res.send({key: config.appInsightsInstrumentationKey})
+    } catch (error) {
+        const errReport = JSON.stringify({
+            apiError: error,
+            apiStatusCode: error.statusCode,
+            message: 'List of users route error',
+        })
+        res.send(errReport).status(500)
+    }
+}
+
+export const router = express.Router({ mergeParams: true })
+
+router.get('/', handleInstrumentationKeyRoute)
+
+export default router
