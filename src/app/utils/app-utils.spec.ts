0da768a4f642ca9231150f14ce3d272a187e215a
fazanki <fazanki@gmail.com>
2019-07-18 11:10:36 +0100

    fixing sonar

--- src/app/utils/app-utils.spec.ts
+++ src/app/utils/app-utils.spec.ts
@@ -2,25 +2,29 @@ import {AppUtils} from './app-utils';
 
 describe('AppUtils', () => {
 
-  it('should set correct page titles', () => {
+  it('should set correct page titles pending details', () => {
     const array = AppUtils.setPageTitle('pending-organisations/organisation/');
     expect(array).toEqual('Pending organisation details - Approve organisation');
   });
-  it('should set correct page titles', () => {
+  it('should set correct page titles confirmation', () => {
     const array = AppUtils.setPageTitle('pending-organisations/approve-success');
     expect(array).toEqual('Confirmation - Approve organisations');
   });
-  it('should set correct page titles', () => {
+  it('should set correct page titles check details', () => {
     const array = AppUtils.setPageTitle('pending-organisations/approve');
     expect(array).toEqual('Check details - Approve organisations');
   });
-  it('should set correct page titles', () => {
+  it('should set correct page titles pending ', () => {
     const array = AppUtils.setPageTitle('pending-organisations');
     expect(array).toEqual('Pending organisations - Approve organisations');
   });
-  it('should set correct page titles', () => {
-    const array = AppUtils.setPageTitle('pending-organisations/approve');
+  it('should set correct page titles active details', () => {
+    const array = AppUtils.setPageTitle('organisations/organisation');
     expect(array).toEqual('Check details - Approve organisations');
+  });
+  it('should set correct page titles general fallback', () => {
+    const array = AppUtils.setPageTitle('');
+    expect(array).toEqual('Active organisations - Approve organisations');
   });
 
 });
--- src/app/utils/app-utils.ts
+++ src/app/utils/app-utils.ts
@@ -8,17 +8,22 @@ export class AppUtils {
     /**
      * it sets correct page titles
      */
-    if (url.indexOf('pending-organisations/organisation/') !== -1) {
+    if (url === 'pending-organisations/organisation/') {
       return 'Pending organisation details - Approve organisation';
-    } else if (url.indexOf('pending-organisations/approve-success') !== -1) {
+    } else if (url === 'pending-organisations/approve-success') {
       return 'Confirmation - Approve organisations';
-    } else if (url.indexOf('pending-organisations/approve') !== -1) {
+    } else if (url === 'pending-organisations/approve') {
       return 'Check details - Approve organisations';
-    } else if (url.indexOf('pending-organisations') !== -1) {
+    } else if (url === 'pending-organisations/approve-success') {
+      return 'Confirmation - Approve organisations';
+    } else if (url === 'pending-organisations') {
       return 'Pending organisations - Approve organisations';
-    } else if (url.indexOf('pending-organisations/approve') !== -1) {
+    } else if (url === 'pending-organisations/approve') {
+      return 'Check details - Approve organisations';
+    } else if (url.indexOf('organisations/organisation') !== -1) {
       return 'Check details - Approve organisations';
     }
+
     return 'Active organisations - Approve organisations';
   }
 }
