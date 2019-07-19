595640cf4e2be56851755abd3a2f39c8dae26deb
nagendrareddyk <kommurunagendra@gmail.com>
2019-07-19 13:17:16 +0100

    Added the LoginandLogoutAutiomation test

--- test/e2e/features/pageObjects/headerPage.js
+++ test/e2e/features/pageObjects/headerPage.js
@@ -5,6 +5,7 @@ const { SHORT_DELAY, MID_DELAY, LONG_DELAY } = require('../../support/constants'
 function HeaderPage() {
 
   this.aoPage = element(by.xpath("//a[contains(text(),'Approve organisations')]"));
+  this.aoPage = element(by.css("[id='validation-error-summary-heading']"));
   this.signOut = element(by.xpath("//a[contains(text(),'Sign out')]"));
 
   this.clickSignOut = function () {
