646709353d1c5b407bd4f83626d90181634935a2
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2019-07-22 12:31:53 +0100

    approve organisation server error message

--- test/e2e/features/step_definitions/steps/confirmApproveOrganisation.steps.js
+++ test/e2e/features/step_definitions/steps/confirmApproveOrganisation.steps.js
@@ -45,7 +45,7 @@ defineSupportCode(function ({ Given, When, Then }) {
     await expect(bannerPage.confirmationScreen.getText())
       .to
       .eventually
-      .contains('Successfully approved');
+      .contains('Organisation approved successfully');
   });
 
   Then(/^I click to Back to Organisations link$/, { timeout: 600 * 1000 }, async function () {
