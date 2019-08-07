305461f9ebe33f546faa0de20e93c7bc456f8fb1
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2019-08-07 15:45:01 +0100

    env settings refactor

--- api/lib/config/environments/prod.config.ts
+++ api/lib/config/environments/prod.config.ts
@@ -2,8 +2,8 @@ export default {
     services: {
         ccdDataApi: 'https://ccd-data-store-api-prod.service.core-compute-prod.internal',
         ccdDefApi: 'https://ccd-definition-store-api-prod.service.core-compute-prod.internal',
-      idamWeb: 'https://idam-web-public-idam-prod.service.core-compute-prod.internal',
-      idamApi: 'https://idam-api-idam-prod.service.core-compute-prod.internal',
+        idamWeb: 'https://idam-web-public-idam-prod.service.core-compute-prod.internal',
+        idamApi: 'https://idam-api-idam-prod.service.core-compute-prod.internal',
         s2s: 'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal',
         draftStoreApi: 'https://draft-store-service-prod.service.core-compute-prod.internal',
         dmStoreApi: 'https://dm-store-prod.service.core-compute-prod.internal',
