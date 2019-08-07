e0e201b451ecbdbf258fdf8e3ba0f02c03ce108e
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-07 11:58:46 +0100

    all changes

--- api/lib/config/environments/demo.config.ts
+++ api/lib/config/environments/demo.config.ts
@@ -5,7 +5,7 @@ export default {
     idamWeb: 'https://idam-web-public.demo.platform.hmcts.net',
     idamApi: 'https://idam-api.demo.platform.hmcts.net',
     s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
-    rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-preview.demo',
+    rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-demo.internal',
   },
   health: {
     ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal/health',
