b72226d848e17e0af3910566082cc7deadb5bc60
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-08 17:23:00 +0100

    new changes

--- api/lib/config/environments/local.config.ts
+++ api/lib/config/environments/local.config.ts
@@ -13,7 +13,7 @@ export default {
     ccdDefApi: 'https://ccd-definition-store-api-demo.service.core-compute-demo.internal/health',
     idamWeb: 'https://idam-web-public.demo.platform.hmcts.net/health',
     idamApi: 'https://idam-api.demo.platform.hmcts.net/health',
-    s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal/health',
+    s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal/health',
     rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-demo.internal/health',
   },
   proxy: {
