687212c205659dcee4de5695c032c38e3ae1eb73
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-09 10:36:11 +0100

    clientid changes

--- config/application.config.ts
+++ config/application.config.ts
@@ -3,8 +3,8 @@ export const application = {
         token: '__auth__',
         userId: '__userid__'
     },
-    microservice: 'xuiwebapp',
-    idam_client: 'xuiwebapp',
+    microservice: 'xui_webapp',
+    idam_client: 'xuiaowebapp',
     oauth_callback_url: 'oauth2/callback',
     protocol: 'https'
 };
