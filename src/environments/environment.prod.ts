f73718382fd124dc0f75659a8d6a47593902d2a0
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-09 11:04:33 +0100

    clientid changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -15,7 +15,7 @@ export const environment = {
   urls: {
     idam: {
       idamApiUrl: 'https://idam-api.ithc.platform.hmcts.net',
-      idamClientID: 'xuiwebapp',
+      idamClientID: 'xuiaowebapp',
       idamLoginUrl: 'https://idam-web-public.ithc.platform.hmcts.net',
       indexUrl: '/',
       oauthCallbackUrl: 'oauth2/callback'
