056a5d824fc19932cf94a58327e5ece2b2b2e4bd
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-11 12:07:20 +0100

    local pointing to demo

--- api/services/serviceAuth.ts
+++ api/services/serviceAuth.ts
@@ -7,9 +7,11 @@ import { getHealth, getInfo } from '../lib/util'
 
 import { tunnel } from '../lib/tunnel'
 import * as log4jui from '../lib/log4jui'
+import {application} from '../lib/config/application.config'
 
 const url = config.services.s2s
-const microservice = config.microservice
+// const microservice = config.microservice
+const microservice =  application.microservice
 const s2sSecret = process.env.S2S_SECRET || 'AAAAAAAAAAAAAAAA'
 
 const logger = log4jui.getLogger('service auth')
@@ -17,13 +19,12 @@ const logger = log4jui.getLogger('service auth')
 export async function postS2SLease() {
     const configEnv = process ? process.env.PUI_ENV || 'local' : 'local'
     let request: AxiosResponse<any>
-    if (configEnv !== 'ldocker' && config !== 'local') {
+    console.log('PUI_ENV is now:', configEnv)
+    console.log('postS2SLease url')
+    console.log(url)
+    if (configEnv !== 'ldocker') {
         const oneTimePassword = otp({ secret: s2sSecret }).totp()
-
         logger.info('generating from secret  : ', s2sSecret, microservice, oneTimePassword)
-
-       // try
-     //   {
         request = await http.post(`${url}/lease`, {
             microservice,
             oneTimePassword,
