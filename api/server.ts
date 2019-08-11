fbde2f15ba95589c1df8aa5da191418e6b1c80b7
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-08-11 14:50:30 +0100

    Trial

--- api/server.ts
+++ api/server.ts
@@ -24,7 +24,7 @@ app.use(
             maxAge: 1800000,
             secure: config.secureCookie !== false,
         },
-        name: "xuiaowebapp",
+        name: "jui-webapp",
         resave: true,
         saveUninitialized: true,
         secret: config.sessionSecret,
