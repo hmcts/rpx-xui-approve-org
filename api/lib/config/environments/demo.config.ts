72ebe9fa644102b2a8e8935351b0fe8798604c4b
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-12 11:19:32 +0100

    removed https to http

--- api/lib/config/environments/demo.config.ts
+++ api/lib/config/environments/demo.config.ts
@@ -5,7 +5,7 @@ export default {
     idamWeb: 'https://idam-web-public.demo.platform.hmcts.net',
     idamApi: 'https://idam-api.demo.platform.hmcts.net',
    // idamLoginUrl: 'https://idam-web-public.demo.platform.hmcts.net',
-    s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
+    s2s: 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
     rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-demo.internal',
   },
   health: {
@@ -14,7 +14,7 @@ export default {
     idamWeb: 'https://idam-web-public.demo.platform.hmcts.net/health',
     idamApi: 'https://idam-api.demo.platform.hmcts.net/health',
     // idamLoginUrl: 'https://idam-web-public.demo.platform.hmcts.net/health',
-    s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal/health',
+    s2s: 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal/health',
     rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-demo.internal/health',
   },
   useProxy: false,
