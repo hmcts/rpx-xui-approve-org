359ede924d412d91458dc2295947310d6edc73e8
Uday Denduluri <uday.denduluri@gmail.com>
2019-08-13 14:51:56 +0100

    Refactor, clean up and make the approve Org details work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   googleAnalyticsKey: 'UA-124734893-1',
   serviceDeskEmail: 'DCD-ITServiceDesk@hmcts.net',
   serviceDeskTel: '0300 3030686',
-  singleOrgUrl: 'api/organisations?pbaNumber=',
+  singleOrgUrl: 'api/organisations?organisationId=',
   orgActiveUrl: 'api/organisations',
   orgPendingUrl: 'api/organisations?status=PENDING',
   orgApprovePendingUrl: 'api/organisations/',
