e4ad2299e155bbc33334e61ba4ad0252853eb51f
fazanki <fazanki@gmail.com>
2019-08-14 12:20:05 +0100

    chaing the endpoint for s2s generation to ithc

--- config/environments/local.config.ts
+++ config/environments/local.config.ts
@@ -2,7 +2,7 @@ export default {
     services: {
         idam_web: 'https://idam.preprod.ccidam.reform.hmcts.net',
         idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
-        s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal'
+        s2s: 'https://rpe-service-auth-provider-ithc.service.core-compute-ithc.internal'
     },
     proxy: {
         host: '172.16.0.7',
@@ -11,5 +11,4 @@ export default {
     protocol: 'http',
     secureCookie: false,
     sessionSecret: 'secretSauce',
-    logging: 'debug'
 };
