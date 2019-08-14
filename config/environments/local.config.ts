767a0eebb0b373341079ef43d3858c62cb81aa1b
fazanki <fazanki@gmail.com>
2019-08-14 12:20:49 +0100

    changing the https

--- config/environments/local.config.ts
+++ config/environments/local.config.ts
@@ -2,7 +2,7 @@ export default {
     services: {
         idam_web: 'https://idam.preprod.ccidam.reform.hmcts.net',
         idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
-        s2s: 'https://rpe-service-auth-provider-ithc.service.core-compute-ithc.internal'
+        s2s: 'http://rpe-service-auth-provider-ithc.service.core-compute-ithc.internal'
     },
     proxy: {
         host: '172.16.0.7',
