04d7dc16bc34751a3faab2e8e320b179140ce2d7
Uday Denduluri <uday.denduluri@gmail.com>
2019-08-15 11:52:57 +0100

    Adding interceptors

--- api/lib/interceptors.spec.ts
+++
@@ -1,9 +0,0 @@
-import { errorInterceptor } from './interceptors'
-
-fdescribe('interceptor', () => {
-
-    it('Should not throw error', () => {
-        const error = {}
-        errorInterceptor(error)
-    })
-})
