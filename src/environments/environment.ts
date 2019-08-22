e47b90e1ded9653d31267cb6eb693c474acfed12
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2019-08-22 11:01:30 +0100

    Aat setup eui 673 (#135)
    * AAT Setup changes EUI-673
    
    * changes to idam aat on environment urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,9 +20,9 @@ export const environment = {
     },
     urls: {
       idam: {
-        idamApiUrl: 'https://idam-api.demo.platform.hmcts.net',
+        idamApiUrl: 'https://idam-api.aat.platform.hmcts.net',
         idamClientID: 'xuiaowebapp',
-        idamLoginUrl: 'https://idam-web-public.demo.platform.hmcts.net',
+        idamLoginUrl: 'https://idam-web-public.aat.platform.hmcts.net',
         indexUrl: '/',
         oauthCallbackUrl: 'oauth2/callback'
       }
