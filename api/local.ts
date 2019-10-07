1c0c7bf029e8b828cca6710e07da2368ab1396fb
Pardeep Singh Basi <paddybasi@gmail.com>
2019-10-07 10:56:07 +0100

    abstracted config from environment and provided a dev default

--- api/local.ts
+++ api/local.ts
@@ -9,12 +9,14 @@ import * as auth from './auth'
 import { appInsights } from './lib/appInsights'
 import { config } from './lib/config'
 import { errorStack } from './lib/errorStack'
+import { exists } from './lib/util'
 import routes from './routes'
 
 const FileStore = sessionFileStore(session)
 
 const app = express()
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+const logger = log4js.getLogger('server')
+logger.level = config.logging ? config.logging : 'OFF'
 
 app.use(
     session({
@@ -28,20 +30,20 @@ app.use(
         saveUninitialized: true,
         secret: config.sessionSecret,
         store: new FileStore({
-            path: process.env.NOW ? '/tmp/sessions' : '.sessions',
+            path: config.now ? '/tmp/sessions' : '.sessions',
         }),
     })
 )
 
-if (config.proxy) {
-    globalTunnel.initialize({
-        host: config.proxy.host,
-        port: config.proxy.port,
-    })
+if (exists(config.proxy, 'host') && exists(config.proxy, 'port')) {
+  logger.info('configuring proxy tunnel: ', config.proxy)
+  globalTunnel.initialize({
+      host: config.proxy.host,
+      port: config.proxy.port,
+  })
 }
 
 app.use(errorStack)
-console.log('use AppInsights ' + appInsights)
 app.use(appInsights)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -53,14 +55,7 @@ app.use(auth.attach)
 
 app.use('/api', routes)
 
-const port = process.env.PORT || 3001
+const port = config.port || 3001
 app.listen(port)
-
-if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    config.appInsightsInstrumentationKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
-}
-
-const logger = log4js.getLogger('server')
-logger.level = config.logging ? config.logging : 'OFF'
 
 logger.info(`Local server up at ${port}`)
