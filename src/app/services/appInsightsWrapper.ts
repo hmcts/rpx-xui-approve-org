975a7a5fc5533bb9491b1cf310dc257fffac89cc
Ronald Mansveld <ronald.mansveld@atos.net>
2019-10-22 12:51:22 +0100

    Code cleanup of app insights wrapper

--- src/app/services/appInsightsWrapper.ts
+++ src/app/services/appInsightsWrapper.ts
@@ -1,84 +1,220 @@
 import { AppInsights } from 'applicationinsights-js';
 
+export interface Properties { [name: string]: string; }
+export interface Measurements { [name: string]: number; }
+
 export abstract class AbstractAppInsights implements Microsoft.ApplicationInsights.IAppInsights {
-  config: Microsoft.ApplicationInsights.IConfig;  context: Microsoft.ApplicationInsights.ITelemetryContext;
-  queue: (() => void)[];
-  abstract startTrackPage(name?: string);
-  abstract stopTrackPage(name?: string, url?: string, properties?: { [name: string]: string; },
-                         measurements?: { [name: string]: number; });
-  abstract trackPageView(name?: string, url?: string, properties?: { [name: string]: string; },
-                         measurements?: { [name: string]: number; }, duration?: number);
-  abstract startTrackEvent(name: string);
-  abstract stopTrackEvent(name: string, properties?: { [name: string]: string; }, measurements?: { [name: string]: number; });
-  abstract trackEvent(name: string, properties?: { [name: string]: string; }, measurements?: { [name: string]: number; });
-  abstract trackDependency(id: string, method: string, absoluteUrl: string, pathName: string, totalTime: number,
-                           success: boolean, resultCode: number,
-                           properties?: { [name: string]: string; }, measurements?: { [name: string]: number; });
+  public config: Microsoft.ApplicationInsights.IConfig;
+  public context: Microsoft.ApplicationInsights.ITelemetryContext;
+  public queue: (() => void)[];
 
-  abstract trackException(exception: Error, handledAt?: string,
-                          properties?: { [name: string]: string; }, measurements?: { [name: string]: number; },
-                          severityLevel?: AI.SeverityLevel);
+  public abstract startTrackPage(name?: string): void;
 
-  abstract trackMetric(name: string, average: number, sampleCount?: number, min?: number, max?: number,
-                       properties?: { [name: string]: string; });
-  abstract trackTrace(message: string, properties?: { [name: string]: string; }, severityLevel?: AI.SeverityLevel) ;
-  abstract flush();
-  abstract setAuthenticatedUserContext(authenticatedUserId: string, accountId?: string, storeInCookie?: boolean);
-  abstract clearAuthenticatedUserContext();
-  abstract downloadAndSetup?(config: Microsoft.ApplicationInsights.IConfig);
-  abstract _onerror(message: string, url: string, lineNumber: number, columnNumber: number, error: Error);
+  public abstract stopTrackPage(
+    name?: string,
+    url?: string,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void;
+
+  public abstract trackPageView(
+    name?: string,
+    url?: string,
+    properties?: Properties,
+    measurements?: Measurements,
+    duration?: number
+  ): void;
+
+  public abstract startTrackEvent(name: string): void;
+
+  public abstract stopTrackEvent(
+    name: string,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void;
+
+  public abstract trackEvent(
+    name: string,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void;
+
+  public abstract trackDependency(
+    id: string,
+    method: string,
+    absoluteUrl: string,
+    pathName: string,
+    totalTime: number,
+    success: boolean,
+    resultCode: number,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void;
+
+
+  public abstract trackException(
+    exception: Error,
+    handledAt?: string,
+    properties?: Properties,
+    measurements?: Measurements,
+    severityLevel?: AI.SeverityLevel
+  ): void;
+
+  public abstract trackMetric(
+    name: string,
+    average: number,
+    sampleCount?: number,
+    min?: number,
+    max?: number,
+    properties?: Properties
+  ): void;
+
+  public abstract trackTrace(
+    message: string,
+    properties?: Properties,
+    severityLevel?: AI.SeverityLevel
+  ): void;
+
+  public abstract flush(): void;
+
+  public abstract setAuthenticatedUserContext(
+    authenticatedUserId: string,
+    accountId?: string,
+    storeInCookie?: boolean
+  ): void;
+
+  public abstract clearAuthenticatedUserContext(): void;
+
+  public abstract downloadAndSetup?(config: Microsoft.ApplicationInsights.IConfig): void;
+
+  public abstract _onerror(
+    message: string,
+    url: string,
+    lineNumber: number,
+    columnNumber: number,
+    error: Error
+  ): void;
 
 }
 
 export class AppInsightsWrapper implements AbstractAppInsights {
-  config: Microsoft.ApplicationInsights.IConfig;  context: Microsoft.ApplicationInsights.ITelemetryContext;
+  config: Microsoft.ApplicationInsights.IConfig;
+  context: Microsoft.ApplicationInsights.ITelemetryContext;
   queue: (() => void)[];
-  startTrackPage(name?: string) {
+
+  public startTrackPage(name?: string): void {
     AppInsights.startTrackPage(name);
   }
-  stopTrackPage(name?: string, url?: string, properties?: { [name: string]: string; }, measurements?: { [name: string]: number; }) {
+
+  public stopTrackPage(
+    name?: string,
+    url?: string,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void {
     AppInsights.stopTrackPage(name, url, properties, measurements);
   }
-  trackPageView(name?: string, url?: string, properties?: { [name: string]: string; }, measurements?: { [name: string]: number; },
-                duration?: number) {
+
+  public trackPageView(
+    name?: string,
+    url?: string,
+    properties?: Properties,
+    measurements?: Measurements,
+    duration?: number
+  ): void {
     AppInsights.trackPageView(name, url, properties, measurements, duration);
   }
-  startTrackEvent(name: string) {
+
+  public startTrackEvent(name: string): void {
     AppInsights.startTrackEvent(name);
   }
-  stopTrackEvent(name: string, properties?: { [name: string]: string; }, measurements?: { [name: string]: number; }) {
+
+  public stopTrackEvent(
+    name: string,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void {
     AppInsights.stopTrackEvent(name, properties, measurements);
   }
-  trackEvent(name: string, properties?: { [name: string]: string; }, measurements?: { [name: string]: number; }) {
+
+  public trackEvent(
+    name: string,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void {
     AppInsights.trackEvent(name, properties, measurements);
   }
-  trackDependency(id: string, method: string, absoluteUrl: string, pathName: string, totalTime: number, success: boolean,
-                  resultCode: number, properties?: { [name: string]: string; }, measurements?: { [name: string]: number; }) {
+
+  public trackDependency(
+    id: string,
+    method: string,
+    absoluteUrl: string,
+    pathName: string,
+    totalTime: number,
+    success: boolean,
+    resultCode: number,
+    properties?: Properties,
+    measurements?: Measurements
+  ): void {
     AppInsights.trackDependency(id, method, absoluteUrl, pathName, totalTime, success, resultCode, properties, measurements);
   }
-  trackException(exception: Error, handledAt?: string, properties?: { [name: string]: string; },
-                 measurements?: { [name: string]: number; }, severityLevel?: AI.SeverityLevel) {
+
+  public trackException(
+    exception: Error,
+    handledAt?: string,
+    properties?: Properties,
+    measurements?: Measurements,
+    severityLevel?: AI.SeverityLevel
+  ): void {
     AppInsights.trackException(exception, handledAt, properties, measurements, severityLevel);
   }
-  trackMetric(name: string, average: number, sampleCount?: number, min?: number, max?: number, properties?: { [name: string]: string; }) {
+
+  public trackMetric(
+    name: string,
+    average: number,
+    sampleCount?: number,
+    min?: number,
+    max?: number,
+    properties?: Properties
+  ): void {
     AppInsights.trackMetric(name, average, sampleCount, min, max, properties);
   }
-  trackTrace(message: string, properties?: { [name: string]: string; }, severityLevel?: AI.SeverityLevel) {
+
+  public trackTrace(
+    message: string,
+    properties?: Properties,
+    severityLevel?: AI.SeverityLevel
+  ): void {
     AppInsights.trackTrace(message, properties, severityLevel);
   }
-  flush() {
+
+  public flush(): void {
     AppInsights.flush();
   }
-  setAuthenticatedUserContext(authenticatedUserId: string, accountId?: string, storeInCookie?: boolean) {
+
+  public setAuthenticatedUserContext(
+    authenticatedUserId: string,
+    accountId?: string,
+    storeInCookie?: boolean
+  ): void {
     AppInsights.setAuthenticatedUserContext(authenticatedUserId, accountId, storeInCookie);
   }
-  clearAuthenticatedUserContext() {
+
+  public clearAuthenticatedUserContext(): void {
     AppInsights.clearAuthenticatedUserContext();
   }
-  downloadAndSetup?(config: Microsoft.ApplicationInsights.IConfig) {
+
+  public downloadAndSetup(config: Microsoft.ApplicationInsights.IConfig): void {
     AppInsights.downloadAndSetup(config);
   }
-  _onerror(message: string, url: string, lineNumber: number, columnNumber: number, error: Error) {
+
+  public _onerror(
+    message: string,
+    url: string,
+    lineNumber: number,
+    columnNumber: number,
+    error: Error
+  ): void {
     AppInsights._onerror(message, url, lineNumber, columnNumber, error);
   }
 }
