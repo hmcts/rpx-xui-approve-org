c9fc363d04a492643fc08cf44574e7bd5e3c2503
Ronald Mansveld <ronald.mansveld@atos.net>
2019-10-22 15:28:02 +0100

    code cleanup of health check guard

--- src/shared/guards/health-check.guard.ts
+++ src/shared/guards/health-check.guard.ts
@@ -1,42 +1,42 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
-import { HealthCheckService } from '../services/health-check.service';
 import * as fromRoot from '../../app/store';
-import { Store } from '@ngrx/store';
+import { HealthCheckService, HealthState } from '../services/health-check.service';
 
 
 @Injectable()
 export class HealthCheckGuard implements CanActivate {
     constructor(
-        private healthCheck: HealthCheckService,
-        private store: Store<fromRoot.State>,
+        private _healthCheck: HealthCheckService,
+        private _store: Store<fromRoot.State>,
     ) {
     }
 
-    canActivate() {
-        return this.checkHealth().pipe(
-            switchMap((res: any) => {
+    public canActivate(): Observable<boolean> {
+        return this._checkHealth().pipe(
+            switchMap((res: HealthState) => {
                 const state = res.healthState;
                 if (!state) {
-                    this.redirectToServiceDownPage();
+                    this._redirectToServiceDownPage();
                 }
                 return of(res.healthState);
             }),
             catchError(() => {
-                this.redirectToServiceDownPage();
+                this._redirectToServiceDownPage();
                 return of(false);
             })
         );
     }
 
-    checkHealth(): Observable<boolean> {
-        return this.healthCheck.doHealthCheck();
+    private _checkHealth(): Observable<HealthState> {
+        return this._healthCheck.doHealthCheck();
     }
 
-    redirectToServiceDownPage() {
-        this.store.dispatch(new fromRoot.Go({ path: ['/service-down'] }));
+    private _redirectToServiceDownPage() {
+        this._store.dispatch(new fromRoot.Go({ path: ['/service-down'] }));
     }
 }
 
