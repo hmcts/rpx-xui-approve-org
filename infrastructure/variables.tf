314ae48f5caa1818325e73feda64f384afb8660d
Ernest Man <ernestman@emsys9.local>
2019-11-06 10:04:58 +0000

    fixed terraform variable file

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,9 +1,9 @@
 variable "product" {
-  type = string
+  type = "string"
 }
 
 variable "component" {
-  type = string
+  type = "string"
 }
 
 variable "team_name" {
@@ -15,7 +15,7 @@ variable "app_language" {
 }
 
 variable "location" {
-  type = string
+  type = "string"
   default = "UK South"
 }
 
@@ -32,7 +32,7 @@ variable "ilbIp"{}
 variable "tenant_id" {}
 
 variable "jenkins_AAD_objectId" {
-  type                        = string
+  type                        = "string"
   description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
@@ -52,64 +52,64 @@ variable "additional_host_name" {
 }
 
 variable "session_secret" {
-  type = string
+  type = "string"
   default = "secretSauce"
 }
 
 variable "node_tls_reject_unathorized" {
-  type = number
+  type = "number"
   default = 1
 }
 variable "api_port" {
-  type = number
+  type = "number"
   default = 3000
 }
 variable "api_now" {
-  type = bool
+  type = "bool"
   default = false
 }
 variable "idam_client" {
-  type = string
+  type = "string"
   default = "xuiaowebapp"
 }
 variable "oauth_callback_url" {
-  type = string
+  type = "string"
   default = "/oauth2/callback"
 }
 variable "max_log_line" {
-  type = number
+  type = "number"
   default = 80
 }
 variable "exception_options_max_lines" {
-  type = number
+  type = "number"
   default = 1
 }
 variable "index_url" {
-  type = string
+  type = "string"
   default = "/"
 }
 variable "logging" {
-  type = string
+  type = "string"
   default = "debug"
 }
 variable "protocol" {
-  type = string
+  type = "string"
   default = "https"
 }
 variable "secure_cookie" {
-  type = bool
+  type = "bool"
   default = false
 }
 variable "cookie_token" {
-  type = string
+  type = "string"
   default = "__auth__"
 }
 variable "cookie_user_id" {
-  type = string
+  type = "string"
   default = "__userid__"
 }
 variable "microservice" {
-  type = string
+  type = "string"
   default = "xui_webapp"
 }
 variable "ccd_data_api_service" {}
@@ -121,7 +121,7 @@ variable "s2s_service" {}
 
 variable "proxy_host" {}
 variable "proxy_port" {
-  type = number
+  type = "number"
   default = 0
 }
 
