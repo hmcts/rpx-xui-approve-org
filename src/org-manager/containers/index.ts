59558f6b0b40f90e5b5dc1ddc76b12322e989b19
fazanki <fazanki@gmail.com>
2019-11-19 11:49:48 +0000

    removing org-pending summary

--- src/org-manager/containers/index.ts
+++ src/org-manager/containers/index.ts
@@ -3,7 +3,6 @@ import { OrgOverviewComponent } from './org-overview/org-overview.component';
 import { OrgSummaryComponent } from './org-summary/org-summary.component';
 import { OverviewPendingComponent } from './../../org-manager/containers/overview-pending/pending-overview.component';
 import { OrgPendingOverviewComponent } from './../../org-manager/containers/org-pending-overview/org-pending-overview.component';
-import { OrgPendingSummaryComponent } from './../../org-manager/containers/org-pending-summary/org-pending-summary.component';
 import { OrgPendingApproveComponent } from './../../org-manager/containers/org-pending-approve/org-pending-approve.component';
 import { OrgApprovalSuccessComponent } from './../../org-manager/containers/org-success/org-approval-success.component';
 
@@ -12,7 +11,7 @@ export const containers: any[] = [
   OrgOverviewComponent,
   OrgSummaryComponent,
   OverviewPendingComponent,
-  OrgPendingSummaryComponent,
+
   OrgPendingOverviewComponent,
   OrgPendingApproveComponent,
   OrgApprovalSuccessComponent
@@ -22,7 +21,6 @@ export * from './overview/org-overview.component';
 export * from './org-overview/org-overview.component';
 export * from './org-summary/org-summary.component';
 export * from './../../org-manager/containers/overview-pending/pending-overview.component';
-export * from './../../org-manager/containers/org-pending-summary/org-pending-summary.component';
 
 
 
--- src/org-manager/org-manager.routing.ts
+++ src/org-manager/org-manager.routing.ts
@@ -6,7 +6,6 @@ import { OrgOverviewComponent } from './containers/org-overview/org-overview.com
 import { OrgSummaryComponent } from './containers/org-summary/org-summary.component';
 import { OverviewPendingComponent } from 'src/org-manager/containers/overview-pending/pending-overview.component';
 import { OrgPendingOverviewComponent } from 'src/org-manager/containers/org-pending-overview/org-pending-overview.component';
-import { OrgPendingSummaryComponent } from 'src/org-manager/containers/org-pending-summary/org-pending-summary.component';
 import { OrgPendingApproveComponent } from 'src/org-manager/containers/org-pending-approve/org-pending-approve.component';
 import { OrgApprovalSuccessComponent } from 'src/org-manager/containers/org-success/org-approval-success.component';
 import { AuthGuard } from 'src/services/auth/auth.guard';
@@ -19,19 +18,6 @@ export const ROUTES: Routes = [
     canActivate: [AuthGuard],
   },
   {
-    path: 'organisations/organisation',
-    component: OrgOverviewComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: ':id',
-        component: OrgSummaryComponent,
-        canActivate: [
-        ]
-      }
-    ]
-  },
-  {
     path: 'pending-organisations',
     component: OrgPendingOverviewComponent,
     canActivate: [AuthGuard],
@@ -39,10 +25,6 @@ export const ROUTES: Routes = [
       {
         path: '',
         component: OverviewPendingComponent,
-      },
-      {
-        path: 'organisation/:id',
-        component: OrgPendingSummaryComponent
       },
       {
         path: 'approve',
