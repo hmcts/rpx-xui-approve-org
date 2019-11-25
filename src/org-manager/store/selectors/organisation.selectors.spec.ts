ce0d934d745ebc320e097744053b8e4fcd2a0d37
Franco Zanki <fazanki@users.noreply.github.com>
2019-11-25 11:13:38 +0000

    Feature/eui 638 org details ngrx refactor fz (#184)
    * removing old stuff phaze1
    
    * adding org details component
    
    * refactoring state and removing unecessary code
    
    * removing org-pending summary
    
    * refatoring reducer actions
    
    * getting active organisation displayed
    
    * renaming components
    
    * removing old summary component
    
    * cleaning up
    
    * renaming approve org
    
    * checking identity bar
    
    * pending organistation wireing
    
    * adding catching
    
    * finishing pending list adding error message and redirect
    
    * rendering approve org component
    
    * wireing up pending org confirmation
    
    * adding entities
    
    * adding removing entities
    
    * wiring up details page
    
    * adding separate loaders for active and penidng
    
    * cleaning up
    
    * fixing a back link
    
    * fixing lints
    
    * fixing tests
    
    * adding test for selectors
    
    * test for reducers
    
    * adding tests for effects
    
    * adding test for actions and components
    
    * adding approve org spec
    
    * fixing the fetching only active organisation on landing page
    
    * fixes as per Ronalds pr comments
    
    * fixng missing a
    
    * adding missing public statements
    
    * adding types in actions
    
    * fixing build and adding extra typings
    
    * allowing details page to load org as per udays pr request
    
    * cleaning up
    
    * changing router links to array

--- src/org-manager/store/selectors/organisation.selectors.spec.ts
+++ src/org-manager/store/selectors/organisation.selectors.spec.ts
@@ -1,8 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { select, Store, StoreModule } from '@ngrx/store';
 import { OrganisationState } from '../reducers/organisation.reducer';
-import { getOrganisationsState, selectedOrganisation } from './organisation.selectors';
+import * as fromSelectors from './organisation.selectors';
 import { reducers } from '../index';
+import {OrganisationVM} from '../../models/organisation';
+
+const OrganisationState = {
+  activeOrganisations: {
+    orgEntities: {},
+    loaded: false,
+    loading: false
+  },
+  pendingOrganisations: {
+    orgEntities: {},
+    loaded: false,
+    loading: false,
+  },
+  errorMessage: '',
+  orgForReview: null
+};
 
 describe('Organisation selectors', () => {
   let store: Store<OrganisationState>;
@@ -10,7 +26,7 @@ describe('Organisation selectors', () => {
     TestBed.configureTestingModule({
       imports: [
         StoreModule.forRoot({}),
-        StoreModule.forFeature('approveOrg', reducers),
+        StoreModule.forFeature('orgStatePending', reducers),
       ],
     });
     store = TestBed.get(Store);
@@ -20,22 +36,135 @@ describe('Organisation selectors', () => {
   describe('getOrganisationState', () => {
     it('should return organisation state', () => {
       let result;
-      store.pipe(select(getOrganisationsState)).subscribe(value => {
+      store.pipe(select(fromSelectors.getOrganisationsState)).subscribe(value => {
         result = value;
 
       });
-      expect(result).toEqual({ organisations: null, loaded: false, loading: false });
+      expect(result).toEqual(OrganisationState);
     });
   });
 
-  describe('selectedOrganisation', () => {
-    it('should return blank organisation', () => {
+  describe('getSelectedActiveOrganisation', () => {
+    it('should return initial active org state', () => {
       let result;
-      store.pipe(select(selectedOrganisation)).subscribe(value => {
+      store.pipe(select(fromSelectors.getActiveOrganisationState)).subscribe(value => {
         result = value;
 
       });
+      expect(result).toEqual({loading: false, loaded: false, orgEntities: {}});
+    });
+  });
+
+  describe('getActiveOrganisationArray', () => {
+    it('should return an array', () => {
+      let result;
+      store.pipe(select(fromSelectors.getActiveOrganisationArray)).subscribe(value => {
+        result = value;
+
+      });
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('getActiveLoaded', () => {
+    it('should return boolian ', () => {
+      let result;
+      store.pipe(select(fromSelectors.getActiveLoaded)).subscribe(value => {
+        result = value;
+
+      });
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('getActiveLoading', () => {
+    it('should return boolian ', () => {
+      let result;
+      store.pipe(select(fromSelectors.getActiveLoading)).subscribe(value => {
+        result = value;
+
+      });
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('getPendingOrganisationsState', () => {
+    it('should return inital state ', () => {
+      let result;
+      store.pipe(select(fromSelectors.getPendingOrganisationsState)).subscribe(value => {
+        result = value;
+      });
+      expect(result).toEqual({loading: false, loaded: false, orgEntities: {}});
+    });
+  });
+
+  describe('getPendingOrganisations', () => {
+    it('should return entities', () => {
+      let result;
+      store.pipe(select(fromSelectors.getPendingOrganisations)).subscribe(value => {
+        result = value;
+      });
       expect(result).toEqual({});
+    });
+  });
+
+  describe('getPendingOrganisationsArray', () => {
+    it('should return array', () => {
+      let result;
+      store.pipe(select(fromSelectors.getPendingOrganisationsArray)).subscribe(value => {
+        result = value;
+      });
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('getPendingLoaded', () => {
+    it('should return boolean', () => {
+      let result;
+      store.pipe(select(fromSelectors.getPendingLoaded)).subscribe(value => {
+        result = value;
+      });
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('getErrorMessage', () => {
+    it('should return empty string', () => {
+      let result;
+      store.pipe(select(fromSelectors.getErrorMessage)).subscribe(value => {
+        result = value;
+      });
+      expect(result).toEqual('');
+    });
+  });
+
+  describe('getOrganisationForReview', () => {
+    it('should return null', () => {
+      let result;
+      store.pipe(select(fromSelectors.getOrganisationForReview)).subscribe(value => {
+        result = value;
+      });
+      expect(result).toEqual(null);
+    });
+  });
+
+  describe('pendingOrganisationsCount', () => {
+    it('should return null', () => {
+      let result;
+      store.pipe(select(fromSelectors.pendingOrganisationsCount)).subscribe(value => {
+        result = value;
+      });
+      expect(result).toEqual(0);
+    });
+  });
+
+  describe('getAllLoaded', () => {
+    it('should return boolean', () => {
+      let result;
+      store.pipe(select(fromSelectors.getAllLoaded)).subscribe(value => {
+        result = value;
+      });
+      expect(result).toEqual(false);
     });
   });
 
