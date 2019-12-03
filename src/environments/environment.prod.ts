82eacb5a692548325b801832f4d3250f8054d91f
Elviani Huseyin <elviani_s@hotmail.com>
2019-12-03 10:38:33 +0000

    adding google analytics

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleAnalyticsKey: 'UA-124734893-1',
+  googleAnalyticsKey: 'UA-124734893-5',
   serviceDeskEmail: 'DCD-ITServiceDesk@hmcts.net',
   serviceDeskTel: '0300 3030686',
   singleOrgUrl: 'api/organisations?organisationId=',
