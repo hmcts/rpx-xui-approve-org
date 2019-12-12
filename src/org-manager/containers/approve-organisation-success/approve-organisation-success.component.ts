dff9d4db15fb80fae9f1c007d0e836b70f41842d
Philip <philip.aberneithy@gmail.com>
2019-12-12 12:20:07 +0000

    Remove extra files to make it a complete rollback to the 24th of September

--- src/org-manager/containers/approve-organisation-success/approve-organisation-success.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-approval-success',
-    templateUrl: './approve-organisation-success.component.html'
-})
-export class ApproveOrganisationSuccessComponent {
-
-}
