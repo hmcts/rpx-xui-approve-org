fc39dee90a8d1e05196ca1e7900997b1580e6fcf
Philip <philip.aberneithy@gmail.com>
2019-12-12 12:20:07 +0000

    Remove extra files to make it a complete rollback to the 24th of September

--- src/org-manager/containers/approve-organisation-success/approve-organisation-success.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ApproveOrganisationSuccessComponent } from './approve-organisation-success.component';
-
-describe('Org Approval Success Component', () => {
-    it('should have a component', () => {
-        const component = new ApproveOrganisationSuccessComponent();
-        expect(component).toBeTruthy();
-});
-});
