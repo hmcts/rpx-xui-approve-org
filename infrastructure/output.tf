69d26bd456b53ad921b88c371465ff3b08ac74b7
ernestman28 <ernest_man28@hotmail.com>
2019-12-13 14:45:57 +0000

    update CNP and remove tf output

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,3 @@
 output "microserviceName" {
   value = "${local.app_full_name}"
 }
-
-output "vaultName" {
-    value = "${local.shared_vault_name}"
-}
-
-output "vaultUri" {
-  value = "${data.azurerm_key_vault.key_vault.vault_uri}"
-}
