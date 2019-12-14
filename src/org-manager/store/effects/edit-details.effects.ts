e52df9486996920b2e669a2a29e74855378ffffd
Pardeep Singh Basi <paddybasi@gmail.com>
2019-12-14 10:48:12 +0000

    Revert "Remove extra files to make it a complete rollback to the 24th of September"
    This reverts commit dff9d4db15fb80fae9f1c007d0e836b70f41842d.

---
+++ src/org-manager/store/effects/edit-details.effects.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import * as fromActions from '../actions';
+import * as fromRood from '../../../app/store';
+import {UpdatePbaServices} from '../../services';
+import {LoggerService} from '../../../app/services/logger.service';
+
+@Injectable()
+export class EditDetailsEffects {
+  constructor(
+    private actions$: Actions,
+    private updatePbaServices: UpdatePbaServices,
+    private loggerService: LoggerService
+  ) { }
+
+  @Effect()
+  submitPBA$ = this.actions$.pipe(
+    ofType(fromActions.SUBMIT_PBA),
+    map((action: fromActions.SubmitPba) => action.payload),
+    switchMap((body) => {
+      return this.updatePbaServices.updatePba(body).pipe(
+        map(() => new fromActions.SubmitPbaSuccess(body)),
+        catchError((error: Error) => {
+          this.loggerService.error(error);
+          return of(new fromActions.SubmitPbaFailure(error));
+        })
+      );
+    })
+  );
+
+  @Effect()
+  submitPbaSuccess$ = this.actions$.pipe(
+      ofType(fromActions.SUBMIT_PBA_SUCCESS),
+      map(() => {
+        return new fromRood.Back();
+      }),
+  );
+}
