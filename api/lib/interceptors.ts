3b8bc497e01db62a0d673f746e5d04b7e266c92c
Pardeep Singh Basi <paddybasi@gmail.com>
2019-12-14 15:06:21 +0000

    added error checking around error interceptor

--- api/lib/interceptors.ts
+++ api/lib/interceptors.ts
@@ -1,75 +1,83 @@
 import * as exceptionFormatter from 'exception-formatter'
 import * as stringify from 'json-stringify-safe'
 import * as errorStack from '../lib/errorStack'
-import { environmentConfig } from './environment.config'
+import {environmentConfig} from './environment.config'
 import * as log4jui from './log4jui'
-import { getTrackRequestObj, shorten, valueOrNull } from './util'
+import {exists, getTrackRequestObj, shorten, valueOrNull} from './util'
 
 const exceptionOptions = {
-    maxLines: 1,
+  maxLines: 1,
 }
 
 export function requestInterceptor(request) {
-    const logger = log4jui.getLogger('outgoing')
+  const logger = log4jui.getLogger('outgoing')
 
-    const url = shorten(request.url, environmentConfig.maxLogLine)
-    logger.info(`${request.method.toUpperCase()} to ${url}`)
-    //add timings to requests
-    request.metadata = { startTime: new Date() }
+  const url = shorten(request.url, environmentConfig.maxLogLine)
+  logger.info(`${request.method.toUpperCase()} to ${url}`)
+  //add timings to requests
+  request.metadata = {startTime: new Date()}
 
-    return request
+  return request
 }
 
 export function successInterceptor(response) {
-    response.config.metadata.endTime = new Date()
-    response.duration = response.config.metadata.endTime - response.config.metadata.startTime
+  response.config.metadata.endTime = new Date()
+  response.duration = response.config.metadata.endTime - response.config.metadata.startTime
 
-    const logger = log4jui.getLogger('return')
+  const logger = log4jui.getLogger('return')
 
-    const url = shorten(response.config.url, environmentConfig.maxLogLine)
+  const url = shorten(response.config.url, environmentConfig.maxLogLine)
 
-    logger.trackRequest({
-        duration: response.duration,
-        name: `Service ${response.config.method.toUpperCase()} call`,
-        resultCode: response.status,
-        success: true,
-        url: response.config.url,
-    })
+  logger.trackRequest({
+    duration: response.duration,
+    name: `Service ${response.config.method.toUpperCase()} call`,
+    resultCode: response.status,
+    success: true,
+    url: response.config.url,
+  })
 
-    logger.info(`Success on ${response.config.method.toUpperCase()} to ${url} (${response.duration})`)
-    return response
+  logger.info(`Success on ${response.config.method.toUpperCase()} to ${url} (${response.duration})`)
+  return response
 }
 
 export function errorInterceptor(error) {
-    const errorConfig = error.config
-    if (errorConfig && errorConfig.metadata) {
-        errorConfig.metadata.endTime = new Date()
-        error.duration = errorConfig.metadata.endTime - errorConfig.metadata.startTime
-    }
+  const errorConfig = error.config
+  if (errorConfig && errorConfig.metadata) {
+    errorConfig.metadata.endTime = new Date()
+    error.duration = errorConfig.metadata.endTime - errorConfig.metadata.startTime
+  }
 
-    const logger = log4jui.getLogger('return')
+  const logger = log4jui.getLogger('return')
 
-    let url = ''
-    if (errorConfig.url) {
-        url = shorten(errorConfig.url, environmentConfig.maxLogLine)
-    }
+  let url = ''
+  if (errorConfig.url) {
+    url = shorten(errorConfig.url, environmentConfig.maxLogLine)
+  }
 
-    // application insights logging
-    logger.trackRequest(getTrackRequestObj(error))
+  // application insights logging
+  logger.trackRequest(getTrackRequestObj(error))
 
-    if (errorConfig.method && error.response) {
-        let data = valueOrNull(error, 'response.data.details')
-        if (!data) {
-            data = valueOrNull(error, 'response.status') ? JSON.stringify(error.response.data, null, 2) : null
-            logger.error(`Error on ${errorConfig.method.toUpperCase()} to ${url} in (${error.duration}) - ${error} \n
+  if (errorConfig.method && error.response) {
+    let data = valueOrNull(error, 'response.data.details')
+    if (!data) {
+      data = valueOrNull(error, 'response.status') ? JSON.stringify(error.response.data, null, 2) : null
+      logger.error(`Error on ${errorConfig.method.toUpperCase()} to ${url} in (${error.duration}) - ${error} \n
             ${exceptionFormatter(data, exceptionOptions)}`)
-        } else {
-            logger.error(`Error on ${errorConfig.method.toUpperCase()} to ${url} in (${error.duration}) - ${error} \n
+    } else {
+      logger.error(`Error on ${errorConfig.method.toUpperCase()} to ${url} in (${error.duration}) - ${error} \n
             ${JSON.stringify(data)}`)
-        }
     }
+  }
 
-    errorStack.push(['request', JSON.parse(stringify(error.request))])
-    errorStack.push(['response', JSON.parse(stringify(error.response))])
-    return Promise.reject(error.response)
+  if (!exists(error, 'request')) {
+    error.request = {url}
+  }
+
+  if (!exists(error, 'response')) {
+    error.response = {url, 'message': 'No api response'}
+  }
+
+  errorStack.push(['request', JSON.parse(stringify(error.request))])
+  errorStack.push(['response', JSON.parse(stringify(error.response))])
+  return Promise.reject(error.response)
 }
