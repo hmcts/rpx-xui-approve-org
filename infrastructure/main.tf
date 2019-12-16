3775783c3885894c4d4a53babece5c655adbd718
Philip <philip.aberneithy@gmail.com>
2019-12-16 14:57:21 +0000

    Working through.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,6 +32,7 @@ module "app" {
         PACKAGES_PROJECT = "${var.team_name}"
         PACKAGES_ENVIRONMENT = "${var.env}"
         PUI_ENV = "${var.env}"
+        NODE_ENV = "${var.env}"
 
         S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
         IDAM_SECRET = "${data.azurerm_key_vault_secret.oauth2_secret.value}"
