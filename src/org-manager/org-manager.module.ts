c5a8710e68273eb6398949f11946d4621e2ff1c5
Elviani Huseyin <elviani_s@hotmail.com>
2019-12-19 15:52:37 +0000

    updating pipes import

--- src/org-manager/org-manager.module.ts
+++ src/org-manager/org-manager.module.ts
@@ -1,21 +1,15 @@
-import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {orgManagerRouting} from './org-manager.routing';
-import {SharedModule} from '../shared/shared.module';
-
-// containers
-import * as fromContainers from './containers';
-
-// components
-import * as fromComponents from './components';
-
-// services
-import * as fromServices from './services';
-import {StoreModule} from '@ngrx/store';
 import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
-import { reducers, effects } from './store';
-import { FilterOrganisationsPipe } from './pipes/filter-organisations.pipe';
+import {StoreModule} from '@ngrx/store';
+import {SharedModule} from '../shared/shared.module';
+import * as fromComponents from './components';
+import * as fromContainers from './containers';
+import {orgManagerRouting} from './org-manager.routing';
+import * as fromPipes from './pipes';
+import * as fromServices from './services';
+import { effects, reducers } from './store';
 
 @NgModule({
   imports: [
@@ -27,7 +21,7 @@ import { FilterOrganisationsPipe } from './pipes/filter-organisations.pipe';
     EffectsModule.forFeature(effects)
   ],
   exports: [...fromContainers.containers],
-  declarations: [...fromContainers.containers, ...fromComponents.components, FilterOrganisationsPipe],
+  declarations: [...fromContainers.containers, ...fromComponents.components, ...fromPipes.pipes],
   providers: [...fromServices.services]
 })
 
