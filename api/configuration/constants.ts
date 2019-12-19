92af8e189e3da1f1db4ab06c2d2ca615aebcb777
Philip <philip.aberneithy@gmail.com>
2019-12-19 10:42:14 +0000

    Changing NODE_CONFIG_ENV to prod to check that it updates which .yaml file it looks at.

--- api/configuration/constants.ts
+++ api/configuration/constants.ts
@@ -1,28 +1,40 @@
-export const CCD_DATA_API_CONFIG_REF = 'services.ccdDataApi'
-export const COOKIE_TOKEN = 'cookies.token'
-export const SECURE_COOKIE = 'secureCookie'
-export const SESSION_SECRET = 'sessionSecret'
-export const NOW = 'now'
-export const APP_INSIGHTS_KEY = 'appInsightsInstrumentationKey'
-export const MICROSERVICE = 'microservice'
-export const LOGGING = 'logging'
-export const PROXY_HOST = 'proxyConfig.host'
-export const PROXY_PORT = 'proxyConfig.port'
-export const COOKIES_USERID = 'cookies.userId'
-export const MAX_LINES = 'exceptionOptions.maxLines'
-export const IDAM_CLIENT = 'idamClient'
+/**
+ * References to the configuration parameters contained with
+ * the /config .yaml files.
+ *
+ * TODO: Unit test
+ * TODO: This needs to look exactly like the .yaml file.
+ *
+ * This files structure should be representative of the .yaml
+ * files in /config
+ */
 export const ENVIRONMENT = 'environment'
-export const MAX_LOG_LINE = 'maxLogLine'
+
+export const APP_INSIGHTS_KEY = 'appInsightsInstrumentationKey'
+
+export const COOKIE_TOKEN = 'cookies.token'
+export const COOKIES_USERID = 'cookies.userId'
+
+export const MAX_LINES = 'exceptionOptions.maxLines'
+
 export const INDEX_URL = 'indexUrl'
+export const MAX_LOG_LINE = 'maxLogLine'
+export const IDAM_CLIENT = 'idamClient'
+export const MICROSERVICE = 'microservice'
+export const NOW = 'now'
 export const OAUTH_CALLBACK_URL = 'oauthCallbackUrl'
 export const PROTOCOL = 'protocol'
+export const SECURE_COOKIE = 'secureCookie'
 
-/**
- * Services
- */
-export const SERVICE_S2S_PATH = 'services.s2s'
+export const SERVICES_CCD_DATA_API_PATH = 'services.ccdDataApi'
+export const SERVICES_CCD_DEF_API_PATH = 'services.ccdDefApi'
 export const SERVICES_IDAM_API_PATH = 'services.idamApi'
 export const SERVICES_IDAM_WEB = 'services.idamWeb'
 export const SERVICES_RD_PROFESSIONAL_API_PATH = 'services.rdProfessionalApi'
-export const SERVICES_CCD_DATA_API_PATH = 'services.ccdDataApi'
-export const SERVICES_CCD_DEF_API_PATH = 'services.ccdDefApi'
+export const SERVICE_S2S_PATH = 'services.s2s'
+
+export const SESSION_SECRET = 'sessionSecret'
+export const PROXY_HOST = 'proxyConfig.host'
+export const PROXY_PORT = 'proxyConfig.port'
+
+export const LOGGING = 'logging'
