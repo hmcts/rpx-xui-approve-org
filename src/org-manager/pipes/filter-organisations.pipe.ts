12827eff72f6f777c41106f34e0a9b21c9bac45e
Elviani Huseyin <elviani_s@hotmail.com>
2019-12-23 11:11:18 +0000

    update to make it readable

--- src/org-manager/pipes/filter-organisations.pipe.ts
+++ src/org-manager/pipes/filter-organisations.pipe.ts
@@ -11,13 +11,17 @@ export class FilterOrganisationsPipe implements PipeTransform {
     if (!searchFilter || searchFilter === '') { return orgs; }
     searchFilter = searchFilter.toLowerCase();
     return orgs.filter((org: OrganisationVM) => {
-      return org && (org.name && org.name.toLowerCase().includes(searchFilter)) ||
-      (org.postCode && org.postCode.toLowerCase().includes(searchFilter)) ||
-      (org.sraId && org.sraId.toLowerCase().includes(searchFilter)) ||
-      (org.pbaNumber && org.pbaNumber.length >= 1 && org.pbaNumber[0].toLowerCase().includes(searchFilter)) ||
-      (org.pbaNumber && org.pbaNumber.length >= 2 && org.pbaNumber[org.pbaNumber.length - 1].toLowerCase().includes(searchFilter)) ||
-      (org.dxNumber && org.dxNumber.length >= 1 && org.dxNumber[0] && org.dxNumber[0].dxNumber && org.dxNumber[0].dxNumber.toLowerCase().includes(searchFilter) ) ||
-      (org.dxNumber && org.dxNumber.length >= 1 && org.dxNumber[0] && org.dxNumber[0].dxExchange && org.dxNumber[0].dxExchange.toLowerCase().includes(searchFilter));
+      if (org) {
+        const isIncludeInName = org.name && org.name.toLowerCase().includes(searchFilter);
+        const isIncludeInPostcode = org.postCode && org.postCode.toLowerCase().includes(searchFilter);
+        const isIncludeInSraId = org.sraId && org.sraId.toLowerCase().includes(searchFilter);
+        const isIncludeInPbaNumber1 = org.pbaNumber && org.pbaNumber.length >= 1 && org.pbaNumber[0].toLowerCase().includes(searchFilter);
+        const isIncludeInPbaNumber2 = org.pbaNumber && org.pbaNumber.length >= 2 && org.pbaNumber[org.pbaNumber.length - 1].toLowerCase().includes(searchFilter);
+        const isIncludeInDxNumber = org.dxNumber && org.dxNumber.length >= 1 && org.dxNumber[0] && org.dxNumber[0].dxNumber && org.dxNumber[0].dxNumber.toLowerCase().includes(searchFilter);
+        const isIncludeInDxExchange = org.dxNumber && org.dxNumber.length >= 1 && org.dxNumber[0] && org.dxNumber[0].dxExchange && org.dxNumber[0].dxExchange.toLowerCase().includes(searchFilter);
+        return isIncludeInName || isIncludeInPostcode || isIncludeInSraId || isIncludeInPbaNumber1 || isIncludeInPbaNumber2 || isIncludeInDxNumber || isIncludeInDxExchange;
+      }
+      return false;
     });
   }
 }
