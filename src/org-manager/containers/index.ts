aa4b5f0f95d184f90894df3bf26db9a0298ef00f
Elviani <elviani@users.noreply.github.com>
2020-01-28 14:42:17 +0000

    remove the gov-ui library and integrate with common lib gov ui (#206)

--- src/org-manager/containers/index.ts
+++ src/org-manager/containers/index.ts
@@ -1,9 +1,9 @@
 import { ActiveOrganisationsComponent } from './active-organisations/active-organisations.component';
-import { PendingOrganisationsComponent } from './pending-organisations/pending-organisations.component';
-import { ApproveOrganisationComponent } from './approve-organisation/approve-organisation.component';
 import { ApproveOrganisationSuccessComponent } from './approve-organisation-success/approve-organisation-success.component';
-import { SearchOrganisationsFormComponent } from './search-organisations-form/search-organisations-form.component';
+import { ApproveOrganisationComponent } from './approve-organisation/approve-organisation.component';
 import {EditDetailsComponent} from './edit-details/edit-details.component';
+import { PendingOrganisationsComponent } from './pending-organisations/pending-organisations.component';
+import { SearchOrganisationsFormComponent } from './search-organisations-form/search-organisations-form.component';
 
 export const containers: any[] = [
   ActiveOrganisationsComponent,
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
-import {GovUiModule} from '../../projects/gov-ui/src/lib/gov-ui.module';
 import {ReactiveFormsModule} from '@angular/forms';
+import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
+import { LoggerService } from 'src/app/services/logger.service';
 import {IdentityBarComponent} from './components/identity-bar/identity-bar.component';
 import { PhaseBannerComponent } from './components/phase-banner/phase-banner.component';
+import { DefaultErrorHandler } from './errorHandler/defaultErrorHandler';
 import { HealthCheckGuard } from './guards/health-check.guard';
 import { HealthCheckService } from './services/health-check.service';
-import { LoggerService } from 'src/app/services/logger.service';
-import { DefaultErrorHandler } from './errorHandler/defaultErrorHandler';
 
 @NgModule( {
   imports: [
     ReactiveFormsModule,
-    GovUiModule,
+    ExuiCommonLibModule.forChild()
   ],
   exports: [
     ReactiveFormsModule,
-    GovUiModule,
     IdentityBarComponent,
     PhaseBannerComponent
   ],
