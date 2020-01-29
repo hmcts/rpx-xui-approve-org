c5c646a4bbe225efc6890a5d3c8f8a15aa0b9d90
fazanki <fazanki@gmail.com>
2020-01-29 13:47:03 +0000

    adding reducer

--- src/app/store/reducers/app.reducer.ts
+++ src/app/store/reducers/app.reducer.ts
@@ -1,12 +1,26 @@
 import * as fromAction from '../actions';
 import {AppUtils} from '../../utils/app-utils';
+import {UserModel} from '../../../models/user.model';
 
 export interface AppState {
   pageTitle: string;
+  userDetails: UserModel | null;
+  modal: {[id: string]: {isVisible?: boolean; countdown?: string}};
+  loaded: boolean;
+  loading: boolean;
 }
 
 export const initialState: AppState = {
-  pageTitle: ''
+  pageTitle: '',
+  userDetails: null,
+  modal: {
+    session: {
+      isVisible: false,
+      countdown: ''
+    }
+  },
+  loaded: false,
+  loading: false
 };
 
 export function reducer(
@@ -30,6 +44,23 @@ export function reducer(
       };
     }
 
+    case fromAction.GET_USER_DETAILS_SUCCESS: {
+      const userDetails = new UserModel(action.payload);
+      return {
+        ...state,
+        userDetails,
+        loaded: true,
+        loading: false,
+      };
+    }
+
+    case fromAction.SET_MODAL: {
+      return {
+        ...state,
+        modal: {...action.payload}
+      };
+    }
+
     case fromAction.LOGOUT: {
       return {
         ...state,
@@ -42,5 +73,7 @@ export function reducer(
 }
 
 export const getPageTitle = (state: AppState) => state.pageTitle;
+export const getUserDetails = (state: AppState) => state.userDetails;
+export const getModal = (state: AppState) => state.modal;
 
 
