98c1de486022e5254a96857f159beec759683233
fazanki <fazanki@gmail.com>
2020-01-30 13:39:23 +0000

    adding new authentication

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -1,169 +1,149 @@
-import axios, { AxiosResponse } from 'axios'
-import * as express from 'express'
-import * as jwtDecode from 'jwt-decode'
-import {environmentConfig, getEnvConfig} from '../lib/environment.config'
-import { http } from '../lib/http'
+import axios from 'axios'
+import { NextFunction, Request, Response, Router } from 'express'
+import * as net from 'net'
+import {Client, ClientMetadata, Issuer, Strategy, TokenSet, UserinfoResponse} from 'openid-client'
+import * as passport from 'passport'
+import * as process from 'process'
+import {router as keepAlive} from '../keepalive'
+import {environmentConfig} from '../lib/environment.config'
 import * as log4jui from '../lib/log4jui'
-import { EnhancedRequest } from '../lib/models'
-import { asyncReturnOrError } from '../lib/util'
-import { getUserDetails } from '../services/idam'
-import { serviceTokenGenerator } from './serviceToken'
+import {propsExist} from '../lib/objectUtilities'
+import {app} from '../local'
 
-const idamUrl = environmentConfig.services.idamApi
-const secret = getEnvConfig<string>('IDAM_SECRET', 'string')
+export const router = Router({mergeParams: true})
+
+const cookieToken = environmentConfig.cookies.token
+const cookieUserId = environmentConfig.cookies.userId
+const idamURl = environmentConfig.services.idamApi
+
 const logger = log4jui.getLogger('auth')
 
-export async function attach(req: EnhancedRequest, res: express.Response, next: express.NextFunction) {
-    const session = req.session!
-    const accessToken = req.cookies[environmentConfig.cookies.token]
+export async function configureIssuer(url: string) {
+    let issuer: Issuer<Client>
 
-    let expired
+    logger.info('getting oidc discovery endpoint')
+    issuer = await Issuer.discover(`${url}/o`)
 
-    if (accessToken) {
-        const jwtData = jwtDecode(accessToken)
-        const expires = new Date(jwtData.exp as string).getTime()
-        const now = new Date().getTime() / 1000
-        expired = expires < now
-    }
-    if (!accessToken || expired) {
-        logger.info('Auth Token expired!')
-        doLogout(req, res, 401)
-    } else {
-        const check = await sessionChainCheck(req, res, accessToken)
-        if (check) {
-            logger.info('Attaching auth')
+    const metadata = issuer.metadata
+    metadata.issuer = environmentConfig.services.iss
 
-            // also use these as axios defaults
-            logger.info('Using Idam Token in defaults')
-            axios.defaults.headers.common.Authorization = `Bearer ${session.auth.token}`
-
-            const token = await asyncReturnOrError(serviceTokenGenerator(), 'Error getting s2s token', res, logger)
-            if (token) {
-                logger.info('Using S2S Token in defaults')
-                axios.defaults.headers.common.ServiceAuthorization = token
-            } else {
-                logger.warn('Cannot attach S2S token ')
-                doLogout(req, res, 401)
-            }
-        }
-
-        next()
-    }
+    return new Issuer(metadata)
 }
 
-export async function getTokenFromCode(req: express.Request, res: express.Response): Promise<AxiosResponse> {
-    console.log(`${environmentConfig.idamClient}:${secret}`)
-    const Authorization = `Basic ${new Buffer(`${environmentConfig.idamClient}:${secret}`).toString('base64')}`
-    const options = {
-        headers: {
-            Authorization,
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
+export async function configure(req: Request, res: Response, next: NextFunction) {
+
+    const host = req.get('host')
+    const fqdn = req.protocol + '://' + host
+
+    // we don't want to configure strategy if coming from direct IP address (e.g. could be health endpoint)
+    if (net.isIP(host)) {
+        return next()
     }
 
-    logger.info('Getting Token from auth code.')
+    // if client is already configured, exit early
+    if (app.locals.client) {
+        return next()
+    }
 
-    const url = `${environmentConfig.services.idamApi}/oauth2/token?grant_type=authorization_code&code=${req.query.code}&redirect_uri=${
-    environmentConfig.protocol
-  }://${req.headers.host}${environmentConfig.oauthCallbackUrl}`
-
-    return http.post(url, {}, options)
-}
-
-async function sessionChainCheck(req: EnhancedRequest, res: express.Response, accessToken: string) {
-    if (!req.session.auth) {
-        logger.warn('Session expired. Trying to get user details again')
-        console.log(getUserDetails(accessToken, idamUrl))
-        const details = await asyncReturnOrError(getUserDetails(accessToken, idamUrl), 'Cannot get user details', res, logger, false)
-
-        if (details) {
-            logger.info('Setting session')
-
-            //const orgIdResponse = await getOrganisationId(details)
-            // no real end point
-            const orgIdResponse = {
-                data: {
-                    id: '1',
-                },
-            }
-            req.session.auth = {
-                email: details.data.email,
-                orgId: orgIdResponse.data.id,
-                roles: details.data.roles,
-                token: accessToken,
-                userId: details.data.id,
-            }
+    if (!app.locals.issuer) {
+        try {
+            app.locals.issuer = await configureIssuer(idamURl)
+        } catch (error) {
+            return next(error)
         }
     }
+    logger.info('fqdn: ', fqdn)
 
-    if (!req.session.auth) {
-        logger.warn('Auth token  expired need to log in again')
-        doLogout(req, res, 401)
-        return false
+    const clientMetadata: ClientMetadata = {
+        client_id: environmentConfig.idamClient,
+        client_secret: process.env.IDAM_SECRET,
+        post_logout_redirect_uris: [`${fqdn}/auth/login`],
+        redirect_uris: [`${fqdn}/oauth2/callback`],
+        response_types: ['code'],
+        token_endpoint_auth_method: 'client_secret_post', // The default is 'client_secret_basic'.
     }
 
-    return true
+    app.locals.client = new app.locals.issuer.Client(clientMetadata)
+
+    logger.info('configuring strategy')
+
+    passport.use('oidc', new Strategy({
+        client: app.locals.client,
+        params: {scope: 'profile openid roles manage-user create-user'},
+        sessionKey: 'xui_webapp', // being explicit here so we can set manually on logout
+        usePKCE: false, // issuer doesn't support pkce - no code_challenge_methods_supported
+    }, oidcVerify))
+
+    next()
+
+    // passport.use('s2s', new BearerStrategy())
 }
 
-export function havePrdAdminRole(userData) {
-  return userData.data.roles.indexOf('prd-admin') === -1
-}
+export async function doLogout(req: Request, res: Response, status = 302) {
 
-export async function oauth(req: EnhancedRequest, res: express.Response, next: express.NextFunction) {
-    const response = await getTokenFromCode(req, res)
-    const accessToken = response.data.access_token
+    req.query.redirect = app.locals.client.endSessionUrl({
+        id_token_hint: req.session.passport.user.tokenset.id_token,
+        // TODO: we can generate a random state and use that
+        // post_logout_redirect_uri: app.locals.client.authorizationUrl({ state: 'testState' })
+    })
 
-    if (accessToken) {
-        const userDetails = await asyncReturnOrError(getUserDetails(accessToken, idamUrl), 'Cannot get user details', res, logger, false)
-        const isPrdAdminRole = havePrdAdminRole(userDetails)
-        if (isPrdAdminRole) {
-            console.log('THIS USER CAN NOT LOGIN');
-            // tslint:disable-next-line
-            res.redirect(`${environmentConfig.services.idamApi}/login?response_type=code&client_id=${environmentConfig.idamClient}&redirect_uri=${environmentConfig.protocol}://${req.headers.host}/oauth2/callback&scope=profile openid roles manage-user create-user manage-roles`)
-            return false
-        }
-        // set browser cookie
-        res.cookie(environmentConfig.cookies.token, accessToken)
+    delete axios.defaults.headers.common.Authorization
+    delete axios.defaults.headers.common['user-roles']
 
-        const jwtData: any = jwtDecode(accessToken)
-        const expires = new Date(jwtData.exp as string).getTime()
-        const now = new Date().getTime() / 1000
-        const expired = expires < now
+    res.clearCookie('roles')
+    res.clearCookie(cookieToken)
+    res.clearCookie(cookieUserId)
 
-        if (expired) {
-            logger.warn('Auth token  expired need to log in again')
-            doLogout(req, res, 401)
+    //passport provides this method on request object
+    req.logout()
+
+    req.session.destroy( () => {
+        if (req.query.redirect || status === 401) {  // 401 is when no accessToken
+            res.redirect(status, req.query.redirect || '/')
+            console.log('Logged out by userDetails')
         } else {
-            const check = await sessionChainCheck(req, res, accessToken)
-            if (check) {
-              axios.defaults.headers.common.Authorization = `Bearer ${req.session.auth.token}`
-              axios.defaults.headers.common['user-roles'] = req.session.auth.roles
-
-              if (req.headers.ServiceAuthorization) {
-                axios.defaults.headers.common.ServiceAuthorization = req.headers.ServiceAuthorization
-              }
-
-              logger.info('save session', req.session)
-              req.session.save(() => {
-                res.redirect(environmentConfig.indexUrl || '/')
-              })
-            }
+            const message = JSON.stringify({message: 'You have been logged out!'})
+            res.status(200).send(message)
+            console.log('Logged out by Session')
         }
-    } else {
-        logger.error('No auth token')
-        res.redirect(environmentConfig.indexUrl || '/')
-    }
-}
-
-export function doLogout(req: EnhancedRequest, res: express.Response, status: number = 302) {
-    res.clearCookie(environmentConfig.cookies.token)
-    res.clearCookie(environmentConfig.cookies.userId)
-    req.session.user = null
-    req.session.save(() => {
-        res.redirect(status, req.query.redirect || '/')
     })
 }
 
-export function logout(req, res) {
-    doLogout(req, res, 200)
+export async function oidcVerify(tokenset: TokenSet, userinfo: UserinfoResponse, done: any) {
+
+    if (!propsExist(userinfo, ['roles'])) {
+        logger.warn('User does not have any access roles.')
+        return done(null, false, {message: 'User does not have any access roles.'})
+    }
+
+    return done(null, {tokenset, userinfo})
+
 }
+
+export function authCallbackSuccess(req: Request, res: Response) {
+    // console.log('callback', req.session)
+
+    // we need extra logic before success redirect
+    const userDetails = req.session.passport.user
+    const roles = userDetails.userinfo.roles
+
+    axios.defaults.headers.common.Authorization = `Bearer ${userDetails.tokenset.access_token}`
+    axios.defaults.headers.common['user-roles'] = roles.join()
+
+    res.cookie(cookieUserId, userDetails.userinfo.uid)
+    res.cookie(cookieToken, userDetails.tokenset.access_token)
+    res.cookie('roles', roles)
+
+    // need this so angular knows which enviroment config to use ...
+    res.cookie('platform', environmentConfig.configEnv)
+
+    res.redirect('/')
+}
+
+router.get('/logout', (req: Request, res: Response) => {
+    doLogout(req, res)
+})
+
+router.get('/login', passport.authenticate('oidc'))
+
+router.use('/keepalive', keepAlive)
