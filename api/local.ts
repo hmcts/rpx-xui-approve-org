fd9285a029f161d4cd2c0d9cf3504ecc74e3f15d
fazanki <fazanki@gmail.com>
2020-01-30 13:39:23 +0000

    adding new authentication

--- api/local.ts
+++ api/local.ts
@@ -1,5 +1,4 @@
 import * as bodyParser from 'body-parser'
-import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 import * as session from 'express-session'
 import * as process from 'process'
@@ -11,9 +10,10 @@ import { errorStack } from './lib/errorStack'
 import * as log4jui from './lib/log4jui'
 import * as tunnel from './lib/tunnel'
 import routes from './routes'
+import * as passport from 'passport'
 
 const FileStore = sessionFileStore(session)
-const app = express()
+export const app = express()
 const logger = log4jui.getLogger('server')
 
 app.use(
@@ -39,9 +39,41 @@ app.use(errorStack)
 app.use(appInsights)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/oauth2/callback', auth.oauth)
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((id, done) => {
+  done(null, id)
+})
+
+app.get('/oauth2/callback',  (req: any, res, next) => {
+  passport.authenticate('oidc', (error, user, info) => {
+
+    // TODO: give a more meaningful error to user rather than redirect back to idam
+    // return next(error) would pass off to error.handler.ts to show users a proper error page etc
+    if (error) {
+      logger.error(error)
+      // return next(error)
+    }
+    if (info) {
+      logger.info(info)
+      // return next(info)
+    }
+    if (!user) {
+      return res.redirect('/auth/login')
+    }
+    req.logIn(user, err => {
+      if (err) {
+        return next(err)
+      }
+      return auth.authCallbackSuccess(req, res)
+    })
+  })(req, res, next)
+})
 
 app.use('/api', routes)
 
