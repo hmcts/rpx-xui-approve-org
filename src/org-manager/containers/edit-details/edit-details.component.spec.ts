52551fda10f716d435f12c0b369b8040b1627f3a
Elviani Huseyin <elviani_s@hotmail.com>
2020-02-03 16:01:57 +0000

    fixing the broken approve organisation review re-routing

--- src/org-manager/containers/edit-details/edit-details.component.spec.ts
+++ src/org-manager/containers/edit-details/edit-details.component.spec.ts
@@ -1,12 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
 import { combineReducers, Store, StoreModule } from '@ngrx/store';
 import * as fromRoot from '../../../app/store';
 import * as fromOrganisationPendingStore from '../../store';
 import { EditDetailsComponent } from './edit-details.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
