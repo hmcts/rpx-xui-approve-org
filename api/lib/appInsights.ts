9f676c3da8d9b2074faabee5618448886d44522b
Philip a <philip.aberneithy@gmail.com>
2020-02-05 17:37:30 +0000

    Config continuing.

--- api/lib/appInsights.ts
+++ api/lib/appInsights.ts
@@ -8,27 +8,27 @@ export let client
 
 // shouldnt do this check here but this is a high level dep
 
-if (getEnvironment() !== DEVELOPMENT) {
-    console.log('environmentConfig.appInsightsInstrumentationKey is ' + getConfigValue(APP_INSIGHTS_KEY))
-    applicationinsights
-        .setup(getConfigValue(APP_INSIGHTS_KEY))
-        .setAutoDependencyCorrelation(true)
-        .setAutoCollectRequests(true)
-        .setAutoCollectPerformance(true)
-        .setAutoCollectExceptions(true)
-        .setAutoCollectDependencies(true)
-        .setAutoCollectConsole(true)
-        .setSendLiveMetrics(true)
-        .setUseDiskRetryCaching(true)
-        .setSendLiveMetrics(true)
-        .start()
-
-    client = applicationinsights.defaultClient
-    client.trackTrace({ message: 'App Insight Activated' })
-
-} else {
-    client = null
-}
+// if (getEnvironment() !== DEVELOPMENT) {
+//     // console.log('environmentConfig.appInsightsInstrumentationKey is ' + getConfigValue(APP_INSIGHTS_KEY))
+//     applicationinsights
+//         .setup(getConfigValue(APP_INSIGHTS_KEY))
+//         .setAutoDependencyCorrelation(true)
+//         .setAutoCollectRequests(true)
+//         .setAutoCollectPerformance(true)
+//         .setAutoCollectExceptions(true)
+//         .setAutoCollectDependencies(true)
+//         .setAutoCollectConsole(true)
+//         .setSendLiveMetrics(true)
+//         .setUseDiskRetryCaching(true)
+//         .setSendLiveMetrics(true)
+//         .start()
+//
+//     client = applicationinsights.defaultClient
+//     client.trackTrace({ message: 'App Insight Activated' })
+//
+// } else {
+//     client = null
+// }
 
 export function appInsights(req: express.Request, res: express.Response, next) {
     if (client) {
