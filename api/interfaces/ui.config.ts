aca718427a951ea9b9ddb2a8fc04fce5922e99c8
Pardeep Singh Basi <paddybasi@gmail.com>
2020-02-13 11:01:36 +0000

    refactoring config files and stripping out old libraries. Also fixed issue with scss files

---
+++ api/interfaces/ui.config.ts
@@ -0,0 +1,38 @@
+export interface UIConfig {
+  configEnv: string,
+  cookies: UIConfigCookies,
+  exceptionOptions: UIConfigExceptionOptions,
+  health: UIConfigServices,
+  idamClient: string,
+  indexUrl: string,
+  logging: string,
+  now: boolean,
+  maxLogLine: number,
+  microservice: string,
+  oauthCallbackUrl: string,
+  protocol: string,
+  proxy: string,
+  noProxy: string,
+  secureCookie: boolean,
+  services: UIConfigServices,
+  sessionSecret: string,
+}
+
+export interface UIConfigCookies {
+  token: string,
+  userId: string,
+}
+
+export interface UIConfigExceptionOptions {
+  maxLines: number,
+}
+
+export interface UIConfigServices {
+  ccdDataApi: string,
+  ccdDefApi: string,
+  idamApi: string,
+  idamWeb: string,
+  rdProfessionalApi: string,
+  s2s: string,
+  feeAndPayApi: string
+}
