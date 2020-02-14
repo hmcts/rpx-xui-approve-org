ab5e0a5410eb5abe7c5a7d77bc6d82c780e227d3
Pardeep Singh Basi <paddybasi@gmail.com>
2020-02-14 11:00:02 +0000

    clean up

--- api/lib/tunnel.ts
+++ api/lib/tunnel.ts
@@ -1,14 +1,16 @@
 import { createGlobalProxyAgent } from 'global-agent'
 import {getConfigValue, getEnvironment} from '../configuration'
 import {DEVELOPMENT} from '../configuration/constants'
-import {HTTP_PROXY, NO_PROXY} from '../configuration/references'
+import {AO_HTTP_PROXY, AO_NO_PROXY} from '../configuration/references'
 import * as log4jui from './log4jui'
 
 const logger = log4jui.getLogger('proxy')
 
 export function init(): void {
   if (getEnvironment() === DEVELOPMENT) {
-    logger.info('configuring global-agent: ', getConfigValue(HTTP_PROXY), 'no proxy:', getConfigValue(NO_PROXY))
-    createGlobalProxyAgent({})
+    logger.info('configuring global-agent: ', getConfigValue(AO_HTTP_PROXY), 'no proxy:', getConfigValue(AO_NO_PROXY))
+    createGlobalProxyAgent({
+      environmentVariableNamespace: 'AO',
+    })
   }
 }
