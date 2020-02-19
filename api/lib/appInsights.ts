1d7df5d21cf1b8dd47934e7bb1a6eb3eb744775f
Philip a <philip.aberneithy@gmail.com>
2020-02-19 14:46:26 +0000

    Update to readme, update to when app insights is used.

--- api/lib/appInsights.ts
+++ api/lib/appInsights.ts
@@ -1,34 +1,30 @@
 import * as applicationinsights from 'applicationinsights'
 import * as express from 'express'
 import {getConfigValue, getEnvironment} from '../configuration'
-import {DEVELOPMENT} from '../configuration/constants'
-import {APP_INSIGHTS_KEY} from '../configuration/references'
+import {APP_INSIGHTS_ENABLED, APP_INSIGHTS_KEY} from '../configuration/references'
 
 export let client
 
-// shouldnt do this check here but this is a high level dep
+if (getConfigValue(APP_INSIGHTS_ENABLED)) {
+    applicationinsights
+        .setup(getConfigValue(APP_INSIGHTS_KEY))
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectRequests(true)
+        .setAutoCollectPerformance(true)
+        .setAutoCollectExceptions(true)
+        .setAutoCollectDependencies(true)
+        .setAutoCollectConsole(true)
+        .setSendLiveMetrics(true)
+        .setUseDiskRetryCaching(true)
+        .setSendLiveMetrics(true)
+        .start()
 
-// if (getEnvironment() !== DEVELOPMENT) {
-//     // console.log('environmentConfig.appInsightsInstrumentationKey is ' + getConfigValue(APP_INSIGHTS_KEY))
-//     applicationinsights
-//         .setup(getConfigValue(APP_INSIGHTS_KEY))
-//         .setAutoDependencyCorrelation(true)
-//         .setAutoCollectRequests(true)
-//         .setAutoCollectPerformance(true)
-//         .setAutoCollectExceptions(true)
-//         .setAutoCollectDependencies(true)
-//         .setAutoCollectConsole(true)
-//         .setSendLiveMetrics(true)
-//         .setUseDiskRetryCaching(true)
-//         .setSendLiveMetrics(true)
-//         .start()
-//
-//     client = applicationinsights.defaultClient
-//     client.trackTrace({ message: 'App Insight Activated' })
-//
-// } else {
-//     client = null
-// }
+    client = applicationinsights.defaultClient
+    client.trackTrace({ message: 'App Insight Activated' })
+
+} else {
+    client = null
+}
 
 export function appInsights(req: express.Request, res: express.Response, next) {
     if (client) {
