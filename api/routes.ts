6c466c86cbafaebce81104d326c4a0d2eb0fbe6b
Uday Denduluri <uday.denduluri@gmail.com>
2020-03-11 15:25:30 +0000

    fix the lint errors

--- api/routes.ts
+++ api/routes.ts
@@ -4,10 +4,10 @@ import environment from './environment'
 import healthCheck from './healthCheck'
 import getAppInsightsInstrumentationKey from './monitoring-tools'
 import organisationRouter from './organisation'
+import pbaAccounts from './pbaAccounts'
 import stateRouter from './states'
 import pbaRouter from './update-pba'
 import userDetailsRouter from './user'
-
 
 const router = express.Router({ mergeParams: true })
 // open routes
@@ -17,6 +17,7 @@ router.use('/healthCheck', healthCheck)
 router.use('/organisations', organisationRouter)
 router.use('/update-pba', pbaRouter)
 router.use('/user', userDetailsRouter)
+router.use('/pbaAccounts', pbaAccounts)
 
 router.use('/monitoring-tools', getAppInsightsInstrumentationKey)
 export default router
