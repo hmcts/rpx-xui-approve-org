7f8082ce787efd27134d5e7aee57aae7f78608a1
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2020-03-13 13:13:01 +0000

    Fixed issue verifying Org list table loaded.

--- test/e2e/features/step_definitions/steps/confirmApproveOrganisation.steps.js
+++ test/e2e/features/step_definitions/steps/confirmApproveOrganisation.steps.js
@@ -18,10 +18,10 @@ async function waitForElement(el) {
 defineSupportCode(function ({ Given, When, Then,And }) {
 
   Then('I click first organization view link', async function(){
-    browser.sleep(AMAZING_DELAY);
-   // await organisationList.waitForOrgListToDisplay();
+    // browser.sleep(AMAZING_DELAY);
+   await organisationList.waitForOrgListToDisplay();
     await organisationList.clickViewOnFirstOrganisation();
-    browser.sleep(LONG_DELAY);
+    // browser.sleep(LONG_DELAY);
 
   });
 
