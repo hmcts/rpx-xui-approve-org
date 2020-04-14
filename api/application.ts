d4bf021dcd34fccba46647313b2a39d5a9d4be0a
Uday Denduluri <uday.denduluri@gmail.com>
2020-04-14 13:58:17 +0100

    lint fix

--- api/application.ts
+++ api/application.ts
@@ -133,8 +133,8 @@ const healthChecks = {
     idamApi: healthcheckConfig(getConfigValue(SERVICES_IDAM_API_PATH)),
     idamWeb: healthcheckConfig(getConfigValue(SERVICES_IDAM_WEB)),
     rdProfessionalApi: healthcheckConfig(getConfigValue(SERVICES_RD_PROFESSIONAL_API_PATH)),
-    s2s: healthcheckConfig(getConfigValue(SERVICE_S2S_PATH)),
     redis: () => ({status: 'UP'}),
+    s2s: healthcheckConfig(getConfigValue(SERVICE_S2S_PATH)),
   },
 }
 
