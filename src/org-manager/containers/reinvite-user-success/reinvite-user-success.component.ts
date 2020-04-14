ea88b8ad531e09d09bdc7957fe45c20d9440bc35
Elviani Huseyin <elviani_s@hotmail.com>
2020-04-14 13:35:30 +0100

    finishing work for resend invite

--- src/org-manager/containers/reinvite-user-success/reinvite-user-success.component.ts
+++ src/org-manager/containers/reinvite-user-success/reinvite-user-success.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from '@hmcts/rpx-xui-common-lib';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import * as fromRoot from '../../../app/store';
 import * as fromStore from '../../store';
 
 @Component({
@@ -11,10 +12,16 @@ import * as fromStore from '../../store';
 export class ReinviteUserSuccessComponent implements OnInit {
 
     public user$: Observable<User>;
+    public orgId: string;
 
     constructor(private readonly store: Store<fromStore.OrganisationRootState>) { }
 
     public ngOnInit() {
         this.user$ = this.store.pipe(select(fromStore.getSelectedUserSelector));
+        this.store.pipe(select(fromStore.getOrganisationIdSelector)).subscribe( id => this.orgId = id );
+    }
+
+    public onGoBack() {
+      this.store.dispatch(new fromRoot.Go({ path: ['/organisation-details', this.orgId] }));
     }
 }
