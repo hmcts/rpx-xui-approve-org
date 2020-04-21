69e06170e53f6d8ee017174c1761aa0ba519b6d8
Uday Denduluri <uday.denduluri@gmail.com>
2020-04-21 15:25:00 +0100

    feature-toggle for the auth service login redirect

--- api/configuration/ui.ts
+++ api/configuration/ui.ts
@@ -9,6 +9,7 @@ import {getConfigValue, getEnvironment, showFeature} from './index'
 import {
   COOKIE_TOKEN,
   COOKIES_USERID,
+  FEATURE_OIDC_ENABLED,
   FEATURE_SECURE_COOKIE_ENABLED,
   IDAM_CLIENT,
   INDEX_URL,
@@ -50,6 +51,7 @@ export const uiConfig = (): UIConfig => {
     microservice: getConfigValue(MICROSERVICE),
     now: getConfigValue(NOW),
     oauthCallbackUrl: getConfigValue(OAUTH_CALLBACK_URL),
+    oidcEnabled: showFeature(FEATURE_OIDC_ENABLED),
     protocol: getConfigValue(PROTOCOL),
     secureCookie: showFeature(FEATURE_SECURE_COOKIE_ENABLED),
     services: {
