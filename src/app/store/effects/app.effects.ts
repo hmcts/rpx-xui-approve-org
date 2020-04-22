7531baa5049d7b0a309e17778425904d3a2830f7
Uday Denduluri <uday.denduluri@gmail.com>
2020-04-22 17:01:32 +0100

    open-id connect UI changes

--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -1,21 +1,64 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 
+import { HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { UserInterface } from '../../..//models/user.model';
+import { LogOutKeepAliveService } from '../../services/keep-alive/keep-alive.service';
+import { UserService } from '../../services/user-service/user.service';
 import * as appActions from '../actions';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AppEffects {
     constructor(
-        private actions$: Actions,
+        private readonly actions$: Actions,
+        private readonly logOutService: LogOutKeepAliveService,
+        private readonly userService: UserService
     ) { }
 
     @Effect({ dispatch: false })
-    logout$ = this.actions$.pipe(
+    public logout$ = this.actions$.pipe(
         ofType(appActions.LOGOUT),
         map(() => {
             window.location.href = '/api/logout';
         })
     );
 
+    @Effect()
+    public sigout$ = this.actions$.pipe(
+        ofType(appActions.SIGNED_OUT),
+        switchMap(() => {
+            return this.logOutService.logOut().pipe(
+                map(() => new appActions.SignedOutSuccess())
+            );
+        })
+    );
+
+    @Effect()
+    public signedOutSuccess$ = this.actions$.pipe(
+        ofType(appActions.SIGNED_OUT_SUCCESS),
+        map(() => new appActions.Go({path: ['/signed-out']}))
+    );
+
+    @Effect({ dispatch: false})
+    public keepAlive$ = this.actions$.pipe(
+        ofType(appActions.KEEP_ALIVE),
+        switchMap((date) => {
+            return this.logOutService.heartBeat()
+                ;
+        })
+    );
+
+    @Effect()
+    public getUser$ = this.actions$.pipe(
+        ofType(appActions.GET_USER_DETAILS),
+        switchMap(() => {
+            return this.userService.getUserDetails()
+                .pipe(
+                    map((userDetails: UserInterface) => new appActions.GetUserDetailsSuccess(userDetails)),
+                    catchError((error: HttpErrorResponse) => of(new appActions.GetUserDetailsFailure(error)))
+                );
+        })
+    );
 }
