bfec0798e8f58390abdd98cc4b1ad6c739a9960b
Pokala <krishna.pokala@atos.net>
2020-05-15 13:48:23 +0100

    increasd coverage

--- src/app/store/effects/app.effects.spec.ts
+++ src/app/store/effects/app.effects.spec.ts
@@ -5,8 +5,7 @@ import { StoreModule } from '@ngrx/store';
 import { cold, hot } from 'jasmine-marbles';
 import { LogOutKeepAliveService } from '../../services/keep-alive/keep-alive.service';
 import { UserService } from '../../services/user-service/user.service';
-import { AddGlobalError,Go,SignedOutSuccess,KeepAlive,SignedOut,Logout } from '../actions';
-
+import { AddGlobalError, Go, SignedOutSuccess, KeepAlive, SignedOut, Logout } from '../actions';
 import * as fromAppEffects from './app.effects';
 
 describe('App Effects', () => {
@@ -27,14 +26,11 @@ describe('App Effects', () => {
                 {provide: UserService, useValue: mockUserService}
             ]
         });
-
         effects = TestBed.get(fromAppEffects.AppEffects);
-
     });
 
     describe('addGlobalErrorEffect$', () => {
         it('should trigger router action', () => {
-            
             const action = new AddGlobalError({
                 header: '',
                 errors: []
@@ -82,15 +78,15 @@ describe('App Effects', () => {
         });
       });
 
-      describe('Keep Alive$', () => {
+    describe('Keep Alive$', () => {
         it('should keep alive', () => {
             const action = new KeepAlive();
             const completion = new Go({
             path: ['auth/keepalive']
-          });
-          actions$ = hot('-a', { a: action });
+            });
+            actions$ = hot('-a', { a: action });
             const expected = cold('-b', { b: completion });
-          expect(effects.keepAlive$).toBeTruthy();
+            expect(effects.keepAlive$).toBeTruthy();
         });
       });
 });
