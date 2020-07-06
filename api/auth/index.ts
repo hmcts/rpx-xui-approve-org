78d1c9d02aaab6f063bb23673d291e02cc5b30a1
Pardeep Singh Basi <paddybasi@gmail.com>
2020-07-06 16:16:48 +0100

    latest library version to fix/tidy up auth event callback and roles regex passed in (#357)
    * latest library version to fix/tidy up auth event callbac and roles
    
    * new lock file

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -1,23 +1,19 @@
-import { AUTH, Strategy, xuiNode } from '@hmcts/rpx-xui-node-lib'
+import { AUTH, xuiNode } from '@hmcts/rpx-xui-node-lib'
 import * as express from 'express'
 import { NextFunction, Request, Response } from 'express'
-import {logger} from '../application'
 import {getConfigValue} from '../configuration'
 import {COOKIE_ROLES} from '../configuration/references'
 import { http } from '../lib/http'
-import {havePrdAdminRole} from './userRoleAuth'
 
-const successCallback = async (strategy: Strategy, isRefresh: boolean, req: Request, res: Response, next: NextFunction) => {
-  const userDetails = req.session.passport.user
-  const roles = userDetails.userinfo.roles
-  if (!havePrdAdminRole(roles)) {
-    logger.warn('User role does not allow login')
-    return await strategy.logout(req, res)
-  }
+//TODO: once both todo's below are complete, we can remove this file
 
+const successCallback = (req: Request, res: Response, next: NextFunction) => {
+  const { roles } = req.session.passport.user.userinfo
+
+  //TODO: remove dependency on the roles cookie, can then remove this event handler entirely
   res.cookie(getConfigValue(COOKIE_ROLES), roles)
 
-  if (!isRefresh) {
+  if (!req.isRefresh) {
     return res.redirect('/')
   }
   next()
@@ -26,6 +22,7 @@ const successCallback = async (strategy: Strategy, isRefresh: boolean, req: Requ
 xuiNode.on(AUTH.EVENT.AUTHENTICATE_SUCCESS, successCallback)
 
 export async function attach(req: express.Request, res: express.Response, next: express.NextFunction) {
+  //TODO: req now has req.headers.Authorization/ServiceAuthorization, so might be worthwhile replacing this in future
   if (!req.http) {
     req.http = http(req)
   }
