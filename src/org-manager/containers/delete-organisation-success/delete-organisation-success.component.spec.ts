61a4153dbb30fc027913e08e52f5aa85c20f2a1b
Philip <philip.aberneithy@gmail.com>
2020-08-03 11:49:07 +0100

    Unit tests around 'Delete this registration request?' page.

--- src/org-manager/containers/delete-organisation-success/delete-organisation-success.component.spec.ts
+++ src/org-manager/containers/delete-organisation-success/delete-organisation-success.component.spec.ts
@@ -1,9 +1,8 @@
-import { DeleteOrganisationSuccessComponent } from './delete-organisation-success.component';
+import {DeleteOrganisationSuccessComponent} from './delete-organisation-success.component';
 
-// TODO check why this is throwing errors for
-xdescribe('Org Approval Success Component', () => {
-    it('should have a component', () => {
-        const component = new DeleteOrganisationSuccessComponent();
-        expect(component).toBeTruthy();
-});
+describe('Organisation Deletion Success Component', () => {
+  it('should have a component', () => {
+    const component = new DeleteOrganisationSuccessComponent();
+    expect(component).toBeTruthy();
+  });
 });
