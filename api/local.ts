986bc17eafe9cb6e8e8b8c27e0f9d43972b3d958
Ronald Mansveld <ronald.mansveld@atos.net>
2020-09-11 09:15:07 +0100

    use newer version of common lib

--- api/local.ts
+++ api/local.ts
@@ -2,7 +2,6 @@ import {app, logger} from './application'
 
 const port = process.env.PORT || 3001
 
-
 console.log('WE ARE USING local.ts on the box.')
 
 app.listen(port, () => logger.info(`Local server up at ${port}`))
