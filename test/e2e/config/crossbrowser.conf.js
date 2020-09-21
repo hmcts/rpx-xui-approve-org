6df894033de7c05bda8331dbb17bf324bd1c5c8a
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2020-09-21 12:20:14 +0100

    EUI_2572_2578_NightlyFixes (#402)

--- test/e2e/config/crossbrowser.conf.js
+++ test/e2e/config/crossbrowser.conf.js
@@ -71,6 +71,21 @@ const config = {
       }
     }
   ],
+
+
+  onPrepare() {
+    const caps = browser.getCapabilities();
+    browser.manage()
+      .window()
+      .maximize();
+    browser.waitForAngularEnabled(false);
+    global.expect = chai.expect;
+    global.assert = chai.assert;
+    global.should = chai.should;
+    global.screenShotUtils = new screenShotUtils({
+      browserInstance: browser
+    })
+  }
 };
 
 
