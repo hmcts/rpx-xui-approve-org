a3a0e4be14b0aa55776fa0ec1bb3f96d5136b52a
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2020-09-21 14:33:56 +0100

    Update crossbrowser.conf.js
    removed an screenshot utils line of code

--- test/e2e/config/crossbrowser.conf.js
+++ test/e2e/config/crossbrowser.conf.js
@@ -82,9 +82,6 @@ const config = {
     global.expect = chai.expect;
     global.assert = chai.assert;
     global.should = chai.should;
-    global.screenShotUtils = new screenShotUtils({
-      browserInstance: browser
-    })
   }
 };
 
