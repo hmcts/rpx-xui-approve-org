f3564f9af9c92cd0e7a338425829e45b4dc7bec6
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2020-09-23 17:26:48 +0100

    tests/hooks async attach screenshot fix. (#405)
    * hooks async attach screenshot fix.
    
    * Debug logs added.
    
    * zap security scan tests fix.
    
    * removed duplicate lines
    
    * removed duplicate and not required line.

--- test/e2e/features/step_definitions/steps/loginLogout.steps.js
+++ test/e2e/features/step_definitions/steps/loginLogout.steps.js
@@ -128,6 +128,7 @@ defineSupportCode(function ({ Given, When, Then }) {
 
   Then(/^I should be redirected to approve organisation dashboard page$/, async function () {
     browser.sleep(SHORT_DELAY);
+    await browserWaits.waitForBrowserReadyState(120);
     await browserWaits.waitForElement(loginPage.dashboard_header);
     await expect(loginPage.dashboard_header.isDisplayed()).to.eventually.be.true;
     await expect(loginPage.dashboard_header.getText())
