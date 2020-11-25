7af9e2c5a55ca0b5587ef3864be81e3d3d221c7a
Philip <philip.aberneithy@gmail.com>
2020-11-25 15:55:59 +0000

    Remove Idam, PRD, and Fee and Pay from health checks as per EUI-2309.

--- api/application.ts
+++ api/application.ts
@@ -208,10 +208,6 @@ function healthcheckConfig(msUrl) {
 
 const healthChecks = {
   checks: {
-    feeAndPayApi: healthcheckConfig(getConfigValue(SERVICES_FEE_AND_PAY_PATH)),
-    idamApi: healthcheckConfig(getConfigValue(SERVICES_IDAM_API_PATH)),
-    idamWeb: healthcheckConfig(getConfigValue(SERVICES_IDAM_WEB)),
-    rdProfessionalApi: healthcheckConfig(getConfigValue(SERVICES_RD_PROFESSIONAL_API_PATH)),
     redis: () => ({status: 'UP'}),
     s2s: healthcheckConfig(getConfigValue(SERVICE_S2S_PATH)),
   },
