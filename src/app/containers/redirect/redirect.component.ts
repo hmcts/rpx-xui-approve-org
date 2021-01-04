1db072240acc6f61abd1c364d2a7a2ab0922ce40
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-04 13:48:29 +0000

    lint errors

--- src/app/containers/redirect/redirect.component.ts
+++ src/app/containers/redirect/redirect.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { CookieService } from "ngx-cookie";
-import { AppUtils } from "src/app/utils/app-utils";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie';
+import { AppUtils } from 'src/app/utils/app-utils';
 
 @Component({
     selector: 'app-redirect',
@@ -14,9 +14,9 @@ export class RedirectComponent implements OnInit {
         const encodedRoles = this.cookieService.getObject('roles');
         if (encodedRoles) {
           const roles = AppUtils.getRoles(encodedRoles);
-          if(roles.includes('prd-admin')) {
+          if (roles.includes('prd-admin')) {
             this.router.navigate(['pending-organisations'])
-          } else if(roles.includes('')) {
+          } else if (roles.includes('')) {
             // EUI-2987
           }
         }
