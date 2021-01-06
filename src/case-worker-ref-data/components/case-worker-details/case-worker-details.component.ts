de915ec1a64e7a6aac38b908292f0dd559b694b0
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-06 15:18:31 +0000

    typo

--- src/case-worker-ref-data/components/case-worker-details/case-worker-details.component.ts
+++ src/case-worker-ref-data/components/case-worker-details/case-worker-details.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-prd-case-worker-details',
-    templateUrl: './case-worker-ref-home.html',
+    templateUrl: './case-worker-details.component.html',
   })
 export class CaseWorkerDetailsComponent {
 }
