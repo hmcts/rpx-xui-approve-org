9043a167c8601ecf9f44cc24d5a6e5a34fab6472
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-08 14:13:06 +0000

    Add new route [temp]for file upload. new front end service/calling it from the component

--- src/case-worker-ref-data/components/case-worker-details/case-worker-details.component.ts
+++ src/case-worker-ref-data/components/case-worker-details/case-worker-details.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { CaseWorkerRefDataService } from '../../services/case-worker-ref-data.service';
 
 @Component({
     selector: 'app-prd-case-worker-details',
     templateUrl: './case-worker-details.component.html',
   })
 export class CaseWorkerDetailsComponent {
+
+  public constructor(private readonly caseWorkerRefDataService: CaseWorkerRefDataService) {}
+
+  public onSubmit(inputElement) {
+    const formData = new FormData();
+    formData.append('excel', inputElement.files.item(0));
+    this.caseWorkerRefDataService.postFile(formData).subscribe(result => console.log(result));
+  }
 }
