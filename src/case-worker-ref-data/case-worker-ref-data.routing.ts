3da55b171dadce9798c7e14855faaa2c1689b560
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-11 12:49:01 +0000

    upload successful component

--- src/case-worker-ref-data/case-worker-ref-data.routing.ts
+++ src/case-worker-ref-data/case-worker-ref-data.routing.ts
@@ -2,11 +2,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'src/services/auth/auth.guard';
 import { CaseWorkerRefDataHomeComponent } from './containers/case-worker-ref-home/case-worker-ref-home.component';
 import { ModuleWithProviders } from '@angular/core';
+import { UploadInfoDetailsComponent } from './components/upload-info-details/upload-info-details.component';
 
 export const ROUTES: Routes = [
     {
       path: '',
       component: CaseWorkerRefDataHomeComponent,
+      canActivate: [AuthGuard],
+    },
+    {
+      path: 'upload-success',
+      component: UploadInfoDetailsComponent,
       canActivate: [AuthGuard],
     }
 ];
--- src/case-worker-ref-data/components/index.ts
+++ src/case-worker-ref-data/components/index.ts
@@ -1,3 +1,4 @@
 import { CaseWorkerDetailsComponent } from './case-worker-details/case-worker-details.component';
+import { UploadInfoDetailsComponent } from './upload-info-details/upload-info-details.component';
 
-export const components: any [] = [CaseWorkerDetailsComponent];
+export const components: any [] = [CaseWorkerDetailsComponent, UploadInfoDetailsComponent];
