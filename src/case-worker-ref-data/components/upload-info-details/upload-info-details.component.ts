0a3b2256aab7247998afdcad95a43a797de622a0
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-11 12:49:01 +0000

    upload successful component

---
+++ src/case-worker-ref-data/components/upload-info-details/upload-info-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CaseWorkerRefDataUploadResponse } from '../../models/case-worker-ref-data.model';
+
+@Component({
+    selector: 'app-prd-upload-info-detail',
+    templateUrl: './upload-info-details.component.html',
+  })
+
+export class UploadInfoDetailsComponent implements OnInit {
+  private readonly uploadResponse: CaseWorkerRefDataUploadResponse;
+  private createdOrAmendedRecords: number;
+  private suspendedRecords: number;
+  constructor(private readonly router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras && navigation.extras.state) {
+      this.uploadResponse = navigation.extras.state as CaseWorkerRefDataUploadResponse;
+    }
+  }
+  public ngOnInit() {
+    console.log(this.uploadResponse);
+    this.createdOrAmendedRecords = this.uploadResponse.recordsAmended + this.uploadResponse.recordsCreated;
+    this.suspendedRecords = this.uploadResponse.recordsDeleted;
+  }
+}
