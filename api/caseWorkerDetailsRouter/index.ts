c859cadeec1fdbcb74d61ea8950fbd20c485b9ce
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-14 12:24:13 +0000

    adding mock handling errors

--- api/caseWorkerDetailsRouter/index.ts
+++ api/caseWorkerDetailsRouter/index.ts
@@ -1,21 +1,15 @@
 import * as express from 'express'
 import * as multer from 'multer'
+import { getErrorResponse } from './mock-error-scenarios'
 
 const upload = multer({ dest: 'uploads/' })
 
 async function caseWorkerDetailsRoute(req: express.Request, res: express.Response) {
-    console.log(req.files)
-    if(req.files[0].originalname === 'fileName-400.xlsx') {
-        console.log('error')
-        res.status(400).json({
-            error_code: 400,
-            status: 'BAD_REQUEST',
-            error_message: 'Missing column headers',
-            error_description: 'File is missing the required column headers. Please check the file',
-            timestamp: Date.now()
-         })
-    } else {
+    const mockErrorRes = getErrorResponse(req, res)
+    if(!mockErrorRes) {
         res.send({recordsCreated: 1, recordsAmended: 1, recordsDeleted: 0, recordsFailed: 0}).status(200)
+    } else {
+        res.status(mockErrorRes.status).json(mockErrorRes.json)
     }
 }
 
