ea70b3f9ba6bad0b7c2ab2dd2477146302d28ee4
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-14 12:06:36 +0000

    adding multer for the uploads. This is only for the mock. Will be removed later.

--- api/caseWorkerDetailsRouter/index.ts
+++ api/caseWorkerDetailsRouter/index.ts
@@ -1,11 +1,15 @@
 import * as express from 'express'
+import * as multer from 'multer'
+
+const upload = multer({ dest: 'uploads/' })
 
 async function caseWorkerDetailsRoute(req: express.Request, res: express.Response) {
+    console.log(req.files)
     res.send({recordsCreated: 1, recordsAmended: 1, recordsDeleted: 0, recordsFailed: 0}).status(200)
 }
 
 export const router = express.Router({ mergeParams: true })
 
-router.post('/', caseWorkerDetailsRoute)
+router.post('/', upload.any(),  caseWorkerDetailsRoute)
 
 export default router
