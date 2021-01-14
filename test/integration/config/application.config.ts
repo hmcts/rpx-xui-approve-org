6354c74d976ceef22cb3f12e9c195d7cf4f13ed8
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2021-01-14 10:02:59 +0000

    API Functional Tests EUI-1592 (#427)
    * API Functional Tests EUI-1592
    
    * new set of tests
    
    * added to jenkins
    
    * config proxy defined
    
    * no appinsights
    
    * changed cookie values
    
    * error fixed
    
    * appinsights key
    
    * new tests added
    
    * all tests added
    
    * refactor tests
    
    * delete org test added
    
    * all tests covered

---
+++ test/integration/config/application.config.ts
@@ -0,0 +1,18 @@
+export const application = {
+    cookies: {
+        token: '__auth__',
+        userId: '__userid__',
+    },
+    microservice: 'xui_webapp',
+    idamClient: 'xuiaowebapp',
+    oauthCallbackUrl: '/oauth2/callback',
+    protocol: 'https',
+    logging: 'debug',
+    maxLogLine: 80,
+    exceptionOptions: {
+        maxLines: 1,
+    },
+    indexUrl: '/',
+    secureCookie: false,
+    sessionSecret: 'secretSauce',
+}
