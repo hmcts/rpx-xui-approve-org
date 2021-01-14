e1e3415ceeb66d1d0cc19a1419c62f513c490f98
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2021-01-14 15:22:52 +0000

    Ao api functional tests e UI 1592 (#439)
    * API Functional Tests EUI-1592
    
    * new set of tests
    
    * added to jenkins
    
    * config proxy defined
    
    * no appinsights
    
    * changed cookie values
    
    * error fixed
    
    * appinsights key
    
    * new tests added
    
    * all tests added
    
    * refactor tests
    
    * delete org test added
    
    * all tests covered
    
    * double login retry logic

--- test/integration/tests/utils/index.ts
+++ test/integration/tests/utils/index.ts
@@ -3,3 +3,4 @@ export { getOauth2Token } from './getToken';
 export { generatePOSTAPIRequest } from './generatePOSTAPI';
 export { authenticateAndGetcookies} from './getCookie';
 export { generateregPOSTAPIRequest } from './generateregPOSTAPI';
+export const timeout = 90000;
