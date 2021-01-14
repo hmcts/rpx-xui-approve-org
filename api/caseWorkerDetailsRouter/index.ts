a33e159d8f4f3635a45db2c0a7ea1033f9fc24d5
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-14 12:17:06 +0000

    scenario for 400

--- api/caseWorkerDetailsRouter/index.ts
+++ api/caseWorkerDetailsRouter/index.ts
@@ -5,7 +5,18 @@ const upload = multer({ dest: 'uploads/' })
 
 async function caseWorkerDetailsRoute(req: express.Request, res: express.Response) {
     console.log(req.files)
-    res.send({recordsCreated: 1, recordsAmended: 1, recordsDeleted: 0, recordsFailed: 0}).status(200)
+    if(req.files[0].originalname === 'fileName-400.xlsx') {
+        console.log('error')
+        res.status(400).json({
+            error_code: 400,
+            status: 'BAD_REQUEST',
+            error_message: 'Missing column headers',
+            error_description: 'File is missing the required column headers. Please check the file',
+            timestamp: Date.now()
+         })
+    } else {
+        res.send({recordsCreated: 1, recordsAmended: 1, recordsDeleted: 0, recordsFailed: 0}).status(200)
+    }
 }
 
 export const router = express.Router({ mergeParams: true })
