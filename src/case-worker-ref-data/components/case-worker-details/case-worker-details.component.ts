6e72bdd19aece77c49385fa23f9d4b4a53451f22
Uday Denduluri <uday.denduluri@gmail.com>
2021-01-15 14:47:45 +0000

    lint issues and merging error handling with partly successful

--- src/case-worker-ref-data/components/case-worker-details/case-worker-details.component.ts
+++ src/case-worker-ref-data/components/case-worker-details/case-worker-details.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
 import { CaseWorkerRefDataService } from '../../services/case-worker-ref-data.service';
 import { CaseWorkerRefDataUploadResponse } from '../../models/case-worker-ref-data.model';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { handleFatalErrors } from '../../utils/case-worker-utils';
 
 @Component({
     selector: 'app-prd-case-worker-details',
@@ -9,11 +10,18 @@ import { Router } from '@angular/router';
   })
 export class CaseWorkerDetailsComponent {
 
+  public errorDesc: string;
+
   public constructor(private readonly caseWorkerRefDataService: CaseWorkerRefDataService,
                      private readonly router: Router) {}
 
   public onSubmit(inputElement: any) {
     const formData = new FormData();
+    if (!inputElement.files.item(0)) {
+      // if there is no file attached
+      this.errorDesc = 'You need to select a file to upload. Please try again.';
+      return;
+    }
     formData.append('excel', inputElement.files.item(0));
     this.caseWorkerRefDataService.postFile(formData).subscribe((response: CaseWorkerRefDataUploadResponse) => {
       if (response.error_details && response.error_details.length > 0) {
@@ -24,7 +32,23 @@ export class CaseWorkerDetailsComponent {
     },
     errorResponse => {
       // Upload errors - EUI-3014
-      console.log(errorResponse);
+      this.showUploadErrors(errorResponse);
     });
   }
+
+  /**
+   * Sort out the actual errors returned via the API
+   */
+  public showUploadErrors(errorResponse: any): void {
+
+    // redirect to the correct page as is necessary
+    const handledStatus = handleFatalErrors(errorResponse.status, this.router);
+
+    // if the error is a 400 then use the error description as the display message
+    if (handledStatus === 400) {
+      this.errorDesc = errorResponse.error.error_description;
+    }
+  }
 }
+
+
