38cbcbfa913b0bead47e913d2257b1476fb4f204
ernestman28 <ernest_man28@hotmail.com>
2021-01-21 14:09:49 +0000

    Infra upgrade (#443)
    * update node chart version
    
    * Update redis chart version
    
    * test
    
    * upgrade terraform to 0.14
    
    * fix Interpolation-only expressions warning
    
    * fix Quoted type constraints
    
    * jenkins nightly fix
    
    * removed duplicate test
    
    Co-authored-by: VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,7 +1,7 @@
 output "microserviceName" {
-  value = "${local.app_full_name}"
+  value = local.app_full_name
 }
 
 output "appInsightsInstrumentationKey" {
-  value = "${azurerm_application_insights.appinsights.instrumentation_key}"
+  value = azurerm_application_insights.appinsights.instrumentation_key
 }
