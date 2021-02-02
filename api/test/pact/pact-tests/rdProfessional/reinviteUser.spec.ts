4fcc39bbf6f33c694f646b0a041daf9139c22656
ruban72 <ruban72@hotmail.com>
2021-02-02 09:21:52 +0000

    Ta-229: Refactor consumer tests to work with Provider tests

--- api/test/pact/pact-tests/rdProfessional/reinviteUser.spec.ts
+++ api/test/pact/pact-tests/rdProfessional/reinviteUser.spec.ts
@@ -1,111 +1,100 @@
-import { Pact } from '@pact-foundation/pact';
+import {Pact} from '@pact-foundation/pact';
 import {expect} from 'chai';
 import * as getPort from 'get-port';
-import {isDone} from 'ng-packagr/lib/brocc/select';
 import * as path from 'path';
 import {delay} from 'rxjs/operators';
 import {postOperation} from "../../../pactUtil";
 import {UserAddedResponse} from "../../../pactFixtures"
+
 const {Matchers} = require('@pact-foundation/pact');
 const {somethingLike} = Matchers;
 
-describe('Reinvite an User', () => {
+describe('Reinvite a User', () => {
 
-  const orgnId = "orgn1500";
+    const orgnId = "UTVC86X";
 
-  let mockServerPort: number;
-  let provider: Pact;
+    let mockServerPort: number;
+    let provider: Pact;
 
-  // @ts-ignore
-
-  before(async  ()=> {
     // @ts-ignore
-    //delay(3000);
-    await new Promise(resolve => setTimeout(resolve, 3000));
 
-    mockServerPort = await getPort()
-    provider = new Pact({
-      consumer: 'xui_approve_org_oidc',
-      log: path.resolve(process.cwd(), "api/test/pact/logs", "mockserver-integration.log"),
-      dir: path.resolve(process.cwd(), "api/test/pact/pacts"),
-      logLevel: 'info',
-      port: mockServerPort,
-      provider: 'rd_professional_api',
-      spec: 2,
-      pactfileWriteMode: "merge"
-    })
-    return provider.setup()
-  })
+    before(async () => {
+        // @ts-ignore
+        //delay(3000);
+        await new Promise(resolve => setTimeout(resolve, 3000));
 
-  // Write Pact when all tests done
-  after(() => {
-    provider.finalize()
-    delay(3000)
-  })
-
-  // verify with Pact, and reset expectations
-  afterEach(() => provider.verify())
-
-  let mockRequest = {
-    "firstName": "joe",
-    "lastName": "bloggs",
-    "email": "joe.blogss@hmcts.net",
-    "roles": [
-      "claimant"
-    ],
-    "resendInvite": true
-  }
-
-  let mockResponse = {
-     "idamStatus": somethingLike("User Added Successfully"),
-     "userIdentifier": somethingLike("User123456")
-  }
-
-  describe('Reinvite User by adding the user to an organisation ', () => {
-    before(done =>{
-      const interaction = {
-        state: 'Then a status message is returned',
-        uponReceiving: 'A Request to Reinvite user to the organisation is received',
-        withRequest: {
-          method: "POST",
-          path:"/refdata/internal/v1/organisations/"+orgnId,
-          body:mockRequest ,
-          headers: {
-            "Content-Type": "application/json",
-            "ServiceAuthorization": "ServiceAuthToken",
-            "Authorization": "Bearer some-access-token"
-          }
-        },
-        willRespondWith: {
-          status: 201,
-          body:mockResponse
-        }
-      }
-      // @ts-ignore
-      provider.addInteraction(interaction).then(() => {
-        done()
-      })
+        mockServerPort = await getPort()
+        provider = new Pact({
+            consumer: 'xui_approveOrg',
+            log: path.resolve(process.cwd(), "api/test/pact/logs", "mockserver-integration.log"),
+            dir: path.resolve(process.cwd(), "api/test/pact/pacts"),
+            logLevel: 'info',
+            port: mockServerPort,
+            provider: 'referenceData_organisationalInternal',
+            spec: 2,
+            pactfileWriteMode: "merge"
+        })
+        return provider.setup()
     })
 
-    it('Reinvite a user for an organisation', async () => {
-      const taskUrl:string  = `${provider.mockService.baseUrl}/refdata/internal/v1/organisations/`+orgnId;
-
-      const resp =  postOperation(taskUrl, mockRequest )
-
-      resp.then((response) => {
-        try{
-          const responseDto:UserAddedResponse  = <UserAddedResponse> response.data
-          expect(response.status).to.be.equal(201);
-          assertResponses(responseDto);
-        }catch(e){
-         console.log(`error occurred in asserting response...`+e)
-        }
-      })
+    // Write Pact when all tests done
+    after(() => {
+        provider.finalize()
+        delay(3000)
     })
-  })
+
+    // verify with Pact, and reset expectations
+    afterEach(() => provider.verify())
+
+    let mockRequest = {
+        "firstName": "joe",
+        "lastName": "bloggs",
+        "email": "joe.blogss@hmcts.net",
+        "roles": [
+            "claimant"
+        ],
+        "resendInvite": true
+    }
+
+    describe('Reinvite User by adding the user to an organisation ', () => {
+        before(done => {
+            const interaction = {
+                state: 'User invited to Organisation',
+                uponReceiving: 'A Request to Reinvite user to the organisationd',
+                withRequest: {
+                    method: "POST",
+                    path: "/refdata/internal/v1/organisations/" + orgnId + "/users/",
+                    body: mockRequest,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "ServiceAuthorization": "ServiceAuthToken",
+                        "Authorization": "Bearer some-access-token"
+                    }
+                },
+                willRespondWith: {
+                    status: 201
+                }
+            }
+            // @ts-ignore
+            provider.addInteraction(interaction).then(() => {
+                done()
+            })
+        })
+
+        it('Reinvite a user for an organisation', async () => {
+            const taskUrl: string = `${provider.mockService.baseUrl}/refdata/internal/v1/organisations/` + orgnId + "/users/";
+
+            const resp = postOperation(taskUrl, mockRequest)
+
+            resp.then((response) => {
+                try {
+                    const responseDto: UserAddedResponse = <UserAddedResponse>response.data
+                    expect(response.status).to.be.equal(201);
+                } catch (e) {
+                    console.log(`error occurred in asserting response...` + e)
+                }
+            })
+        })
+    })
 })
 
-function assertResponses(response:UserAddedResponse){
-  expect(response.idamStatus).to.be.equal('User Added Successfully');
-  expect(response.userIdentifier).to.be.equal('User123456');
-}
