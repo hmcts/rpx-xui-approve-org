5e2fafb853a4bc305e87727df3f5f760d0917089
kasi-subramaniam <60706745+kasi-subramaniam@users.noreply.github.com>
2021-02-25 10:01:24 +0000

    TA-229 PactTests -from master  Clean Build (#445)
    * TA-229-Pact - Added tests for delete-org
    
    * TA-229-Pact - Added more PACT Tests
    
    * TA-229-Pact - Refactored PACT Tests and all are passingn now.
    
    * TA-229-Pact -Updated with Correct Consumer/Provider names.
    
    * added cert and updated pact version and tests
    
    * Renamed files and introducin dummy Promise to increase wait time in between calls.
    
    * re-generated yarn.lock file
    
    * adding new yarn.lock file
    
    * Reverting to commit [ aeea834f34832dc4d0db0e3a905680551072fd70 ] to see if this fixes pipeline issue.
    
    * Added tests for s2s,rdPro,idam-api interactions
    
    * Ta-229: Refactor consumer tests to work with Provider tests
    
    * Ta-229: Refactor consumer tests to work with Provider tests
    
    * Ta-229: Remove duplicate tests and add Pact to jenkins pipleine for master
    
    * Reverting pact-node library to version ^9.0.6.
    
    * Adding back deleted yarn.lock files both at root level as well within  /api folder.
    
    * refactored tests with provider setup in settings
    
    * fixing pact publish issue
    
    Co-authored-by: satya chundur <>
    Co-authored-by: ruban72 <ruban72@hotmail.com>
    Co-authored-by: VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>

---
+++ api/test/pact/pact-tests/idam/getUserDetails.spec.ts
@@ -0,0 +1,87 @@
+import { expect } from 'chai';
+import { idamGetUserDetails } from '../../../pactUtil';
+import { PactTestSetup } from '../settings/provider.mock';
+
+const { Matchers } = require('@pact-foundation/pact');
+const { somethingLike } = Matchers;
+const pactSetUp = new PactTestSetup({ provider: 'Idam_api', port: 8000 });
+
+
+describe("Idam API user details", async () => {
+  before(async () => {
+    await new Promise(resolve => setTimeout(resolve, 3000));
+   })
+
+  const RESPONSE_BODY = {
+    "id": somethingLike("abc123"),
+    "forename": somethingLike("Joe"),
+    "surname": somethingLike("Bloggs"),
+    "email": somethingLike("joe.bloggs@hmcts.net"),
+    "active": somethingLike(true),
+    "roles": somethingLike([
+      somethingLike("solicitor"), somethingLike("caseworker")
+    ])
+  }
+
+  describe("Get user details from Idam", () => {
+
+    const jwt = 'some-access-token';
+
+    before(async () => {
+      await pactSetUp.provider.setup();
+      const interaction = {
+        state: "a valid user exists",
+        uponReceiving: "a request for that user",
+        withRequest: {
+          method: "GET",
+          path: "/details",
+          headers: {
+            Authorization: "Bearer some-access-token"
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: RESPONSE_BODY,
+        },
+      }
+      // @ts-ignore
+      pactSetUp.provider.addInteraction(interaction)
+
+    })
+
+    it("Returns the user details from IDAM", async () => {
+      const taskUrl = `${pactSetUp.provider.mockService.baseUrl}/details`;
+      const response = idamGetUserDetails(taskUrl);
+
+      response.then((axiosResponse) => {
+        const dto: IdamGetDetailsResponseDto = <IdamGetDetailsResponseDto>axiosResponse.data;
+        assertResponses(dto);
+      }).then(() => {
+        pactSetUp.provider.verify(),
+        pactSetUp.provider.finalize()
+      })
+    })
+
+  })
+})
+
+function assertResponses(dto: IdamGetDetailsResponseDto) {
+  expect(dto.active).to.be.equal(true);
+  expect(dto.email).to.be.equal('joe.bloggs@hmcts.net');
+  expect(dto.forename).to.be.equal('Joe');
+  expect(dto.surname).to.be.equal('Bloggs');
+  expect(dto.roles[0]).to.be.equal('solicitor');
+  expect(dto.roles[1]).to.be.equal('caseworker');
+}
+
+export interface IdamGetDetailsResponseDto {
+  id: string,
+  forename: string,
+  surname: string,
+  email: string,
+  active: boolean
+  roles: string[]
+}
