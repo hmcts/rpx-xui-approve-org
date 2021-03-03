e8d0279256e7097c257943a30bd9d7826848eed7
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-03-03 15:20:12 +0000

    added es7-object and array lib to avoid IE issues

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -34,7 +34,8 @@ import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 import 'isomorphic-fetch';
-
+import 'core-js/es7/object';
+import 'core-js/es7/array';
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
