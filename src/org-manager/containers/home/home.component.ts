d7265195a84b7a2925acb34a672b7107dc4ae3a4
Paul Graham <paul.graham@hmcts.net>
2021-05-11 12:07:16 +0100

    PR review
    Sorted a bunch of things that were raised in a PR review by @phillip-whitaker.

--- src/org-manager/containers/home/home.component.ts
+++ src/org-manager/containers/home/home.component.ts
@@ -3,10 +3,10 @@ import { NavigationEnd, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-organisations-home-component',
-  templateUrl: './organisations-home.component.html',
+  selector: 'app-home-component',
+  templateUrl: './home.component.html',
 })
-export class OrganisationsHomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy {
   public readonly tabs = [
     { url: '/organisation/pending', label: 'New registrations' },
     { url: '/organisation/pbas', label: 'New PBAs' },
@@ -20,9 +20,11 @@ export class OrganisationsHomeComponent implements OnInit, OnDestroy {
   public ngOnInit(): void {
     this.setupRoute();
     // Watch for changes to the route to set the appropriate tab.
-    this.routeSubscription = this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.setupRoute();
+    this.routeSubscription = this.router.events.subscribe({
+      next: event => {
+        if (event instanceof NavigationEnd) {
+          this.setupRoute();
+        }
       }
     });
   }
