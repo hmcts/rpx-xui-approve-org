21fe98eed96a551414e31c3328963567f95d4142
Paul Graham <paul.graham@hmcts.net>
2021-06-22 10:05:10 +0100

    Organisation address component
    Reusing the new organisation address component elsewhere.

--- src/org-manager/containers/active-organisations/active-organisations.component.ts
+++ src/org-manager/containers/active-organisations/active-organisations.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import {takeWhile} from 'rxjs/operators';
+import { takeWhile } from 'rxjs/operators';
+
 import * as fromOrganisation from '../../../org-manager/store/';
-import {OrganisationVM} from '../../models/organisation';
+import { OrganisationVM } from '../../models/organisation';
 
 /**
  * Bootstraps Active Organisations
@@ -15,6 +16,7 @@ import {OrganisationVM} from '../../models/organisation';
 
 export class ActiveOrganisationsComponent implements OnInit {
   public orgs$: Observable<OrganisationVM[]>;
+  public activeOrgsCount$: Observable<number>;
   public loading$: Observable<boolean>;
   public activeSearchString$: Observable<string>;
 
@@ -32,9 +34,9 @@ export class ActiveOrganisationsComponent implements OnInit {
     });
 
     this.orgs$ = this.store.pipe(select(fromOrganisation.getActiveOrganisationArray));
+    this.activeOrgsCount$ = this.store.pipe(select(fromOrganisation.activeOrganisationsCount));
     this.loading$ = this.store.pipe(select(fromOrganisation.getActiveLoading));
     this.activeSearchString$ = this.store.pipe(select(fromOrganisation.getActiveSearchString));
-
   }
 
   public submitSearch(searchString: string) {
