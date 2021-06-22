c3603246ebec3d3f1034b94a6afedae7046fe5f8
Paul Graham <paul.graham@hmcts.net>
2021-06-22 10:05:10 +0100

    Organisation address component
    Reusing the new organisation address component elsewhere.

--- src/org-manager/containers/delete-organisation/delete-organisation.component.spec.ts
+++ src/org-manager/containers/delete-organisation/delete-organisation.component.spec.ts
@@ -1,19 +1,21 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
-import {combineReducers, Store, StoreModule} from '@ngrx/store';
-import {of} from 'rxjs';
-import {Go} from '../../../app/store/actions';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { combineReducers, Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
+
+import { Go } from '../../../app/store/actions';
 import * as fromRoot from '../../../app/store/reducers';
-import {BackLinkComponent} from '../../../org-manager/components';
+import { BackLinkComponent } from '../../../org-manager/components';
 import * as fromOrganisationPendingStore from '../../../org-manager/store';
+import { OrganisationAddressComponent } from '../../components/organisation-address';
 import {
   ActiveOrganisationMockCollection,
   PendingOrganisationsMockCollection2,
   PendingOrganisationsMockCollectionObj,
-  ReviewedOrganisationMockCollection
+  ReviewedOrganisationMockCollection,
 } from '../../mock/pending-organisation.mock';
-import {DeleteOrganisation, DeletePendingOrganisation} from '../../store/actions';
-import {DeleteOrganisationComponent} from './delete-organisation.component';
+import { DeleteOrganisation, DeletePendingOrganisation } from '../../store/actions';
+import { DeleteOrganisationComponent } from './delete-organisation.component';
 
 describe('DeleteOrganisationComponent', () => {
   let component: DeleteOrganisationComponent;
@@ -35,7 +37,8 @@ describe('DeleteOrganisationComponent', () => {
       ],
       declarations: [
         BackLinkComponent,
-        DeleteOrganisationComponent
+        DeleteOrganisationComponent,
+        OrganisationAddressComponent
       ]
     }).compileComponents();
     store = TestBed.get(Store);
