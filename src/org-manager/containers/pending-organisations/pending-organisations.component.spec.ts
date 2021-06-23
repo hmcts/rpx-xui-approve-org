31a7ec468320c04a94c8799ff66f49b4e1775bc0
Paul Graham <paul.graham@hmcts.net>
2021-06-23 09:23:30 +0100

    EUI-3689 & EUI-3791
    Refactored the Active Organisations component so that it is using the top-level search. In the midst of that, abstracted out the common logic between that component and the Pending Organisations component to remove duplication.

--- src/org-manager/containers/pending-organisations/pending-organisations.component.spec.ts
+++ src/org-manager/containers/pending-organisations/pending-organisations.component.spec.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { combineReducers, Store, StoreModule } from '@ngrx/store';
 
@@ -11,44 +10,32 @@ import { FilterOrganisationsPipe } from '../../pipes';
 import { PendingOrganisationsComponent } from './pending-organisations.component';
 
 describe('PendingOrganisationComponent', () => {
-    let component: PendingOrganisationsComponent;
-    let fixture: ComponentFixture<PendingOrganisationsComponent>;
-    let store: Store<fromOrganisationPendingStore.OrganisationRootState>;
-    beforeEach((() => {
-        TestBed.configureTestingModule({
-            imports: [
-                RouterTestingModule,
-                ReactiveFormsModule,
-                StoreModule.forRoot({
-                    ...fromRoot.reducers,
-                    feature: combineReducers(fromOrganisationPendingStore.reducers),
-                }),
-            ],
-            providers: [FormBuilder],
-            declarations: [
-              PendingOrganisationsComponent,
-              FilterOrganisationsPipe,
-              OrganisationAddressComponent
-            ],
-            schemas: [
-                CUSTOM_ELEMENTS_SCHEMA
-            ]
-        }).compileComponents();
-        store = TestBed.get(Store);
+  let component: PendingOrganisationsComponent;
+  let fixture: ComponentFixture<PendingOrganisationsComponent>;
+  let store: Store<fromOrganisationPendingStore.OrganisationRootState>;
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        StoreModule.forRoot({
+          ...fromRoot.reducers
+        }),
+      ],
+      declarations: [
+        PendingOrganisationsComponent,
+        FilterOrganisationsPipe,
+        OrganisationAddressComponent
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    }).compileComponents();
+    store = TestBed.get(Store);
 
-        fixture = TestBed.createComponent(PendingOrganisationsComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    }));
+    fixture = TestBed.createComponent(PendingOrganisationsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
-
-    // it('should dispatch UpdatePendingOrganisationsSearchString action on submitSearch', () => {
-    //   const expectedAction = new fromOrganisation.UpdatePendingOrganisationsSearchString('');
-    //   spyOn(store, 'dispatch').and.callThrough();
-    //   component.submitSearch('');
-    //   expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
-    // });
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
 });
