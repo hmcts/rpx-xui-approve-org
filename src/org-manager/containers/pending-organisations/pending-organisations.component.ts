d0efe0cfe772831d46369f7c2fd1dc9fa2e44b46
Paul Graham <paul.graham@hmcts.net>
2021-06-23 09:23:30 +0100

    EUI-3689 & EUI-3791
    Refactored the Active Organisations component so that it is using the top-level search. In the midst of that, abstracted out the common logic between that component and the Pending Organisations component to remove duplication.

--- src/org-manager/containers/pending-organisations/pending-organisations.component.ts
+++ src/org-manager/containers/pending-organisations/pending-organisations.component.ts
@@ -1,54 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
-import { FilterOrganisationsPipe } from 'src/org-manager/pipes';
+import { Component } from '@angular/core';
+import { Action, MemoizedSelector, Store } from '@ngrx/store';
 
 import * as fromStore from '../../../org-manager/store';
 import { OrganisationVM } from '../../models/organisation';
 import * as fromOrganisation from '../../store/';
+import { OrganisationListComponent } from './../organisation-list/organisation-list.component';
 
 @Component({
   selector: 'app-pending-overview-component',
   templateUrl: './pending-organisations.component.html',
 })
-
-export class PendingOrganisationsComponent implements OnInit {
-  public loaded$: Observable<boolean>;
-  public searchString: string = '';
-  public filteredOrgs: OrganisationVM[];
-  public filteredOrgsCount: number;
-
-  private readonly filter: FilterOrganisationsPipe = new FilterOrganisationsPipe();
-  private orgs: OrganisationVM[];
-
-  constructor(private readonly store: Store<fromStore.OrganisationRootState>) {}
-
-  public ngOnInit(): void {
-    this.loaded$ = this.store.pipe(select(fromOrganisation.getPendingLoaded));
-    this.loaded$.pipe(takeWhile(loaded => !loaded)).subscribe(loaded => {
-      if (!loaded) {
-        this.store.dispatch(new fromOrganisation.LoadPendingOrganisations());
-      }
-    });
-    this.store.pipe(select(fromStore.getPendingOrganisationsArray)).subscribe(orgs => {
-      this.orgs = orgs;
-      this.doFiltering();
-    });
-    // this.columnConfig = PendingOverviewColumnConfig;
-    this.store.dispatch(new fromStore.ClearErrors());
-    this.store.pipe(select(fromOrganisation.getSearchString)).subscribe(str => {
-      this.searchString = str;
-      this.doFiltering();
-    });
+export class PendingOrganisationsComponent extends OrganisationListComponent {
+  public get loadedSelector(): MemoizedSelector<object, boolean> {
+    console.log('Pending.loadedSelector getter');
+    return fromOrganisation.getPendingLoaded;
+  }
+  public get loadAction(): Action {
+    return new fromOrganisation.LoadPendingOrganisations();
+  }
+  public get organisationsSelector(): MemoizedSelector<object, OrganisationVM[]> {
+    return fromStore.getPendingOrganisationsArray;
   }
 
-  private doFiltering(): void {
-    if (this.orgs && this.searchString) {
-      this.filteredOrgs = this.filter.transform(this.orgs, this.searchString);
-    } else {
-      this.filteredOrgs = this.orgs;
-    }
-    this.filteredOrgsCount = (this.filteredOrgs || []).length;
+  constructor(protected readonly store: Store<fromStore.OrganisationRootState>) {
+    super(store);
   }
 }
