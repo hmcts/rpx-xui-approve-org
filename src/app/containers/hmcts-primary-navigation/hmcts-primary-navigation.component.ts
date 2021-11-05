dc535b6b284f2693d79b1c70356b60661011d17c
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-11-05 16:17:44 +0000

    removed log

--- src/app/containers/hmcts-primary-navigation/hmcts-primary-navigation.component.ts
+++ src/app/containers/hmcts-primary-navigation/hmcts-primary-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FeatureToggleService } from '@hmcts/rpx-xui-common-lib';
 import { Observable, of } from 'rxjs';
 import { NavItem } from '../../store';
@@ -8,7 +8,7 @@ import { NavItem } from '../../store';
     templateUrl: './hmcts-primary-navigation.component.html',
     styleUrls: ['./hmcts-primary-navigation.component.scss']
 })
-export class HmctsPrimaryNavigationComponent implements OnInit {
+export class HmctsPrimaryNavigationComponent {
 
     @Input() set userLoggedIn(value) {
         this.isUserLoggedIn = value;
@@ -22,14 +22,7 @@ export class HmctsPrimaryNavigationComponent implements OnInit {
     constructor(private featureToggleService: FeatureToggleService) {
     }
 
-    ngOnInit(): void {
-      console.log('items', this.items);
-    }
-
     public isFeatureNavEnabled$(navItem: NavItem): Observable<boolean> {
-        console.log('navItem', navItem);
-        console.log('isfeatureToggleable', navItem.feature.isfeatureToggleable);
-
         return navItem.feature.isfeatureToggleable ? this.featureToggleService.isEnabled(navItem.feature.featureName) : of(true);
     }
 }
