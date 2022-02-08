e010001f5395f281512591193e9a2935c0a3fdd4
Paul Howes <paul.howes@HMCTS.NET>
2022-02-08 17:27:45 +0000

    Approve PBA submit functionality

--- src/org-manager/components/index.ts
+++ src/org-manager/components/index.ts
@@ -1,23 +1,28 @@
 import { BackLinkComponent } from './back-link/back-link.component';
+import { NewPBAsConfirmComponent } from './new-pbas-confirm/new-pbas-confirm.component';
 import { NewPBAsInfoComponent } from './new-pbas-info/new-pbas-info.component';
 import { NotificationBannerComponent } from './notification-banner/notification-banner.component';
 import { OrganisationAddressComponent } from './organisation-address';
 import { OrganisationDetailsInfoComponent } from './organisation-details-info/organisation-details-info.component';
 import { PBAAccountApprovalComponent } from './pba-account-approval/pba-account-approval.component';
+import { PBAAccountDecisionComponent } from './pba-account-decision/pba-account-decision.component';
 
 export const components: any[] = [
   BackLinkComponent,
   NewPBAsInfoComponent,
+  NewPBAsConfirmComponent,
   NotificationBannerComponent,
   OrganisationAddressComponent,
   OrganisationDetailsInfoComponent,
-  PBAAccountApprovalComponent
+  PBAAccountApprovalComponent,
+  PBAAccountDecisionComponent
 ];
 
 export * from './back-link/back-link.component';
 export * from './new-pbas-info/new-pbas-info.component';
+export * from './new-pbas-confirm/new-pbas-confirm.component';
 export * from './notification-banner/notification-banner.component';
 export * from './organisation-address';
 export * from './organisation-details-info/organisation-details-info.component';
 export * from './pba-account-approval/pba-account-approval.component';
-
+export * from './pba-account-decision/pba-account-decision.component';
--- src/org-manager/components/new-pbas-info/new-pbas-info.component.ts
+++ src/org-manager/components/new-pbas-info/new-pbas-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { OrganisationVM } from 'src/org-manager/models/organisation';
 
 @Component({
@@ -8,8 +9,19 @@ import { OrganisationVM } from 'src/org-manager/models/organisation';
 export class NewPBAsInfoComponent {
 
   @Input() public org: OrganisationVM;
+  @Input() public newPBAs: Map<string, string>;
+  @Output() public submitForm = new EventEmitter();
+  @Output() public newPBA = new EventEmitter<{ name: string, value: string }>();
 
   constructor() {
 
+  }
+
+  public onSubmitForm(): void {
+    this.submitForm.emit();
+  }
+
+  public setNewPBA($event): void {
+    this.newPBA.emit({ name: $event.name, value: $event.value });
   }
 }
