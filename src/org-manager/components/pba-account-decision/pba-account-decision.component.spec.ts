5cfd9f95c62ae3747007afe29432704e120fffd1
Paul Howes <paul.howes@HMCTS.NET>
2022-02-08 17:27:45 +0000

    Approve PBA submit functionality

--- src/org-manager/components/pba-account-decision/pba-account-decision.component.spec.ts
+++ src/org-manager/components/pba-account-decision/pba-account-decision.component.spec.ts
@@ -1,29 +1,22 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { PBAAccountApprovalComponent } from '..';
-import { OrganisationAddressComponent } from '../organisation-address';
-
-import { NewPBAsInfoComponent } from './new-pbas-info.component';
+import { PBAAccountDecisionComponent } from './pba-account-decision.component';
 
 describe('NewPBAsInfoComponent', () => {
-  let component: NewPBAsInfoComponent;
-  let fixture: ComponentFixture<NewPBAsInfoComponent>;
+  let component: PBAAccountDecisionComponent;
+  let fixture: ComponentFixture<PBAAccountDecisionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterModule, RouterTestingModule.withRoutes([])],
-      declarations: [NewPBAsInfoComponent, OrganisationAddressComponent, PBAAccountApprovalComponent],
-      schemas: [
-        CUSTOM_ELEMENTS_SCHEMA
-      ],
+      declarations: [PBAAccountDecisionComponent]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NewPBAsInfoComponent);
+    fixture = TestBed.createComponent(PBAAccountDecisionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -31,5 +24,4 @@ describe('NewPBAsInfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
 });
