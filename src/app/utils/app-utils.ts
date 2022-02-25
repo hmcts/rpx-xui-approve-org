09dec1f5063186aeec6984e30843f31f900adde6
Paul Howes <paul.howes@HMCTS.NET>
2022-02-25 12:39:25 +0000

    Load and filter tabs

--- src/app/utils/app-utils.ts
+++ src/app/utils/app-utils.ts
@@ -22,12 +22,14 @@ export class AppUtils {
   public static mapOrganisation(apiOrg: Organisation): OrganisationVM {
     const organisationVm = new OrganisationVM();
     organisationVm.name = apiOrg.name;
-    organisationVm.adminEmail = apiOrg.superUser.email;
+    if (apiOrg.superUser) {
+      organisationVm.adminEmail = apiOrg.superUser.email;
+      organisationVm.admin = `${apiOrg.superUser.firstName} ${apiOrg.superUser.lastName}`;
+    }
     organisationVm.pbaNumber = apiOrg.paymentAccount;
     organisationVm.organisationId = apiOrg.organisationIdentifier;
     organisationVm.view = 'View';
     organisationVm.status = apiOrg.status;
-    organisationVm.admin = `${apiOrg.superUser.firstName} ${apiOrg.superUser.lastName}`;
     if (apiOrg.contactInformation && apiOrg.contactInformation[0]) {
       organisationVm.dxNumber = apiOrg.contactInformation[0].dxAddress;
       organisationVm.addressLine1 = apiOrg.contactInformation[0].addressLine1;
