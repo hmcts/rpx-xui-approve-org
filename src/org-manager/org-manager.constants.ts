8ae2413dbfe3d7613d85cb8122ba6cc46452a76b
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2022-02-28 12:37:18 +0000

    implemenationbeforeAPIAdd

--- src/org-manager/org-manager.constants.ts
+++ src/org-manager/org-manager.constants.ts
@@ -1,28 +1,3 @@
-/**
- * Feed to create inputs for changing the PBA
- */
-const pbaInputFeed =  [
-  {
-    config: {
-      label: 'PBA number 1 (optional)',
-      hint: '',
-      name: 'pba1',
-      id: 'pba1',
-      type: 'text',
-      classes: ''
-    },
-  },
-  {
-    config: {
-      label: 'PBA number 2 (optional)',
-      hint: '',
-      name: 'pba2',
-      id: 'pba2',
-      type: 'text',
-      classes: ''
-    }
-  }
-];
 
 const pbaErrorMessages = ['There is a problem. Enter a PBA number, for example PBA1234567'];
 const pbaServerErrorMessages = 'There is problem with the services, please try again later';
@@ -31,11 +6,20 @@ const statusCodes = {
   serverErrors: [0, 500, 502, 503, 504]
 };
 
-
-
 export class OrgManagerConstants {
-  static PBA_INPUT_FEED = pbaInputFeed;
   static PBA_ERROR_MESSAGES = pbaErrorMessages;
   static STATUS_CODES = statusCodes;
   static PBA_SERVER_ERROR_MESSAGE = pbaServerErrorMessages;
+}
+
+/**
+ * Feed to create inputs for changing the PBA
+ */
+export class PBAConfig {
+  label: string
+  hint: string
+  name: string
+  id: string
+  type: string
+  classes: string
 }
