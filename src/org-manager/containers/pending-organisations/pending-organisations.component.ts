2c40363b3f4fcaffe4d7dc0b1f6ba21773ad4fe0
Paul Howes <paul.howes@HMCTS.NET>
2022-03-02 15:06:20 +0000

    Search across tabs functionality

--- src/org-manager/containers/pending-organisations/pending-organisations.component.ts
+++ src/org-manager/containers/pending-organisations/pending-organisations.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingService } from '@hmcts/rpx-xui-common-lib';
+import { SessionStorageService } from '../../../shared/services/session-storage.service';
 import { OrganisationService } from '../../services';
 import { OrganisationListComponent } from './../organisation-list/organisation-list.component';
 
@@ -15,9 +16,10 @@ export class PendingOrganisationsComponent extends OrganisationListComponent imp
     protected router: Router,
     protected ref: ChangeDetectorRef,
     protected organisationService: OrganisationService,
-    protected loadingService: LoadingService
+    protected loadingService: LoadingService,
+    protected sessionStorageService: SessionStorageService
   ) {
-    super(router, ref, organisationService, loadingService);
+    super(router, ref, organisationService, loadingService, sessionStorageService);
   }
 
   public ngOnInit(): void {
