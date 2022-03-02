addf0b50eb43e394a5e7b659bab0fa9564c164da
Ronald Mansveld <ronald.mansveld@atos.net>
2022-03-02 11:43:26 +0000

    clean up app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,38 @@
 import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { ExuiCommonLibModule, FeatureToggleService, LaunchDarklyService } from '@hmcts/rpx-xui-common-lib';
+import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
 import { EffectsModule } from '@ngrx/effects';
-import { RouterStateSerializer, StoreRouterConnectingModule, DefaultRouterStateSerializer } from '@ngrx/router-store';
+import { DefaultRouterStateSerializer, RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
 // ngrx
 import { MetaReducer, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
 import { CookieModule } from 'ngx-cookie';
+import { OrgManagerModule } from 'src/org-manager/org-manager.module';
+import { DefaultErrorHandler } from 'src/shared/errorHandler/defaultErrorHandler';
 import { environment } from '../environments/environment';
+import { EnvironmentConfig } from '../models/environmentConfig.model';
+import { AuthService } from '../services/auth/auth.service';
 import { SharedModule } from '../shared/shared.module';
+import { initApplication } from './app-initilizer';
+import { ROUTES } from './app.routes';
+// from Components
+import * as fromComponents from './components';
+// from Containers
+import * as fromContainers from './containers/';
 import { AppComponent } from './containers/app/app.component';
+import { AbstractAppInsights, AppInsightsWrapper } from './services/appInsightsWrapper';
+import { CryptoWrapper } from './services/cryptoWrapper';
+import { EnvironmentService } from './services/environment.service';
+import { JwtDecodeWrapper } from './services/jwtDecodeWrapper';
+import { LogOutKeepAliveService } from './services/keep-alive/keep-alive.service';
+import { LoggerService } from './services/logger.service';
+import { MonitoringService } from './services/monitoring.service';
 import { CustomSerializer, reducers } from './store/';
 import { effects } from './store/effects';
 
-// from Containers
-import * as fromContainers from './containers/';
-
-// from Components
-import * as fromComponents from './components';
-
-import { ROUTES } from './app.routes';
-
-import { OrgManagerModule } from 'src/org-manager/org-manager.module';
-
-import { ExuiCommonLibModule, FeatureToggleService, LaunchDarklyService } from '@hmcts/rpx-xui-common-lib';
-import config from 'config';
-import { DefaultErrorHandler } from 'src/shared/errorHandler/defaultErrorHandler';
-import {AuthService} from '../services/auth/auth.service';
-import { AbstractAppInsights, AppInsightsWrapper } from './services/appInsightsWrapper';
-import { CryptoWrapper } from './services/cryptoWrapper';
-import { JwtDecodeWrapper } from './services/jwtDecodeWrapper';
-import { LoggerService } from './services/logger.service';
-import { MonitoringService } from './services/monitoring.service';
-
-import {NgIdleKeepaliveModule} from '@ng-idle/keepalive';
-import { initApplication } from './app-initilizer';
-import { EnvironmentService } from './services/environment.service';
-import {LogOutKeepAliveService} from './services/keep-alive/keep-alive.service';
-import { EnvironmentConfig, ENVIRONMENT_CONFIG } from '../models/environmentConfig.model'
-
-export const metaReducers: MetaReducer<any>[] = !config.production
-  ? []
-  : [];
+export const metaReducers: MetaReducer<any>[] = [];
 
 export function launchDarklyClientIdFactory(envConfig: EnvironmentConfig): string {
     return envConfig.launchDarklyClientId || '';
