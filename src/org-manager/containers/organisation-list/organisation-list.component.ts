400ded205171ae169e7e3b1b23bc1b3dc7c1e184
Paul Howes <paul.howes@HMCTS.NET>
2022-03-02 15:06:20 +0000

    Search across tabs functionality

--- src/org-manager/containers/organisation-list/organisation-list.component.ts
+++ src/org-manager/containers/organisation-list/organisation-list.component.ts
@@ -7,6 +7,7 @@ import { AppUtils } from '../../../app/utils/app-utils';
 import { SortOrder } from '../../../enums/sort-order';
 import SortField from '../../../models/common/sort-field.model';
 import { SearchOrganisationRequest, SortParameter } from '../../../models/dtos';
+import { SessionStorageService } from '../../../shared/services/session-storage.service';
 import { handleFatalErrors, WILDCARD_SERVICE_DOWN } from '../../../shared/utils/handle-fatal-errors';
 
 import { OrganisationVM } from '../../models/organisation';
@@ -28,7 +29,8 @@ export abstract class OrganisationListComponent implements OnInit, OnDestroy {
     protected router: Router,
     protected ref: ChangeDetectorRef,
     protected organisationService: OrganisationService,
-    protected loadingService: LoadingService
+    protected loadingService: LoadingService,
+    protected sessionStorageService: SessionStorageService
   ) {
   }
 
@@ -43,6 +45,7 @@ export abstract class OrganisationListComponent implements OnInit, OnDestroy {
           fieldName: 'organisationId',
           order: SortOrder.ASC
         };
+        this.sessionStorageService.setItem('searchString', searchString);
         this.showSpinner$ = this.loadingService.isLoading;
         const loadingToken = this.loadingService.register();
         this.performSearchPagination(searchString).pipe(take(1)).subscribe(result => {
@@ -56,8 +59,7 @@ export abstract class OrganisationListComponent implements OnInit, OnDestroy {
           handleFatalErrors(error.status, this.router, WILDCARD_SERVICE_DOWN);
         });
       });
-
-    this.organisationService.setOrganisationSearchString('');
+    this.organisationService.setOrganisationSearchString(this.sessionStorageService.getItem('searchString'));
   }
 
   public performSearchPagination(searchString): Observable<any> {
