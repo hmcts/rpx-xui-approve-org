2c980eabd9e25a87318cab6a61c9407ed40c5cf9
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2022-03-11 17:00:55 +0000

    remove markup

--- src/org-manager/containers/edit-details/edit-details.component.ts
+++ src/org-manager/containers/edit-details/edit-details.component.ts
@@ -1,54 +1,75 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import {Observable, Subscription} from 'rxjs';
-import {take, tap} from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+import { UpdatePbaServices } from 'src/org-manager/services/update-pba.services';
 import * as fromRoot from '../../../app/store';
-import {OrgManagerConstants} from '../../org-manager.constants';
+import { OrganisationDetails } from '../../models/organisation';
+import { PendingPaymentAccount } from '../../models/pendingPaymentAccount.model';
+import { PBAConfig } from '../../org-manager.constants';
 import * as fromStore from '../../store';
+import { PBANumberModel } from '../pending-pbas/models';
 
-/**
- * Bootstraps Edit Organisation Details
- */
 @Component({
   selector: 'app-change-details',
   templateUrl: './edit-details.component.html'
 })
 export class EditDetailsComponent implements OnInit, OnDestroy {
   public changePbaFG: FormGroup;
-  public pbaInputs: {config: {name: string}}[];
+  public pbaInputs: PBAConfig[];
   public pbaError$: Observable<object>;
   public pbaErrorsHeader$: Observable<any>;
   public orgDetails$: Observable<any>;
-  private subscirptions: Subscription;
+  private subscriptions: Subscription;
   public orgId: string;
   public pbaNumbers: string[];
   public saveDisabled = true;
   public serverError$: Observable<{ type: string; message: string }>;
+  public organisationDetails: OrganisationDetails;
 
-  constructor(private readonly store: Store<fromStore.OrganisationRootState>) {}
+  constructor(
+    private readonly updatePbaServices: UpdatePbaServices,
+    private readonly store: Store<fromStore.OrganisationRootState>,
+    private readonly router: Router,
+    private readonly fb: FormBuilder) { }
+
+  public get fPba() { return this.changePbaFG.controls; }
 
   public ngOnInit(): void {
-    this.pbaInputs = OrgManagerConstants.PBA_INPUT_FEED;
-    this.changePbaFG = new FormGroup({});
+    this.pbaInputs = [];
+    this.changePbaFG = new FormGroup({
+      pbaNumbers: this.fb.array([])
+    });
     this.getOrgs();
-    this.createPbaForm();
     this.getErrorMsgs();
   }
 
   private getOrgs(): void {
     this.orgDetails$ = this.store.pipe(select(fromStore.getActiveAndPending),
-        tap((value) => {
-          if (value) {
-            this.orgId = value.organisationId;
-            this.pbaNumbers = value.pbaNumber;
-            this.saveDisabled = !value.pbaNumber;
-          } else if (!value && !this.orgId) {
-            this.store.dispatch(new fromStore.LoadActiveOrganisation());
-            this.store.dispatch(new fromStore.LoadPendingOrganisations());
-          }
-        }));
+      tap((value) => {
+        if (value) {
+          this.orgId = value.organisationId;
+          this.pbaNumbers = value.pbaNumber;
+          this.createPbaForm();
+          this.saveDisabled = !value.pbaNumber;
+        } else if (!value && !this.orgId) {
+          this.store.dispatch(new fromStore.LoadActiveOrganisation());
+          this.store.dispatch(new fromStore.LoadPendingOrganisations());
+        }
+      }));
   }
+
+  public get pbaFormArrayNumbers(): FormArray {
+    return this.changePbaFG.get('pbaNumbers') as FormArray;
+  }
+
+  public get currentPaymentAccounts(): PBANumberModel[] {
+    return this.organisationDetails.paymentAccount
+      .filter(pba => !this.organisationDetails.pendingRemovePaymentAccount.includes(pba));
+  }
+
   private getErrorMsgs() {
     this.store.dispatch(new fromStore.ClearPbaErrors());
     this.pbaError$ = this.store.pipe(select(fromStore.getPbaFromErrors));
@@ -56,66 +77,82 @@ export class EditDetailsComponent implements OnInit, OnDestroy {
     this.serverError$ = this.store.pipe(select(fromStore.getServerErrors));
   }
 
-  public createPbaForm(): void {
-    for (const inputs of this.pbaInputs ) {
-      this.changePbaFG.addControl(inputs.config.name, new FormControl(''));
-      const validators = [
-        Validators.pattern(/(PBA\w*)/i),
-        Validators.minLength(10),
-        Validators.maxLength(10)
-      ];
-      this.changePbaFG.controls[inputs.config.name].setValidators(validators);
-      this.changePbaFG.controls[inputs.config.name].updateValueAndValidity();
+  public onAddNewBtnClicked(): void {
+    if (this.pbaInputs.length) {
+      this.appendAnotherNumber(this.pbaInputs.length + 1);
+      this.addPbaFormItem(this.pbaInputs[this.pbaInputs.length - 1].name);
     }
-
-    this.store.pipe(select(fromStore.getPbaNumber), take(1)).subscribe((pba: string) => {
-      pba.split(',').map((p, i) => {
-        this.changePbaFG.patchValue({[`pba${i + 1}`]: p});
-      });
-    });
-
-    this.subscirptions = this.changePbaFG.valueChanges.subscribe(value => {
-      const pba: string[] = Object.keys(value).map(key => value[key]).filter(item => item !== '');
-      const isNewPba = JSON.stringify(this.pbaNumbers) === JSON.stringify(pba);
-      this.saveDisabled = !isNewPba;
-    });
-
   }
 
-  // convenience getter for easy access to form fields
-  get fPba() { return this.changePbaFG.controls; }
+  public appendAnotherNumber(index: number) {
+    const config = new PBAConfig();
+    config.label = `PBA number ${index} (optional)`;
+    config.name = `pba${index}`;
+    config.id = `pba${index}`;
+    config.type = 'text';
+    this.pbaInputs.push(config);
+  }
+
+  public addPbaFormItem(inputsName: string) {
+    this.changePbaFG.addControl(inputsName, new FormControl(''));
+    const validators = [
+      Validators.pattern(/(PBA\w*)/i),
+      Validators.minLength(10),
+      Validators.maxLength(10)
+    ];
+    this.changePbaFG.controls[inputsName].setValidators(validators);
+    this.changePbaFG.controls[inputsName].updateValueAndValidity();
+  }
+
+  public createPbaForm(): void {
+    if (this.pbaNumbers && !this.pbaInputs.length) {
+      for (let i = 0; i < this.pbaNumbers.length; i++) {
+        this.appendAnotherNumber(i + 1);
+      }
+
+      if (!this.pbaNumbers.length) {
+        this.appendAnotherNumber(1);
+      }
+      for (const inputs of this.pbaInputs) {
+        this.addPbaFormItem(inputs.name);
+        this.pbaNumbers = [''];
+      }
+
+      this.store.pipe(select(fromStore.getPbaNumber), take(1)).subscribe((pba: string) => {
+        pba.split(',').map((p, i) => {
+          this.changePbaFG.patchValue({ [`pba${i + 1}`]: p });
+        });
+      });
+
+      this.subscriptions = this.changePbaFG.valueChanges.subscribe(value => {
+        const pba: string[] = Object.keys(value).map(key => value[key]).filter(item => item !== '');
+        const isNewPba = JSON.stringify(this.pbaNumbers) === JSON.stringify(pba);
+        this.saveDisabled = !isNewPba;
+      });
+    }
+  }
 
   public onSubmitPba(): void {
-    this.dispatchStoreValidation();
-    const {valid, value} = this.changePbaFG;
+    const { valid, value } = this.changePbaFG;
     const paymentAccounts: string[] = Object.keys(value).map(key => value[key]).filter(item => item !== '');
-    if (valid) {
-      this.store.dispatch(new fromStore.SubmitPba({paymentAccounts, orgId: this.orgId}));
-    }
-  }
 
-  private dispatchStoreValidation(): void {
-    const validation = {
-      isInvalid: {
-        pba1: [
-          (this.fPba.pba1.errors && this.fPba.pba1.errors.pattern),
-          (this.fPba.pba1.errors && this.fPba.pba1.errors.minlength),
-          (this.fPba.pba1.errors && this.fPba.pba1.errors.maxLength)
-        ],
-        pba2: [
-          (this.fPba.pba2.errors && this.fPba.pba2.errors.pattern),
-          (this.fPba.pba2.errors && this.fPba.pba2.errors.minlength),
-          (this.fPba.pba2.errors && this.fPba.pba2.errors.maxLength)
-        ]
-      },
-      errorMsg: OrgManagerConstants.PBA_ERROR_MESSAGES
-    };
-    this.store.dispatch(new fromStore.DispatchSaveValidation(validation));
+    const paymentAccountUpdated: string[] = [];
+    paymentAccounts.forEach(paymentAccount => {
+      if (typeof paymentAccount === 'string') {
+        paymentAccountUpdated.push(paymentAccount.toString());
+      }
+    });
+
+    if (valid) {
+      this.updatePbaServices.updatePba({ paymentAccounts: paymentAccountUpdated, orgId: this.orgId }).subscribe(() => {
+        this.router.navigateByUrl(`/organisation-details/${this.orgId}`);
+      });
+    }
   }
 
   public ngOnDestroy(): void {
-    if (this.subscirptions) {
-      this.subscirptions.unsubscribe();
+    if (this.subscriptions) {
+      this.subscriptions.unsubscribe();
     }
   }
 
@@ -123,4 +160,10 @@ export class EditDetailsComponent implements OnInit, OnDestroy {
     this.store.dispatch(new fromRoot.Back());
   }
 
+  public pendingChanges(appending: string[]): PendingPaymentAccount {
+    return {
+      pendingAddPaymentAccount: appending,
+      pendingRemovePaymentAccount: []
+    };
+  }
 }
