98b63ae3faa9197ef7d4c021717a125eaedbd70e
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2022-03-15 22:23:24 +0000

    revertm

--- src/org-manager/containers/organisation-details/organisation-details.component.ts
+++ src/org-manager/containers/organisation-details/organisation-details.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { User } from '@hmcts/rpx-xui-common-lib';
 import { select, Store } from '@ngrx/store';
-import { Observable, of, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { AppUtils } from 'src/app/utils/app-utils';
+import { Observable, Subscription } from 'rxjs';
+import { filter, take, takeWhile } from 'rxjs/operators';
+
+import * as fromRoot from '../../../app/store';
 import { UserApprovalGuard } from '../../guards/users-approval.guard';
 import { OrganisationUserListModel, OrganisationVM } from '../../models/organisation';
-import { OrganisationService } from '../../services/organisation.service';
-import { PbaAccountDetails } from '../../services/pba-account-details.services';
 import * as fromStore from '../../store';
+
+/**
+ * Bootstraps Organisation Details
+ */
 @Component({
   selector: 'app-org-details',
   templateUrl: './organisation-details.component.html'
 })
 export class OrganisationDetailsComponent implements OnInit, OnDestroy {
+
   public orgs$: Observable<OrganisationVM>;
   public userLists$: Observable<OrganisationUserListModel>;
   public showUsers = false;
@@ -22,88 +26,82 @@ export class OrganisationDetailsComponent implements OnInit, OnDestroy {
   public organisationId: string;
   public organisationAdminEmail: string;
   public isActiveOrg = false;
-  private orgId: string;
   public organisationDeletable = false;
-  private readonly getAllLoadedSubscription: Subscription;
+
+  private getShowOrgDetailsSubscription: Subscription;
+  private getAllLoadedSubscription: Subscription;
+  private getOrganisationDeletableSubscription: Subscription;
 
   constructor(
     private readonly store: Store<fromStore.OrganisationRootState>,
-    private readonly userApprovalGuard: UserApprovalGuard,
-    private readonly pbaAccountDetails: PbaAccountDetails,
-    private readonly organisationService: OrganisationService,
-    private readonly route: ActivatedRoute,
-  ) {
-    this.route.params.subscribe(params => {
-      this.orgId = params.orgId ? params.orgId : '';
-    });
-  }
+    private readonly userApprovalGuard: UserApprovalGuard
+  ) { }
 
   public ngOnInit(): void {
+
     this.isXuiApproverUserdata = this.userApprovalGuard.isUserApprovalRole();
     if (this.isXuiApproverUserdata) {
+      this.getShowOrgDetailsSubscription = this.store.pipe(select(fromStore.getShowOrgDetailsUserTabSelector)).subscribe(value => this.showUsers = value);
     }
 
-    this.organisationService.getSingleOrganisation({ id: this.orgId })
-      .pipe(map(apiOrg => AppUtils.mapOrganisation(apiOrg)))
-      .subscribe(organisationVM => {
-        this.organisationId = organisationVM.organisationId;
-        this.organisationAdminEmail = organisationVM.adminEmail;
-        this.showUserNavigation = false;
-        this.organisationService.getSingleOrganisation({ id: this.orgId })
-      .pipe(map(apiOrg => AppUtils.mapOrganisation(apiOrg)))
-      .subscribe(organisationVM => {
-        this.organisationId = organisationVM.organisationId;
-        this.organisationAdminEmail = organisationVM.adminEmail;
-        this.showUserNavigation = false;
-        if (organisationVM.status === 'ACTIVE') {
-          this.isActiveOrg = true;
-          this.organisationService.getOrganisationDeletableStatus(this.organisationId).subscribe(value => this.organisationDeletable = value);
-        }
+    this.store.dispatch(new fromStore.ResetOrganisationUsers());
+    this.getAllLoadedSubscription = this.store.pipe(select(fromStore.getAllLoaded)).pipe(takeWhile(loaded => !loaded)).subscribe(loaded => {
+      if (!loaded) {
+        this.store.dispatch(new fromStore.LoadActiveOrganisation());
+        this.store.dispatch(new fromStore.LoadPendingOrganisations());
+      }
+    });
 
-        if (organisationVM.pbaNumber && organisationVM.pbaNumber.length) {
-          let ids: string;
-          organisationVM.pbaNumber.forEach(pbaNumber => {
-            ids = !ids ? pbaNumber : `${ids},${pbaNumber}`;
-          });
-          this.pbaAccountDetails.getAccountDetails(ids).subscribe(accountResponse => {
-            organisationVM.accountDetails = accountResponse;
-          });
+    this.orgs$ = this.store.pipe(select(fromStore.getActiveAndPending));
+    this.orgs$.pipe(
+      filter(value => value !== undefined),
+      take(1)
+    ).subscribe(({ organisationId, pbaNumber, isAccLoaded, status, adminEmail }) => {
+      this.organisationId = organisationId;
+      this.organisationAdminEmail = adminEmail;
+      this.showUserNavigation = false;
+      if (status === 'ACTIVE') {
+        this.isActiveOrg = true;
+        // Check the deletable status of the organisation (required to control visibility of the "Delete" button)
+        this.store.dispatch(new fromStore.GetOrganisationDeletableStatus(this.organisationId));
+        this.getOrganisationDeletableSubscription = this.store.pipe(select(fromStore.getOrganisationDeletable)).subscribe(value => this.organisationDeletable = value);
+        if (this.isXuiApproverUserdata) {
+          this.showUserNavigation = true;
+          this.store.dispatch(new fromStore.LoadOrganisationUsers(organisationId));
         }
+      }
 
-        if (organisationVM.organisationId) {
-          this.userLists$ = this.organisationService.getOrganisationUsers(organisationVM.organisationId).pipe(
-            map(data => {
-              const orgUserListModel = {
-                users: AppUtils.mapUsers(data.users),
-                isError: false,
-              } as OrganisationUserListModel;
-              return orgUserListModel;
-            }));
+      if (!isAccLoaded && pbaNumber.length) {
+        this.store.dispatch(new fromStore.LoadPbaAccountsDetails({
+          orgId: organisationId,
+          pbas: pbaNumber.toString()
         }
-
-        this.orgs$ = of(organisationVM);
-      });
+        )
+        );
+      }
+    });
+    this.userLists$ = this.store.pipe(select(fromStore.getOrganisationUsersList));
   }
 
   public onGoBack() {
-    this.router.navigateByUrl(this.isActiveOrg ? '/active-organisation' : '/pending-organisations');
+    this.store.dispatch(new fromRoot.Go({ path: [this.isActiveOrg ? '/active-organisation' : '/pending-organisations'] }));
   }
 
   public approveOrganisation(data: OrganisationVM) {
-    if() {
-      this.router.navigateByUrl('/approve-organisations');
+    if (data) {
+      this.store.dispatch(new fromStore.AddReviewOrganisations(data));
     }
   }
 
   public deleteOrganisation(data: OrganisationVM) {
-    if() {
-      this.router.navigateByUrl('/delete-organisation');
+    if (data) {
+      this.store.dispatch(new fromStore.NavigateToDeleteOrganisation(data));
     }
   }
 
   public reviewOrganisation(data: OrganisationVM): void {
-    if() {
-      this.router.navigateByUrl('/review-organisation');
+    if (data) {
+      this.store.dispatch(new fromStore.NavigateToReviewOrganisation(data));
     }
   }
 
@@ -113,24 +111,26 @@ export class OrganisationDetailsComponent implements OnInit, OnDestroy {
   }
 
   public onShowUserDetails(user: User) {
-    if(user) {
+    if (user) {
       this.store.dispatch(new fromStore.ShowUserDetails({ userDetails: user, isSuperUser: this.organisationAdminEmail === user.email, orgId: this.organisationId }));
     }
   }
 
   public ngOnDestroy() {
-    if(this.getAllLoadedSubscription); {
+    if (this.getAllLoadedSubscription) {
       this.getAllLoadedSubscription.unsubscribe();
+    }
+    if (this.getShowOrgDetailsSubscription) {
+      this.getShowOrgDetailsSubscription.unsubscribe();
+    }
+    this.store.dispatch(new fromStore.ShowOrganisationDetailsUserTab({ orgId: this.organisationId, showUserTab: false }));
+
+    if (this.getOrganisationDeletableSubscription) {
+      this.getOrganisationDeletableSubscription.unsubscribe();
     }
 
     // Update the "organisation deletable" status in the store manually to false (to avoid the "Delete" button being
     // displayed when it shouldn't)
     this.store.dispatch(new fromStore.GetOrganisationDeletableStatusSuccess(false));
-
-    if (this.getOrganisationDeletableSubscription) {
-      this.getOrganisationDeletableSubscription.unsubscribe();
-    }
   }
 }
-
-
