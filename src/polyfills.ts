1d5f37554afce0791922f6d568446d374f12730b
Ronald Mansveld <ronald.mansveld@atos.net>
2022-03-15 17:19:00 +0000

    Update Angular to 13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -22,9 +22,6 @@ import 'core-js/es7/object';
 import 'core-js/es7/array';
 import 'isomorphic-fetch';
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /* IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
 
@@ -32,14 +29,6 @@ import 'isomorphic-fetch';
 /* Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
-
-
-/*
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
