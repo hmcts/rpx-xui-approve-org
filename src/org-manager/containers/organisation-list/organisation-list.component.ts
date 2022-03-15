3e8bead427e3053c35636349d94172b38903ef7c
Ronald Mansveld <ronald.mansveld@atos.net>
2022-03-15 15:57:35 +0000

    update to Angular 10

--- src/org-manager/containers/organisation-list/organisation-list.component.ts
+++ src/org-manager/containers/organisation-list/organisation-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, OnDestroy, OnInit, Directive } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingService } from '@hmcts/rpx-xui-common-lib';
 import { Observable, Subscription } from 'rxjs';
@@ -14,6 +14,7 @@ import { handleFatalErrors, WILDCARD_SERVICE_DOWN } from '../../../shared/utils/
 import { OrganisationVM } from '../../models/organisation';
 import { OrganisationService } from '../../services';
 
+@Directive()
 export abstract class OrganisationListComponent implements OnInit, OnDestroy {
 
   public searchString: string = '';
