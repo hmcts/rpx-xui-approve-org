16a48fdffbbb84e7f8123cc4de8a574fcfbcf28b
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2022-03-20 08:54:44 +0000

    server error from MO

--- src/org-manager/org-manager.constants.ts
+++ src/org-manager/org-manager.constants.ts
@@ -1,6 +1,11 @@
 
 const pbaErrorMessages = ['There is a problem. Enter a PBA number, for example PBA1234567'];
 const pbaServerErrorMessages = 'There is problem with the services, please try again later';
+const pbaErrorAlreadyUsedMessages = ['This PBA number xxxxxxxxxx has already been used.' +
+                                     'You should check that the PBA has been entered' +
+                                     'correctly.You should also check if your organisation' +
+                                     'has already been registered.If you are still having' +
+                                     'problems, contact HMCTS.'];
 
 const statusCodes = {
   serverErrors: [0, 500, 502, 503, 504]
@@ -8,6 +13,7 @@ const statusCodes = {
 
 export class OrgManagerConstants {
   public static PBA_ERROR_MESSAGES = pbaErrorMessages;
+  public static PBA_ERROR_ALREADY_USED_MESSAGES = pbaErrorMessages;
   public static STATUS_CODES = statusCodes;
   public static PBA_SERVER_ERROR_MESSAGE = pbaServerErrorMessages;
 }
