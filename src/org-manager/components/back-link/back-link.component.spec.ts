a6feeb49c207959e818229fcee0edc01b8f9aa69
Philip Whitaker <phillip.whitaker1@hmcts.net>
2022-04-11 16:27:03 +0100

    added unit tests for back-link component - 100% coverage

--- src/org-manager/components/back-link/back-link.component.spec.ts
+++ src/org-manager/components/back-link/back-link.component.spec.ts
@@ -1,15 +1,21 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BackLinkComponent } from '..';
-import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import * as fromRoot from '../../../app/store';
+import { ActivatedRoute, RouterModule, UrlSegment } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 describe('BackLinkComponent', () => {
   let component: BackLinkComponent;
   let fixture: ComponentFixture<BackLinkComponent>;
 
+  let store = jasmine.createSpyObj('store', ['dispatch']);
+  let router: ActivatedRoute;
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterModule, RouterTestingModule.withRoutes([]) ],
+      imports: [ RouterModule, RouterTestingModule.withRoutes([]), StoreModule.forRoot({}) ],
       declarations: [ BackLinkComponent ]
     })
     .compileComponents();
@@ -19,10 +25,37 @@ describe('BackLinkComponent', () => {
     fixture = TestBed.createComponent(BackLinkComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.inject(ActivatedRoute);
+    component.store = store;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+
+  describe('onGoBack()', () => {
+    it('should go back to pending-organisations if on active-organisation URL path', () => {
+      router.url = of([new UrlSegment('active-organisation', {})]);
+
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      component.onGoBack();
+
+      expect(store.dispatch).toHaveBeenCalledWith(new fromRoot.Go({path: ['/pending-organisations']}));
+    });
+
+    it('should go back to normal history if not on active-organisation URL path', () => {
+      router.url = of([new UrlSegment('not-active-organisation', {})]);
+
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      component.onGoBack();
+
+      expect(store.dispatch).toHaveBeenCalledWith(new fromRoot.Back());
+    });
+  });
 });
