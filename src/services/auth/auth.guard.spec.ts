de16c2e5f0a451307e6f52c03b2e170ca17220f9
Philip Whitaker <phillip.whitaker1@hmcts.net>
2022-04-11 11:59:39 +0100

    Added unit tests for the auth.guard - 100% coverage

--- src/services/auth/auth.guard.spec.ts
+++ src/services/auth/auth.guard.spec.ts
@@ -1,88 +1,53 @@
-/*import { inject, TestBed } from '@angular/core/testing';
-import { AuthService } from './auth.service';
-
-import { StoreModule } from '@ngrx/store';
-import {AuthGuard} from './auth.guard';
-import {CookieOptionsProvider, CookieService} from 'ngx-cookie';
-import {AppConfigService} from '../config/configuration.services';
-import {environment} from '../../../environments/environment';
-import {Router} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
-
-const config = {
-  config: {
-    cookies: {
-      token: 'bob',
-      userId: 'ben'
-    },
-    services: {
-      idam_web: 'http://idam_url.com'
-    },
-    oauth_callback_url: 'callback_url',
-    api_base_url: 'api_base',
-    idam_client: 'client_name'
-  }
-};
-
-const router = {
-  navigate: () => { }
-};
-
-const cookieService = {
-  get: key => {
-    return cookieService[key];
-  },
-  set: (key, value) => {
-    cookieService[key] = value;
-  },
-  removeAll: () => { }
-};
-
+import { HttpClient } from "@angular/common/http";
+import { TestBed } from "@angular/core/testing";
+import { of } from "rxjs";
+import { AuthGuard } from "./auth.guard";
+import { AuthService } from "./auth.service";
 
 class HttpClientMock {
-
   get() {
-    return 'response';
+      return of('response');
   }
 }
 
-class AppConfigServiceMock {
-  getRoutesConfig() {
-    return {
-      idam: {
-        idamLoginUrl: 'dummy',
-        idamClientID: 'dummy',
-        oauthCallbackUrl: 'dummy'
-      }
-    };
-  }
-}
+describe('AuthGuard', () => {
+  let authService: AuthService;
+  let authGuard: AuthGuard;
+  let loginRedirectSpy: jasmine.Spy;
 
-
-
-describe('AuthService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        StoreModule.forRoot({})
-      ],
       providers: [
         AuthService,
-        { provide: AppConfigService, useClass: AppConfigServiceMock},
-        { provide: environment, useValue: config },
-        { provide: Router, useValue: router },
-        { provide: CookieService, useValue: cookieService },
+        AuthGuard,
         { provide: HttpClient, useClass: HttpClientMock }
       ]
     });
+
+    authService = TestBed.inject(AuthService);
+    authGuard = TestBed.inject(AuthGuard);
+    loginRedirectSpy = spyOn(authService, 'loginRedirect').and.returnValue();
   });
 
-  it('should exist', inject([AuthGuard], (gurad: AuthGuard) => {
-    expect(gurad).toBeTruthy();
-  }));
+  it('should exist', () => {
+    expect(authGuard).toBeTruthy();
+  });
 
+  it('should redirect to login when unauthenticated', () => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(of(false));
 
-  it('should exist', inject([AuthGuard], (gurad: AuthGuard) => {
-    expect(gurad.canActivate).toBeDefined();
-  }));
-});*/
+    authGuard.canActivate().subscribe(canActivate => {
+      expect(loginRedirectSpy).toHaveBeenCalled();
+      expect(canActivate).toBeFalse();
+    });
+  });
+
+  it('should return true when authenticated', () => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(of(true));
+
+    authGuard.canActivate().subscribe(canActivate => {
+      expect(loginRedirectSpy).not.toHaveBeenCalled();
+      expect(canActivate).toBeTrue();
+    });
+  });
+});
