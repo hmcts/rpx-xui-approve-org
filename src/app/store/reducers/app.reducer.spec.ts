436ffbc24f9519de237c75caa113012325855bbe
Philip Whitaker <phillip.whitaker1@hmcts.net>
2022-04-14 11:36:08 +0100

    added tests for user model and reducer

--- src/app/store/reducers/app.reducer.spec.ts
+++ src/app/store/reducers/app.reducer.spec.ts
@@ -1,6 +1,7 @@
 import * as appReducer from './app.reducer';
 import * as fromActions from '../actions';
 import { AppUtils } from 'src/app/utils/app-utils';
+import { UserInterface, UserModel } from 'src/models/user.model';
 
 describe('App Reducer', () => {
     it('should return the default state', () => {
@@ -34,6 +35,26 @@ describe('App Reducer', () => {
             const expectedState = {
                 ...appReducer.initialState,
                 globalError: null
+            };
+            expect(state).toEqual(expectedState);
+        });
+    });
+
+    describe('GET_USER_DETAILS_SUCCESS', () => {
+        it('should return state', () => {
+            const userDetails = {
+                email: 'example@hmcts.net',
+                userId: '123',
+                orgId: '1'
+            };
+
+            const action = new fromActions.GetUserDetailsSuccess(userDetails as UserInterface);
+
+            const state = appReducer.reducer(appReducer.initialState, action);
+            const expectedState = {
+                ...appReducer.initialState,
+                userDetails: new UserModel(userDetails),
+                loaded: true
             };
             expect(state).toEqual(expectedState);
         });
