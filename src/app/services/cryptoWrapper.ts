5aba0d76cdd7b8fdaff38faff5272c03e30ff2c2
Phillip Whitaker <phillip.whitaker1@hmcts.net>
2022-05-05 14:35:34 +0100

    updated libraries

--- src/app/services/cryptoWrapper.ts
+++ src/app/services/cryptoWrapper.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { AES, enc, SHA256, WordArray } from 'crypto-js';
+import { AES, enc, SHA256 } from 'crypto-js';
 
 @Injectable()
 export class CryptoWrapper {
-    encrypt(message: string, secret: string = 'secret'): WordArray {
+    encrypt(message: string, secret: string = 'secret'): CryptoJS.lib.CipherParams {
         const userIdEncrypted = AES.encrypt(message, secret, SHA256);
         return userIdEncrypted;
     }
 
-    decrypt(encrypted: WordArray, secret: string = 'secret'): string {
+    decrypt(encrypted: CryptoJS.lib.CipherParams, secret: string = 'secret'): string {
         return AES.decrypt(encrypted, secret, SHA256).toString(enc.Utf8);
     }
 }
--- src/app/services/jwtDecodeWrapper.ts
+++ src/app/services/jwtDecodeWrapper.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import * as jwtDecode from 'jwt-decode';
+import jwt_decode from "jwt-decode";
 
 @Injectable()
 export class JwtDecodeWrapper {
     decode(jwt: string): any {
-        return jwtDecode(jwt);
+        return jwt_decode(jwt);
     }
 }
