6192cc7e39e95fcd5cf56ebd21297dcb95b7a54a
rtiru-hmcts <rohith.tiru1@hmcts.net>
2022-05-18 09:47:00 +0100

    EUI-5864: Activate a newly registered organisation.
      This commit contains the following changes.
        1. Created a new folder feature_ver2.0 to cater to new approve org design.
        2. Incorporated Login change done as part of tests/EUI-5862-LoginLogout branch.
        3. Added all necesary steps, features and pageobjects under feature_ver_2.0.
        4. Created a new entry in package.json and conf file to run 2.0 tests.

---
+++ test/e2e/features_ver2.0/app/approveOrganisation.feature
@@ -0,0 +1,23 @@
+@2.0_test @fullfunctional @approve_org
+Feature: Approve New Organisation
+
+  Background: 
+    When I navigate to EUI Approve Organisation Url
+    Given I am logged into approve organisation with HMCTS admin
+    Then I should be redirected to approve organisation dashboard page
+    Then I Check the home page title appear
+
+  Scenario: Approve New Organisation
+    When I click first organization view link
+    Then I see "Approve organisation" on organisation details page
+    And I see the organisation state as "PENDING"
+    When I select approve organisation
+    Then I am on confirm decision page
+    When I confirm the decision
+    Then I get a registration confirmation
+    When I select "Active organisations" tab
+    And I search for the organisation
+    Then I can see the orgainsation listed
+    When I click first organization view link
+    Then I see "Organisation details" on organisation details page
+    And I see the organisation state as "ACTIVE"
---
+++ test/e2e/features_ver2.0/step_definitions/steps/organisationDetails.steps.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { defineSupportCode } = require('cucumber');
+const browserWaits = require('../../../support/customWaits');
+const organisationDetails = require('../../pageObjects/organisationDetailsPage');
+
+defineSupportCode(function ({ Given, When, Then, And }) {
+
+  Then('I see {string} on organisation details page', { timeout: 600 * 1000 }, async function (orgDetailsHeader) {
+    await browserWaits.waitForElement(organisationDetails.heading);
+    expect(organisationDetails.heading.getText()).to.eventually.equal(orgDetailsHeader);
+    let orgDetailsKeys = [
+      'Name',
+      'SRA ID',
+      'Administrator',
+      'Address',
+      'DX',
+      'PBA number'
+    ];
+    let orgDetails = await organisationDetails.getOrgDetails();
+    expect(Object.keys(orgDetails)).to.include.all.members(orgDetailsKeys);
+
+  });
+
+  When(/^I select approve organisation$/, async function () {
+    await organisationDetails.approveOrg();
+  });
+
+  Then('I see the organisation state as {string}', async function (orgState) {
+    expect(organisationDetails.identityBar.getText()).to.eventually.include(orgState);
+  });
+
+});
