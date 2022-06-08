04fe0df320bc55450924b8997b260ce982edf93f
rtiru-hmcts <rohith.tiru1@hmcts.net>
2022-06-08 13:38:05 +0100

    EUI-5935: Added tests and support files for Change/Add Multiple PBAs when org in Pending/Active state.
      This commit also addresses change for the following tickets:  EUI-5936, EUI-5937
      This commit addresses the following tests.
        Test to change multiple PBAs when Org in Pending state.
        Test to change multiple PBAs when Org in Active state.
        Test to add multiple PBAs when Org in Pending state.
        Test to add multiple PBAs when Org in Active state.

--- test/e2e/features_ver2.0/pageObjects/organisationDetailsPage.js
+++ test/e2e/features_ver2.0/pageObjects/organisationDetailsPage.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const { SHORT_DELAY, MID_DELAY, LONG_DELAY } = require('../../support/constants');
 const browserWaits = require('../../support/customWaits');
 
 function OrganisationDetailsPage() {
@@ -10,6 +9,7 @@ function OrganisationDetailsPage() {
     this.orgDetailsKeys = $$('.govuk-summary-list__key');
     this.orgDetailsValues = $$('.govuk-summary-list__value');
     this.orgDetailsActions = $$('.govuk-summary-list__actions');
+    this.pbaPageLinks = $$('[href^="/change\/pba"]'); //element(by.partialLinkText("/change/pba"));
     this.approveOrgOption = element(by.id('reason-0'));
     this.submitButton = element(by.xpath('//button[contains(text(),"Submit")]'));
 
@@ -32,6 +32,13 @@ function OrganisationDetailsPage() {
     this.approveOrg = async function () {
         await this.approveOrgOption.click();
         await this.submitButton.click();
+    };
+
+    this.clickChangePba = async function(count){
+        let elementLink = await this.pbaPageLinks.get(count - 1);
+        await browserWaits.waitForElementClickable(elementLink);
+        await browser.sleep(2000);
+        await elementLink.click();
     };
 
 }
