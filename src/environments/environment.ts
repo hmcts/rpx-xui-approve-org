91cc31883e0a92fd07ba11d236f3426678378634
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2022-07-15 11:01:34 +0100

    EUI-5986-approve org user list pagination (#661)
    * approve org user list pagination
    
    * linting error
    
    * cve
    
    * nodeMock update inline with node api changes
    
    * cve
    
    * CVE tmp fix
    
    * cve suppress
    
    * updated test url
    
    Co-authored-by: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Co-authored-by: ernestman28 <ernest_man28@hotmail.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,6 +11,7 @@ export const environment = {
     serviceDeskTel: '0300 3030686',
     singleOrgUrl: 'api/organisations?organisationId=',
     organisationUsersUrl: 'api/organisations?usersOrgId=',
+    organisationUsersUrlWithoutRole: 'api/allUserListWithoutRoles?usersOrgId=',
     orgActiveUrl: 'api/organisations?status=ACTIVE',
     updatePbaUrl: 'api/updatePba',
     pbaAccUrl: 'api/pbaAccounts',
