bf55a5b7884961a2485e37af1ec0e5e1373796a5
jDman <davis.jaym@gmail.com>
2022-07-21 14:52:19 +0100

    remove telemetry api call for requests

--- api/lib/appInsights.ts
+++ api/lib/appInsights.ts
@@ -1,7 +1,6 @@
 import * as applicationInsights from 'applicationinsights'
-import * as express from 'express'
-import {getConfigValue, showFeature} from '../configuration'
-import {APP_INSIGHTS_KEY, FEATURE_APP_INSIGHTS_ENABLED} from '../configuration/references'
+import { getConfigValue, showFeature } from '../configuration'
+import { APP_INSIGHTS_KEY, FEATURE_APP_INSIGHTS_ENABLED } from '../configuration/references'
 
 export let client
 
@@ -14,10 +13,9 @@ if (showFeature(FEATURE_APP_INSIGHTS_ENABLED)) {
         .setAutoCollectExceptions(true)
         .setAutoCollectDependencies(true)
         .setAutoCollectConsole(true)
-        .setSendLiveMetrics(true)
         .setUseDiskRetryCaching(true)
         .setSendLiveMetrics(true)
-        .start()
+        .start();
 
     client = applicationInsights.defaultClient
     client.context.tags[client.context.keys.cloudRole] = 'xui-ao'
@@ -25,12 +23,4 @@ if (showFeature(FEATURE_APP_INSIGHTS_ENABLED)) {
 
 } else {
     client = null
-}
-
-export function appInsights(req: express.Request, res: express.Response, next) {
-    if (client) {
-        client.trackNodeHttpRequest({ request: req, response: res })
-    }
-
-    next()
 }
