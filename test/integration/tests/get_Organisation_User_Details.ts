c8484ffa4395d3a223e23867e6b70b5d8d53d15a
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2022-08-03 17:02:54 +0100

    path change of func test

--- test/integration/tests/get_Organisation_User_Details.ts
+++ test/integration/tests/get_Organisation_User_Details.ts
@@ -3,7 +3,7 @@ const should = require('chai').should()
 
 suite('Approve Org -> Get Organisation Users details', function() {
   this.timeout(timeout);
-  test('GET Organisation User details', () => generateAPIRequest ('GET', '/api/organisations?usersOrgId=2GIHJH9')
+  test('GET Organisation User details', () => generateAPIRequest ('GET', '/api/allUserListWithoutRoles?usersOrgId=2GIHJH9')
      // console.log('response', response.headers.get('cache-control'))
         .then(response => {
            response.status.should.be.eql(200);
