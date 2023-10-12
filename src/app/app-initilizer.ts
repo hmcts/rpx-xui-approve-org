8a41ad627b6667d35da14f2d05644629efb46df1
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-10-12 14:37:45 +0100

    Remove store from initilizer

--- src/app/app-initilizer.ts
+++ src/app/app-initilizer.ts
@@ -1,8 +1,6 @@
-import { Store } from '@ngrx/store';
 import { EnvironmentService } from './services/environment.service';
-import * as fromApp from './store';
 
-export function initApplication(store: Store<fromApp.State>, environmentService: EnvironmentService): VoidFunction {
+export function initApplication(environmentService: EnvironmentService): VoidFunction {
   return () => new Promise((resolve) => {
     environmentService.getEnv$().subscribe(() => {
       resolve(true);
