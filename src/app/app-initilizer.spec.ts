56502c0734c1453e081342a1718be2e7fc815a9a
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-10-12 14:37:45 +0100

    Remove store from initilizer

--- src/app/app-initilizer.spec.ts
+++ src/app/app-initilizer.spec.ts
@@ -3,11 +3,10 @@ import { initApplication } from './app-initilizer';
 
 describe('initApplication', () => {
   const serviceSpy = jasmine.createSpyObj('envservice', ['getEnv$']);
-  const storeSpy = jasmine.createSpyObj('store', ['dispatch', 'pipe']);
 
   it('initApplication', () => {
     serviceSpy.getEnv$.and.returnValue(of(true));
-    const returnValue: VoidFunction = initApplication(storeSpy, serviceSpy);
+    const returnValue: VoidFunction = initApplication(serviceSpy);
     expect(returnValue).toEqual(jasmine.any(Function));
     returnValue();
     expect(serviceSpy.getEnv$).toHaveBeenCalled();
