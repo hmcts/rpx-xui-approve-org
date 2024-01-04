d4f8e4cebd5c8edbcc88d39d41c883c1f175bc20
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2024-01-04 13:09:38 +0000

    codecept migration (#823)
    * codecept migration
    
    * functional test updates
    
    * smoke test command fix
    
    * functional test command for smoke test updated
    
    * disablly a11y test , to be fixed
    
    * codecept conf upate and playwright install chrome updated
    
    * test fixes
    
    * functional test command fix
    
    * functional and pa11y test updates
    
    * Functional test updates and fixes
    
    * functional test retry login enhancements
    
    * functional test step def fix
    
    * a11y test fix
    
    * a11y test config fix

--- test/e2e/features/step_definitions/steps/confirmApproveOrganisation.steps.js
+++ test/e2e/features/step_definitions/steps/confirmApproveOrganisation.steps.js
@@ -3,73 +3,73 @@
 const bannerPage = require('../../pageObjects/approveOrganisationObjects');
 const organisationList = require('../../pageObjects/organisationListPage');
 
-const { Then } = require('@cucumber/cucumber');
 const { AMAZING_DELAY, SHORT_DELAY, MID_DELAY, LONG_DELAY } = require('../../../support/constants');
-const config = require('../../../config/conf.js');
-const EC = protractor.ExpectedConditions;
+const config = require('../../../config/conf_old.js');
 const browserWaits = require('../../../support/customWaits');
 
 async function waitForElement(el) {
-  await browser.wait((result) => {
-    return element(by.className(el)).isPresent();
-  }, 600000);
+  await $(`.${el}`).wait()
 }
 
-Then('I click first organization view link', { timeout: 600 * 1000 }, async function(){
+Then('I click first organization view link', async function(){
   browser.sleep(LONG_DELAY);
   // await organisationList.waitForOrgListToDisplay();
   await organisationList.clickViewOnFirstOrganisation();
 });
 
-Then(/^I Select the Organisation and click Activate$/, async function () {
+Then('I click last organization view link', async function () {
+  browser.sleep(LONG_DELAY);
+  // await organisationList.waitForOrgListToDisplay();
+  await organisationList.clickViewOnLastOrganisation();
+});
+
+Then('I Select the Organisation and click Activate', async function () {
   // await waitForElement(bannerPage.approveorgBanner);
-  // await expect(bannerPage.approveorgBanner.isDisplayed()).to.eventually.be.true;
-  // await expect(bannerPage.selectCheckBox.isDisplayed()).to.eventually.be.true;
+  // await expect(bannerPage.approveorgBanner.isDisplayed()).to.be.true;
+  // await expect(bannerPage.selectCheckBox.isDisplayed()).to.be.true;
   // await browserWaits.waitForElement(bannerPage.selectCheckBox);
   // await bannerPage.selectCheckBox.click();
   // browser.sleep(MID_DELAY);
   await browserWaits.waitForElement(bannerPage.activate_button, 60);
-  await expect(bannerPage.activate_button.isDisplayed()).to.eventually.be.true;
+  await expect(await bannerPage.activate_button.isDisplayed()).to.be.true;
   await bannerPage.activate_button.click();
   browser.sleep(MID_DELAY);
 });
 
-Then(/^I approve the selected Organisations button$/, { timeout: 600 * 1000 }, async function () {
+Then('I approve the selected Organisations button',  async function () {
   // await waitForElement('govuk-heading-xl');
   await browserWaits.waitForElement(bannerPage.approve_button);
-  await expect(bannerPage.approve_button.isDisplayed()).to.eventually.be.true;
-  await expect(bannerPage.approve_button.getText())
+  await expect(await bannerPage.approve_button.isDisplayed()).to.be.true;
+  await expect(await bannerPage.approve_button.getText())
     .to
-    .eventually
     .equal('Confirm');
   await bannerPage.approve_button.click();
 });
 
-Then(/^I see the Confirmation screen of Organisations$/, { timeout: 600 * 1000 }, async function () {
+Then('I see the Confirmation screen of Organisations', async function () {
   await browserWaits.waitForElement(bannerPage.confirmationScreen);
-  await expect(bannerPage.confirmationScreen.isDisplayed()).to.eventually.be.true;
-  await expect(bannerPage.confirmationScreen.getText())
+  await expect(await bannerPage.confirmationScreen.isDisplayed()).to.be.true;
+  await expect(await bannerPage.confirmationScreen.getText())
     .to
-    .eventually
     .contains('Organisation approved successfully');
 });
 
-Then(/^I see the Confirmation screen of Deleted Organisation$/, { timeout: 600 * 1000 }, async function () {
+Then('I see the Confirmation screen of Deleted Organisation', async function () {
   await browserWaits.waitForElement(bannerPage.confirmationScreen);
-  await expect(bannerPage.confirmationScreen.isDisplayed()).to.eventually.be.true;
-  await expect(bannerPage.confirmationScreen.getText())
+  await expect(await bannerPage.confirmationScreen.isDisplayed()).to.be.true;
+  await expect(await bannerPage.confirmationScreen.getText())
     .to
-    .eventually
     .contains('has been deleted');
 });
 
-Then(/^I click to Back to Organisations link$/, { timeout: 600 * 1000 }, async function () {
-  await expect(bannerPage.backtoOrganisations.isDisplayed()).to.eventually.be.true;
+Then('I click to Back to Organisations link', async function () {
+  await expect(await bannerPage.backtoOrganisations.isDisplayed()).to.be.true;
   await bannerPage.backtoOrganisations.click();
-  await expect(bannerPage.secondaryHeader.isDisplayed()).to.eventually.be.true;
-  await expect(bannerPage.secondaryHeader.getText())
+  await expect(await bannerPage.secondaryHeader.isDisplayed()).to.be.true;
+  await expect(await bannerPage.secondaryHeader.getText())
     .to
-    .eventually
     .equals('New registrations');
 });
+
+
 
--- test/e2e/features/step_definitions/steps/loginLogout.steps.js
+++ test/e2e/features/step_definitions/steps/loginLogout.steps.js
@@ -3,16 +3,12 @@
 const loginPage = require('../../pageObjects/loginLogoutObjects');
 const headerPage = require('../../pageObjects/headerPage');
 
-const { Given, When, Then } = require('@cucumber/cucumber');
 const { AMAZING_DELAY, SHORT_DELAY, MID_DELAY, LONG_DELAY } = require('../../../support/constants');
-const config = require('../../../config/conf.js');
-const EC = protractor.ExpectedConditions;
+const config = require('../../../config/config');
 const browserWaits = require('../../../support/customWaits');
 
 async function waitForElement(el) {
-  await browser.wait((result) => {
-    return element(by.className(el)).isPresent();
-  }, 600000);
+  await browserWaits.waitForElement($(el))
 }
 
 async function loginattemptCheckAndRelogin(username, password, world){
@@ -20,7 +16,7 @@ async function loginattemptCheckAndRelogin(username, password, world){
 
   while (loginAttemptRetryCounter < 3){
     try {
-      await browserWaits.waitForstalenessOf(loginPage.emailAddress, 5);
+      await browserWaits.waitForElement(loginPage.dashboard_header)
       break;
     } catch (err){
       const emailFieldValue = await loginPage.getEmailFieldValue();
@@ -33,24 +29,23 @@ async function loginattemptCheckAndRelogin(username, password, world){
         }
 
         console.log(err+' email field is still present with empty value indicating  Login page reloaded due to EUI-1856 : Login re attempt '+loginAttemptRetryCounter);
-        world.attach(err +' email field is still present with empty value indicating Login page reloaded due to EUI-1856 : Login re attempt '+loginAttemptRetryCounter);
         await loginPage.loginWithCredentials(username, password);
         loginAttemptRetryCounter++;
       }
     }
   }
   console.log('ONE ATTEMPT:  EUI-1856 issue occured / total logins => '+firstAttemptFailedLogins+' / '+loginAttempts);
-  world.attach('ONE ATTEMPT:  EUI-1856 issue occured / total logins => '+firstAttemptFailedLogins+' / '+loginAttempts);
+  // world.attach('ONE ATTEMPT:  EUI-1856 issue occured / total logins => '+firstAttemptFailedLogins+' / '+loginAttempts);
 
   console.log('TWO ATTEMPT: EUI-1856 issue occured / total logins => '+secondAttemptFailedLogins+' / '+loginAttempts);
-  world.attach('TWO ATTEMPT: EUI-1856 issue occured / total logins => '+secondAttemptFailedLogins+' / '+loginAttempts);
+  // world.attach('TWO ATTEMPT: EUI-1856 issue occured / total logins => '+secondAttemptFailedLogins+' / '+loginAttempts);
 }
 
 let loginAttempts = 0;
 let firstAttemptFailedLogins = 0;
 let secondAttemptFailedLogins = 0;
 
-When(/^I navigate to approve organisation Url$/, { timeout: 600 * 1000 }, async function () {
+When('I navigate to approve organisation Url', async function () {
   await browser.get(config.config.baseUrl);
   await browser.driver.manage()
     .deleteAllCookies();
@@ -58,107 +53,101 @@ When(/^I navigate to approve organisation Url$/, { timeout: 600 * 1000 }, async 
   browser.sleep(AMAZING_DELAY);
 });
 
-Then(/^I should see failure error summary$/, async function () {
-  await waitForElement('heading-large');
-  await expect(loginPage.failure_error_heading.isDisplayed()).to.eventually.be.true;
-  await expect(loginPage.failure_error_heading.getText())
+Then('I should see failure error summary', async function () {
+  await waitForElement('.heading-large');
+  await expect(await loginPage.failure_error_heading.isDisplayed()).to.be.true;
+  await expect(await loginPage.failure_error_heading.getText())
     .to
-    .eventually
-    .be
-    .oneOf(['Incorrect email or password', 'There is a problem with your account login details']);
+    .includes('Incorrect email or password');
 });
 
-Then(/^I am on Idam login page$/, { timeout: 600 * 1000 }, async function () {
-  await waitForElement('heading-large');
-  await expect(loginPage.signinTitle.isDisplayed()).to.eventually.be.true;
-  await expect(loginPage.signinTitle.getText())
+Then('I am on Idam login page', async function () {
+  await waitForElement('.heading-large');
+  await expect(await loginPage.signinTitle.isDisplayed()).to.be.true;
+  await expect(await loginPage.signinTitle.getText())
     .to
-    .eventually
     .equal('Sign in');
-  await expect(loginPage.emailAddress.isDisplayed()).to.eventually.be.true;
-  await expect(loginPage.password.isDisplayed()).to.eventually.be.true;
+  await expect(await loginPage.emailAddress.isDisplayed()).to.be.true;
+  await expect(await loginPage.password.isDisplayed()).to.be.true;
 });
 
-When(/^I enter an valid email-address and password to login$/, async function () {
-  await loginPage.emailAddress.sendKeys(this.config.username); //replace username and password
-  await loginPage.password.sendKeys(this.config.password);
+When('I enter an valid email-address and password to login', async function () {
+  await loginPage.emailAddress.sendKeys(config.username); //replace username and password
+  await loginPage.password.sendKeys(config.password);
   // browser.sleep(SHORT_DELAY);
   await loginPage.signinBtn.click();
   browser.sleep(SHORT_DELAY);
   loginAttempts++;
-  await loginattemptCheckAndRelogin(this.config.username, this.config.password, this);
+  await loginattemptCheckAndRelogin(config.username, config.password, this);
 });
 
-When(/^I enter an Invalid email-address and password to login$/, async function () {
+When('I enter an Invalid email-address and password to login', async function () {
   await loginPage.givenIAmUnauthenticatedUser();
 });
 
-Given(/^I should be redirected to the Idam login page$/, async function () {
+Given('I should be redirected to the Idam login page', async function () {
   await browserWaits.waitForElement(loginPage.signinTitle);
-  await expect(loginPage.signinTitle.getText())
+  await expect(await loginPage.signinTitle.getText())
     .to
-    .eventually
     .equal('Sign in');
-  browser.sleep(LONG_DELAY);
 });
 
-Then(/^I select the sign out link$/, async function () {
+Then('I select the sign out link', async function () {
   browser.sleep(SHORT_DELAY);
-  await expect(loginPage.signOutlink.isDisplayed()).to.eventually.be.true;
+  await expect(await loginPage.signOutlink.isDisplayed()).to.be.true;
   browser.sleep(SHORT_DELAY);
   await loginPage.signOutlink.click();
   browser.sleep(SHORT_DELAY);
 });
 
-Then(/^I should be redirected to approve organisation dashboard page$/, async function () {
+Then('I should be redirected to approve organisation dashboard page', async function () {
   browser.sleep(SHORT_DELAY);
-  await browserWaits.waitForBrowserReadyState(120);
+  // await browserWaits.waitForBrowserReadyState(120);
   await browserWaits.waitForElement(loginPage.dashboard_header);
-  await expect(loginPage.dashboard_header.isDisplayed()).to.eventually.be.true;
-  await expect(loginPage.dashboard_header.getText())
+  await expect(await loginPage.dashboard_header.isDisplayed()).to.be.true;
+  await expect(await loginPage.dashboard_header.getText())
     .to
-    .eventually
     .equal('Approve organisation');
 });
 
-Given(/^I am logged into approve organisation with HMCTS admin$/, async function () {
-  await loginPage.loginWithCredentials(this.config.username, this.config.password);
-  browser.sleep(SHORT_DELAY);
+Given('I am logged into approve organisation with HMCTS admin', async function () {
+  await loginPage.loginWithCredentials(config.username, config.password);
+  await browser.sleep(SHORT_DELAY);
   loginAttempts++;
-  await loginattemptCheckAndRelogin(this.config.username, this.config.password, this);
+  await loginattemptCheckAndRelogin(config.username, config.password, this);
 });
 
-Given(/^I am logged into approve organisation with CWD admin$/, async function () {
+Given('I am logged into approve organisation with CWD admin', async function () {
   await loginPage.loginWithCredentials('cwd_admin@mailinator.com', 'Welcome01');
   browser.sleep(SHORT_DELAY);
   loginAttempts++;
   await loginattemptCheckAndRelogin('cwd_admin@mailinator.com', 'Welcome01', this);
 });
 
-Given(/^I am logged into approve organisation with approver prd admin$/, async function () {
-  await loginPage.loginWithCredentials(config.config.params.approver_username, config.config.params.approver_password);
+Given('I am logged into approve organisation with approver prd admin', async function () {
+  await loginPage.loginWithCredentials(config.approver_username, config.approver_password);
   browser.sleep(LONG_DELAY);
   loginAttempts++;
-  await loginattemptCheckAndRelogin(config.config.params.approver_username, config.config.params.approver_password, this);
+  await loginattemptCheckAndRelogin(config.approver_username, config.approver_password, this);
 });
 
-Given(/^I am logged into approve organisation with non approver prd admin$/, async function () {
-  await loginPage.loginWithCredentials(this.config.username, this.config.password);
+Given('I am logged into approve organisation with non approver prd admin', async function () {
+  await loginPage.loginWithCredentials(config.username, config.password);
   browser.sleep(LONG_DELAY);
   loginAttempts++;
-  await loginattemptCheckAndRelogin(this.config.username, this.config.password, this);
+  await loginattemptCheckAndRelogin(config.username, config.password, this);
 
   // await browserWaits.waitForElement(headerPage.signOut);
 });
 
-Given(/^I am logged into approve organisation with FR judge details$/, async function () {
-  await loginPage.loginWithCredentials(this.config.username, this.config.password);
+Given('I am logged into approve organisation with FR judge details', async function () {
+  await loginPage.loginWithCredentials(config.username, config.password);
   loginAttempts++;
-  await loginattemptCheckAndRelogin(this.config.username, this.config.password, this);
+  await loginattemptCheckAndRelogin(config.username, config.password, this);
 });
 
-Given(/^I navigate to approve organisation Url direct link$/, { timeout: 600 * 1000 }, async function () {
-  await browser.get(config.config.baseUrl + '/cases/case-filter');
+Given('I navigate to approve organisation Url direct link', async function () {
+  await browser.get(config.baseUrl + '/cases/case-filter');
   if (!process.env.TEST_URL.includes('demo')){ //Do not delete cookies for demo env.
     await browser.driver.manage()
       .deleteAllCookies();
@@ -167,11 +156,10 @@ Given(/^I navigate to approve organisation Url direct link$/, { timeout: 600 * 1
   browser.sleep(AMAZING_DELAY);
 });
 
-Then(/^I should be redirected back to Login page after direct link$/, async function () {
+Then('I should be redirected back to Login page after direct link', async function () {
   browser.sleep(LONG_DELAY);
-  await expect(loginPage.signinTitle.getText())
+  await expect(await loginPage.signinTitle.getText())
     .to
-    .eventually
     .equal('Sign in');
   browser.sleep(LONG_DELAY);
 });
