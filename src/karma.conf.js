d848e1dbdfefd8563251c3c261e91c6a643533d0
Josh <josh.glasgow@hmcts.net>
2024-04-19 16:31:28 +0100

    Change karma conf

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -17,18 +17,21 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
+      type: 'lcov',
       dir: require('path').join(__dirname, '../reports/tests/coverage/ng'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true,
-      thresholds: {
-        statements: 80,
-        lines: 80,
-        branches: 0,
-        functions: 60
-      }
+      subdir: '.'
     },
-    reporters: ['progress', 'kjhtml'],
+    specReporter: {
+      maxLogLines: 5, // limit number of lines logged per test
+      suppressErrorSummary: false,// do not print error summary
+      suppressFailed: false, // do not print information about failed tests
+      suppressPassed: false, // do not print information about passed tests
+      suppressSkipped: true, // do not print information about skipped tests
+      showSpecTiming: false, // print the time elapsed for each spec
+      failFast: false // test would finish with error when a first fail occurs.
+    },
+    reporters: ['spec', 'progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
