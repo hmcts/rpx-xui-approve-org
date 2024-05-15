73235e37b135d77f262cf1a8adc091c863714c78
Josh-HMCTS <128602796+Josh-HMCTS@users.noreply.github.com>
2024-05-15 11:39:07 +0100

    EXUI-1800 Angular upgrade (#837)
    * Upgrade angular from v15 to v16
    
    * Update ngrx, Update materials, Fix formatting in effect files
    
    * Update xui-translation version
    
    * update ngx-cookie
    
    * Update ngx-logger + ng-idle/keepalive
    
    * Update ng-idle/core, Update ngx-pagination, Fix app.routes spec
    
    * Update ngx-cookie use of 'getObject' to 'get'
    
    * ngx-logger re-set disableConsoleLogging to false, resolve yarn lint errors
    
    * Update yarn audit
    
    * Add path to sonar coverage inclusions
    
    * undo sonar tests inclusions
    
    * Change karma conf
    
    * Revert Karma conf changes
    
    * tidy
    
    * Replace deprecated cookiemodule 'forRoot' to 'withOptions'
    
    * Update test options
    
    * change karma conf
    
    * revert karma conf changes
    
    * change karma conf
    
    * Update to angular 17
    
    * Update yarn audit, change polyfills for testing
    
    * Update xui-common-lib and translation to use angular 17 pre-release version
    
    * Update yarn audit
    
    * Add 'waitForOrgListToDisplay' on 'deleteActiveOrganisation'
    
    * change deleteTest
    
    * Resolve crash on 'textFieldMatches'

--- src/services/auth/auth.guard.ts
+++ src/services/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
@@ -7,7 +7,7 @@ import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   constructor(
     public authService: AuthService,
   ) {}
