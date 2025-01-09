0c22cee1fb5667d5ab3205050d81ac5052170732
Anthony Dummer <anthony.dummer@HMCTS.net>
2025-01-09 15:04:45 +0000

    EXUI-2102 - adding in trace

--- test/e2e/features/pageObjects/loginLogoutObjects.js
+++ test/e2e/features/pageObjects/loginLogoutObjects.js
@@ -7,7 +7,7 @@ const reportLogger = require('../../../codeceptCommon/reportLogger')
 function loginLogoutObjects() {
   console.log('in loginLogoutObjects');
   console.log('emailAddress is: ' + element(by.css('input#username')));
-  this.emailAddress = element(by.css('input#username'));
+  this.emailAddress = element(by.css('[id=\'username\']'));
   console.log('emailAddress is: ' + this.emailAddress.getAttribute('value'));
   console.log('emailAddress is: ' + this.emailAddress.getAttribute('name'));
   console.log('password: ' + element(by.css('[id=\'password\']')));
