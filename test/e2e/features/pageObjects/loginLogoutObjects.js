d8f9f98297a6250a1c2bbb87d90b1e289b271ab5
Anthony Dummer <anthony.dummer@HMCTS.net>
2025-01-09 10:50:47 +0000

    EXUI-2102 - adding in trace

--- test/e2e/features/pageObjects/loginLogoutObjects.js
+++ test/e2e/features/pageObjects/loginLogoutObjects.js
@@ -5,12 +5,20 @@ const BrowserWaits = require('../../support/customWaits');
 const reportLogger = require('../../../codeceptCommon/reportLogger')
 
 function loginLogoutObjects() {
+  console.log('in loginLogoutObjects');
+  console.log('emailAddress is: ' + element(by.css('input#username')));
   this.emailAddress = element(by.css('input#username'));
+  console.log('password: ' + element(by.css('[id=\'password\']')));
   this.password = element(by.css('[id=\'password\']'));
+  console.log('signinTitle: ' + element(by.css('h1.heading-large')));
   this.signinTitle = element(by.css('h1.heading-large'));
+  console.log('signinBtn: ' + element(by.css('input.button')));
   this.signinBtn = element(by.css('input.button'));
+  console.log('signOutlink: ' + element(by.xpath('//a[@class=\'hmcts-header__navigation-link\']')));
   this.signOutlink = element(by.xpath('//a[@class=\'hmcts-header__navigation-link\']'));
+  console.log('failure_error_heading: ' + element(by.css('[id=\'validation-error-summary-heading\']')));
   this.failure_error_heading = element(by.css('[id=\'validation-error-summary-heading\']'));
+  console.log('failure_error_message: ' + element(by.css('[id=\'validation-error-summary-list\']')));
   this.dashboard_header= element(by.xpath('//a[@class=\'hmcts-header__link\']'));
   this.microsoftSignInObjects = {
     email: $('#i0116'),
