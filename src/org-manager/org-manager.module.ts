a57cf03fa9b8ccd90f0e83a5dd4fa782de4c82ae
Josh-HMCTS <128602796+Josh-HMCTS@users.noreply.github.com>
2025-05-27 15:33:03 +0100

    EXUI-2990 - upgrade to angular 18 (#877)
    * upgrade to angular 18
    
    * linting
    
    * update to use latest version of tk
    
    * remove console log
    
    * remove excessive server log, remove redisURL from preview.yml
    
    * add rediscloud url
    
    * test enabling OIDC on preview
    
    * update to use release version of lib for angular upgrade

--- src/org-manager/org-manager.module.ts
+++ src/org-manager/org-manager.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
 import { EffectsModule } from '@ngrx/effects';
@@ -13,20 +13,13 @@ import * as fromPipes from './pipes';
 import * as fromServices from './services';
 import { effects, reducers } from './store';
 
-@NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule,
+@NgModule({ exports: [...fromContainers.containers],
+  declarations: [...fromContainers.containers, ...fromComponents.components, ...fromPipes.pipes], imports: [CommonModule,
     orgManagerRouting,
     SharedModule,
     StoreModule.forFeature('orgState', reducers),
     EffectsModule.forFeature(effects),
-    ExuiCommonLibModule
-  ],
-  exports: [...fromContainers.containers],
-  declarations: [...fromContainers.containers, ...fromComponents.components, ...fromPipes.pipes],
-  providers: [...fromServices.services]
-})
+    ExuiCommonLibModule], providers: [...fromServices.services, provideHttpClient(withInterceptorsFromDi())] })
 
 export class OrgManagerModule {
 
