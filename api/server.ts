ba707930612e3d440e69a2af6aa8a79efe12e43f
Josh <josh.glasgow@hmcts.net>
2025-07-14 15:00:31 +0100

    remove idam query code, set node lib to release versoin

--- api/server.ts
+++ api/server.ts
@@ -3,35 +3,33 @@ import 'source-map-support/register';
 import * as ejs from 'ejs';
 import * as express from 'express';
 import * as path from 'path';
-import { createApp, logger } from './application';
+import { app, logger } from './application';
 
-createApp().then((app) => {
-  console.log('WE ARE USING server.ts on the box.');
+console.log('WE ARE USING server.ts on the box.');
 
-  /**
-   * Used Server side
-   */
-  app.engine('html', ejs.renderFile);
-  app.set('view engine', 'html');
-  app.set('views', __dirname);
+/**
+ * Used Server side
+ */
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
 
-  app.use(express.static(path.join(__dirname, '..', 'assets'), { index: false }));
-  app.use(express.static(path.join(__dirname, '..'), { index: false }));
+app.use(express.static(path.join(__dirname, '..', 'assets'), { index: false }));
+app.use(express.static(path.join(__dirname, '..'), { index: false }));
 
-  /**
-   * Used on server.ts only but should be fine to lift and shift to local.ts
-   */
-  app.use('/*', (req, res) => {
-    console.time(`GET: ${req.originalUrl}`);
-    res.render('../index', {
-      providers: [{ provide: 'REQUEST', useValue: req }, { provide: 'RESPONSE', useValue: res }],
-      req,
-      res
-    });
-    console.timeEnd(`GET: ${req.originalUrl}`);
+/**
+ * Used on server.ts only but should be fine to lift and shift to local.ts
+ */
+app.use('/*', (req, res) => {
+  console.time(`GET: ${req.originalUrl}`);
+  res.render('../index', {
+    providers: [{ provide: 'REQUEST', useValue: req }, { provide: 'RESPONSE', useValue: res }],
+    req,
+    res
   });
-
-  const port = process.env.PORT || 3000;
-
-  app.listen(port, () => logger.info(`Local server up at ${port}`));
+  console.timeEnd(`GET: ${req.originalUrl}`);
 });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => logger.info(`Local server up at ${port}`));
