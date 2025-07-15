01eb39c197796abf6724b499703e1462993b00c0
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2025-07-15 11:04:38 +0100

    Update org-workflows.test.ts (#902)
    Fixing nightly build

--- playwright_tests/org-workflows.test.ts
+++ playwright_tests/org-workflows.test.ts
@@ -14,11 +14,7 @@ test('i can approve a pending org', async ({ page, userName }) => {
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(page.getByRole('heading', { name: 'Confirm your decision' })).toBeVisible();
   await page.getByRole('button', { name: 'Confirm' }).click();
-
-  const spinner = page.locator('xuilib-loading-spinner');
-  if (await spinner.isVisible()) {
-    await spinner.waitFor({ state: 'detached', timeout: 5000 });
-  }
+  await page.waitForTimeout(5000);
   const successDivs = await page.locator('div').filter({ hasText: /SUCCESS\s*Registration approved/i });
   await expect(successDivs.first()).toBeVisible({ timeout: 5000 });
 });
@@ -35,11 +31,7 @@ test('i can reject a pending org', async ({ page, userName }) => {
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(page.getByRole('heading', { name: 'Confirm your decision' })).toBeVisible();
   await page.getByRole('button', { name: 'Confirm' }).click();
-
-  const spinner = page.locator('xuilib-loading-spinner');
-  if (await spinner.isVisible()) {
-    await spinner.waitFor({ state: 'detached', timeout: 5000 });
-  }
+  await page.waitForTimeout(5000);
   const rejectDivs = await page.locator('div').filter({ hasText: /SUCCESS\s*Registration rejected/i });
   await expect(rejectDivs.first()).toBeVisible({ timeout: 5000 });
 });
@@ -56,7 +48,7 @@ test('i can place registration under review for a pending org', async ({ page, u
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(page.getByRole('heading', { name: 'Confirm your decision' })).toBeVisible();
   await page.getByRole('button', { name: 'Confirm' }).click();
-
+  await page.waitForTimeout(5000);
   const underDivs = await page.locator('div').filter({ hasText: /SUCCESS\s*Registration put under/i });
   await expect(underDivs.first()).toBeVisible({ timeout: 5000 });
 });
@@ -74,19 +66,23 @@ test('i can delete an active org', async ({ page, userName }) => {
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(page.getByRole('heading', { name: 'Confirm your decision' })).toBeVisible();
   await page.getByRole('button', { name: 'Confirm' }).click();
-
-  const spinner = page.locator('xuilib-loading-spinner');
-  if (await spinner.isVisible()) {
-    await spinner.waitFor({ state: 'detached', timeout: 5000 });
-  }
+  await page.waitForTimeout(5000);
   const successDivs = await page.locator('div').filter({ hasText: /SUCCESS\s*Registration approved/i });
   await expect(successDivs.first()).toBeVisible({ timeout: 5000 });
   await page.getByRole('tab', { name: 'Active organisations' }).click();
   await page.getByLabel('Search').click();
   await page.getByLabel('Search').fill(orgName);
   await page.getByRole('button', { name: 'Search' }).click();
-  await page.waitForTimeout(5000);
-  await page.getByRole('link', { name: 'View' }).first().click();
+  for (let i = 0; i < 6; i++) {
+    await page.waitForTimeout(5000);
+    try {
+      console.log(`Attempt ${i}: Failed to click on 'View' link, retrying...`);
+      await page.getByRole('link', { name: 'View' }).first().click();
+      break;
+    } catch (error) {
+      console.error(error);
+    }
+  }
   await expect(page.locator('h1')).toBeVisible();
   await expect(page.locator('div').filter({ hasText: 'Organisation details Users' }).nth(1)).toBeVisible();
   await page.getByRole('button', { name: 'Delete organisation' }).click();
