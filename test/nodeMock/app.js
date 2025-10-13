25911e5cdecebc3ea11662f694509d5db88e4b92
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2025-10-13 10:06:01 +0100

    CME-774: Angular-20 upgrade, fix smoke tests

---
+++ test/nodeMock/app.js
@@ -0,0 +1,5 @@
+// Adapter to maintain backward compatibility for tests requiring '../../nodeMock/app'
+// Re-export the backend mock app implementation
+module.exports = require('../backendMock/app');
+
+
