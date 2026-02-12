73a314da54ce598f7fd6cf03311bc07d8849501a
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2026-02-12 17:20:40 +0000

    deprecated property

--- api/caseWorkerDetailsRouter/util.ts
+++ api/caseWorkerDetailsRouter/util.ts
@@ -19,12 +19,6 @@ export interface File {
   fieldname: string;
   /** Name of the file on the uploader's computer. */
   originalname: string;
-  /**
-   * Value of the `Content-Transfer-Encoding` header for this file.
-   * @deprecated since July 2015
-   * @see RFC 7578, Section 4.7
-   */
-  encoding: string;
   /** Value of the `Content-Type` header for this file. */
   mimetype: string;
   /** Size of the file in bytes. */
