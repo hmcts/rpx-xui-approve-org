dc694defb2afa4ce047550ce4a0a261e1c2153af
danH-test <dan.hitchcock@hmcts.net>
2026-04-29 09:57:02 +0100

    Update test case name for login/logout functionality to include smoke tag

--- playwright_tests/login.test.ts
+++ playwright_tests/login.test.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { signIn } from './helpers/login';
 
-const userIdentifier = 'base';
-
-test('login and log out from AO with valid user', async ({ page }) => {
+test('login and log out from AO with valid user @smoke', async ({ page }) => {
   await signIn(page);
   await expect(page.getByRole('heading', { name: 'Organisation approvals' })).toBeVisible();
   await page.getByRole('link', { name: 'Sign out' }).click();
