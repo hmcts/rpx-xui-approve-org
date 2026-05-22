5e5afe31884bda5d4bad9573f08aba4b9d26aef5
Chris Jones <christopher.jones@hmcts.net>
2026-05-22 15:11:58 +0100

    add axe audits to tab load tests

--- playwright_tests/tabs-load.test.ts
+++ playwright_tests/tabs-load.test.ts
@@ -1,15 +1,19 @@
 import { test, expect } from './e2e/page-objects/page.fixtures';
+import { runAxeAudit } from './helpers/axe';
 import { ensureAuthenticatedPage } from './helpers/sessionCapture';
 
 test.beforeEach(async ({ page }) => {
   await ensureAuthenticatedPage(page, 'base');
 });
-test('all tabs on login load data', async ({ organisationApprovalsPage }) => {
+test('all tabs on login load data', async ({ page, organisationApprovalsPage }, testInfo) => {
   await expect(organisationApprovalsPage.heading).toBeVisible();
   await expect(organisationApprovalsPage.tabPanel).toBeVisible();
   await expect(organisationApprovalsPage.pendingOverviewPanel).toBeVisible();
+  await runAxeAudit(page, testInfo, { reportName: 'Pending organisations tab' });
   await organisationApprovalsPage.openNewPbasTab();
   await expect(organisationApprovalsPage.pendingPbasPanel).toBeVisible();
+  await runAxeAudit(page, testInfo, { reportName: 'New PBAs tab' });
   await organisationApprovalsPage.openActiveOrganisationsTab();
   await expect(organisationApprovalsPage.activeOrganisationsPanel).toBeVisible();
+  await runAxeAudit(page, testInfo, { reportName: 'Active organisations tab' });
 });
