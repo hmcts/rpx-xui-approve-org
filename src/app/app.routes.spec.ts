49ed3cdf178c8351a3d4ebed19daadc2027b063d
Chris Jones <christopher.jones@hmcts.net>
2026-06-08 14:00:13 +0100

    updated cjtest

--- src/app/app.routes.spec.ts
+++ src/app/app.routes.spec.ts
@@ -20,7 +20,7 @@ import { JwtDecodeWrapper } from './services/jwtDecodeWrapper';
 import { LoggerService } from './services/logger.service';
 import { MonitoringService } from './services/monitoring.service';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-var testVariable = 'testValue';
+var testVariable = 'testValue123';
 
 @Component({
   selector: 'app-mock-root',
