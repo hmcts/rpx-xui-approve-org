c5348476bac7e9e5669cdb001e926e5b7fc64251
Chris Jones <christopher.jones@hmcts.net>
2026-06-18 09:07:35 +0100

    tidied up code

--- api/common/interceptorsMock.ts
+++ api/common/interceptorsMock.ts
@@ -1,4 +1,3 @@
-// exception-formatter is CommonJS-only, so this import form is required here.
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import exceptionFormatter = require('exception-formatter');
 
