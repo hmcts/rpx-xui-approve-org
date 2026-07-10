df1af49e8dcf79095a10b9ff6047ed184beade4d
Balaji Sridharan <balaji.sridharan@hmcts.net>
2026-07-10 16:08:57 +0100

    lint errors fix

--- src/app/containers/app/app.component.spec.ts
+++ src/app/containers/app/app.component.spec.ts
@@ -13,7 +13,6 @@ import * as fromRoot from '../../store';
 import { HeaderComponent } from '../header/header.component';
 import { AppComponent } from './app.component';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
 const windowMock: Window = { gtag: () => {} } as any;
 const idleMockService = jasmine.createSpyObj('idleService', ['appStateChanges']);
 const environmentMockService = jasmine.createSpyObj('environmentService', ['getEnv$']);
